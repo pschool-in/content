711bb97bb71064c8c8f5a95324d176f92cb713f5
priya2786 <45584551+priya2786@users.noreply.github.com>
2021-07-13 11:39:57 +0530

    Create ta.picture1.js

---
+++ tamil/playlists/ta.picture1.js
@@ -0,0 +1,14 @@
+export default {
+  label: 'படம்',
+  id: 'ta-picture',
+  img: 'image',
+  list: [
+    {
+      type: 'completeWord',
+      label: 'விடுபட்ட எழுத்தை நிரப்புக ',
+      id: 'complete-word',
+      commonData: {
+        images: 'custom',
+        lang: 'ta'
+      },
+      data: [
