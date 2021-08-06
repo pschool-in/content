072e914298850fb7afb475dc27783d0046701c3a
Rethna <rethna2@gmail.com>
2021-08-06 16:01:51 +0530

    updated hindi

--- hindi/playlists/hi-sound.js
+++ hindi/playlists/hi-sound.js
@@ -2,5 +2,45 @@ export default {
   label: 'sound',
   id: 'hi-sound',
   img: 'sound',
-  list: []
+  list: [
+    {
+      type: 'slides',
+      id: 'animals',
+      label: 'Animals',
+      commonData: {
+        audio: 'hi-animals.mp3'
+      },
+      data: [
+        {
+          audioOffset: 0,
+          text: `cat| बिल्ली
+dog| कुत्ता
+goat| बकरी
+sheep| भेड्
+horse| घोडा
+pig| सुअर
+elephant| हाथी
+lion| शेर
+monkey| बंदर
+tiger| बाघ`
+        }
+      ]
+    },
+    {
+      type: 'rightOne',
+      label: 'Select Picture',
+      id: 'pick-word',
+      commonData: {
+        title: 'Select Picture by listening to the Sound',
+        type: 'image'
+      },
+      data: [
+        {
+          audio: 'hi-animals.mp3',
+          words:
+            'cat, dog, goat, sheep, horse, pig, elephant, lion, monkey, tiger'
+        }
+      ]
+    }
+  ]
 };
