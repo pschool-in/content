46354a58f7ae710668b4196649b792be5221fa03
Rethna <rethna2@gmail.com>
2021-08-27 21:50:02 +0530

    hindi modification

--- hindi/playlists/hi-sound.js
+++ hindi/playlists/hi-sound.js
@@ -25,8 +25,8 @@ monkey| बंदर
 tiger| बाघ`
         },
         {
-            audioOffset: 20,
-            text: `hippo| जलहस्ती
+          audioOffset: 20,
+          text: `hippo| जलहस्ती
 rhino| गैंडा
 fox| लोमड़ी
 crocodile| मगरमच्छ
@@ -36,40 +36,40 @@ fish| मछली
 frog| मेंडक
 snake| साँप
 tortoise| कछुआ`
-          },
-          {
-            audioOffset: 40,
-            text: ` rat| चूहा
+        },
+        {
+          audioOffset: 40,
+          text: `rat| चूहा
 squirrel| गिलहरी
 ant| चींटी
 fly| मक्खी
 bat| चमगादड़
 buffalo| भेंस
 butterfly| तितली `
-          },
-          {
-            audioOffset: 54,
-            text: `leopard| तेंदुआ
+        },
+        {
+          audioOffset: 54,
+          text: `leopard| तेंदुआ
 cow| गाय
 donkey| गधा
 mongoose| नेवला
 mosquito| मच्छर
 lizard| छिपकली
 rabbit| खरगोश`
-          }        
+        }
       ]
     },
     {
-        type: 'slides',
-        id: 'birds',
-        label: 'चिड़िया',
-        commonData: {
-          audio: 'hi-birds.mp3'
-        },
-        data: [
-          {
-            audioOffset: 0,
-            text: `crow| कौवा
+      type: 'slides',
+      id: 'birds',
+      label: 'चिड़िया',
+      commonData: {
+        audio: 'hi-birds.mp3'
+      },
+      data: [
+        {
+          audioOffset: 0,
+          text: `crow| कौवा
 duck| बतख़
 hen| मुर्गी
 parrot| तोता
@@ -78,42 +78,42 @@ sparrow| गौरेया
 chick| चूजा
 myna| मैना
 ostrich| शुतरमुर्ग`
-          },
-          {
-            audioOffset: 18,
-            text: `owl| उल्लू
+        },
+        {
+          audioOffset: 18,
+          text: `owl| उल्लू
 rooster| मुर्गा
 woodpecker| कठफोड़व
 eagle| गिद्ध
 crane| सारस
 flamingo| मराल
 kingfisher| नीलकंठ`
-          }
-        ]
-      },
+        }
+      ]
+    },
     {
-        type: 'slides',
-        id: 'flowers',
-        label: 'फूल',
-        data: {
-          audio: 'hi-flowers-bodyParts-kitchen.mp3',
-          audioOffset: 0,
-          text: `rose | गुलाब
+      type: 'slides',
+      id: 'flowers',
+      label: 'फूल',
+      data: {
+        audio: 'hi-flowers-bodyParts-kitchen.mp3',
+        audioOffset: 0,
+        text: `rose | गुलाब
 lotus | कमल
 sunflower | सूरजमुखी
 marigold | गेंदा
 jasmine | चमेली
 hibiscus | गुडहल`
-        }
-      },
+      }
+    },
     {
-        type: 'slides',
-        id: 'bodyParts',
-        label: 'शरीर के अंग',
-        data: {
-          audio: 'hi-flowers-bodyParts-kitchen.mp3',
-          audioOffset: 12,
-          text: `ear| कान
+      type: 'slides',
+      id: 'bodyParts',
+      label: 'शरीर के अंग',
+      data: {
+        audio: 'hi-flowers-bodyParts-kitchen.mp3',
+        audioOffset: 12,
+        text: `ear| कान
 eye| आंख
 foot| टांग
 hair| केश
@@ -121,35 +121,35 @@ hand|  हाथ
 mouth| मुंह
 nose| नाक
 tooth| दांत`
-        }
-      },
+      }
+    },
     {
-        type: 'slides',
-        id: 'kitchen',
-        label: 'रसोई की सामग्री',
-        data: {
-          audio: 'hi-flowers-bodyParts-kitchen.mp3',
-          audioOffset: 28,
-          text: `stove| चूल्हा
+      type: 'slides',
+      id: 'kitchen',
+      label: 'रसोई की सामग्री',
+      data: {
+        audio: 'hi-flowers-bodyParts-kitchen.mp3',
+        audioOffset: 28,
+        text: `stove| चूल्हा
 fork| कांटा
 spoon| चमचा
 knife| छुरी
 pan|कड़ाही
 pot| मटका
 butter| मक्खन`
-        }
-      },
+      }
+    },
     {
-        type: 'slides',
-        id: 'fruits',
-        label: 'फल',
-        commonData: {
-          audio: 'hi-fruits.mp3'
-        },
-        data: [
-          {
-            audioOffset: 0,
-            text: `apple| सेब
+      type: 'slides',
+      id: 'fruits',
+      label: 'फल',
+      commonData: {
+        audio: 'hi-fruits.mp3'
+      },
+      data: [
+        {
+          audioOffset: 0,
+          text: `apple| सेब
 grapes| अंगूर
 banana| केला
 strawberry| झरबेर
@@ -158,30 +158,30 @@ jackfruit| कटहल
 mango| आम
 orange| संतरा
 papaya| पपीता `
-          },
-          {
-            audioOffset: 18,
-            text: `pineapple| अनानास
+        },
+        {
+          audioOffset: 18,
+          text: `pineapple| अनानास
 pomegranate| अनार
 watermelon| तरबूज
 fig| अंजीर
-custard apple| शरीफा
+custard-apple| शरीफा
 dates| खजूर
 muskmelon| खरबूज`
-          }
-        ]
-      },
+        }
+      ]
+    },
     {
-        type: 'slides',
-        id: 'vegetables',
-        label: 'सब्जियां',
-        commonData: {
-          audio: 'hi-vegetables.mp3'
-        },
-        data: [
-          {
-            audioOffset: 0,
-            text: `carrot| गाजर
+      type: 'slides',
+      id: 'vegetables',
+      label: 'सब्जियां',
+      commonData: {
+        audio: 'hi-vegetables.mp3'
+      },
+      data: [
+        {
+          audioOffset: 0,
+          text: `carrot| गाजर
 beetroot| चुकंदर
 onion| प्याज
 tomato| टमाटर
@@ -191,10 +191,10 @@ cauliflower| गोभी
 corn| मक्का
 cucumber| खीरा
 garlic| लहसुन`
-          },
-          {
-            audioOffset: 20,
-            text: `ginger| अदरक
+        },
+        {
+          audioOffset: 20,
+          text: `ginger| अदरक
 pumpkin| कद्दू
 almond| बादाम
 beans| फलियां
@@ -204,10 +204,10 @@ capsicum| शिमला मिर्च
 cashewnut| काजू
 chilli| मिर्च
 coconut| नारियल`
-          },
-          {
-            audioOffset: 40,
-            text: `corianderleaf| धनिए के पत्ते
+        },
+        {
+          audioOffset: 40,
+          text: `corianderleaf| धनिए के पत्ते
 curryleaf| करी पत्ता
 drumstick| ढोल का छड़ी
 greenpeas| हरी मटर
@@ -216,14 +216,15 @@ mintleaf| पुदीना
 peanut| मूंगफली
 raddish| मूली
 spinach| पालक`
-          }
-        ]
-      },
+        }
+      ]
+    },
     {
-        type: 'slides',
-        id: 'vehicles',
-        label: 'वाहनों',
-        data: [  {
+      type: 'slides',
+      id: 'vehicles',
+      label: 'वाहनों',
+      data: [
+        {
           audio: 'hi-vehicles.mp3',
           audioOffset: 0,
           text: `aeroplane| हवाई जहाज
@@ -234,10 +235,11 @@ bicycle| साइकिल
 helicopter| हेलीकॉप्टर
 motorbike| मोटरसाइकिल
 ship| समुंद्री जहाज
-tractor| ट्रैक्टर`},
-          {
-            audioOffset: 18,
-            text: `train| रेल गाडी
+tractor| ट्रैक्टर`
+        },
+        {
+          audioOffset: 18,
+          text: `train| रेल गाडी
 ambulance| रोगी वाहन
 auto| ऑटो
 cart| कार्ट
@@ -245,20 +247,20 @@ excavator| खोदक मशीन
 fire-engine| दमकल
 lorry| लॉरी
 Van|वेन`
-          }
-        ]
+        }
+      ]
+    },
+    {
+      type: 'slides',
+      id: 'house',
+      label: 'मकान',
+      commonData: {
+        audio: 'hi-house.mp3'
       },
-     {
-        type: 'slides',
-        id: 'house',
-        label: 'मकान',
-        commonData: {
-          audio: 'hi-house.mp3'
-        },
-        data: [
-          {
-            audioOffset: 0,
-            text: `house| मकान
+      data: [
+        {
+          audioOffset: 0,
+          text: `house| मकान
 table| मेज
 bed| बिस्तर
 candle| मोमबत्ती
@@ -266,30 +268,30 @@ chair| कुर्सी
 shelf| अलमारी
 baby| बच्चा
 boy| लड़का`
-          },
-          {
-            audioOffset: 16,
-            text: `girl| लड़की
+        },
+        {
+          audioOffset: 16,
+          text: `girl| लड़की
 man| आदमी
 woman| स्री
 ball| गेंद
 glasses| ऐनक
 clock| दीवार घड़ी
 watch| घड़ी`
-          }
-          ]
-     },
-          {
-        type: 'slides',
-        id: 'other-images',
-        label: 'अन्य छवियां',
-        commonData: {
-          audio: 'hi-others.mp3'
-        },
-        data: [
-          {
-            audioOffset: 0,
-            text: `sun| रवि
+        }
+      ]
+    },
+    {
+      type: 'slides',
+      id: 'other-images',
+      label: 'अन्य छवियां',
+      commonData: {
+        audio: 'hi-others.mp3'
+      },
+      data: [
+        {
+          audioOffset: 0,
+          text: `sun| रवि
 moon| चांद
 rain| वर्षा
 star| सितारा
@@ -297,11 +299,10 @@ milk| दूध
 ring| अंगूठी
 brush| ब्रश
 bone| हड्डी`
-
-          },
-          {
-            audioOffset: 16,
-            text: `bell| घंटी
+        },
+        {
+          audioOffset: 16,
+          text: `bell| घंटी
 bird| चिड़िया
 conch| शंख 
 five| पांच
@@ -309,11 +310,10 @@ drums| ढोल
 food| खाना
 fruits| फल
 king| राजा`
-
-          },
-          {
-            audioOffset: 32,
-            text: `kite| पतंग
+        },
+        {
+          audioOffset: 32,
+          text: `kite| पतंग
 ladder| सीढ़ी
 ladoo| लड्डू
 leaf| पत्ता
@@ -321,64 +321,68 @@ rishi| ऋषि
 wheel| पहिया
 whistle| सीटी
 world| दुनियाा`
-          }
-        ]
-      },
+        }
+      ]
+    },
     {
-        type: 'slides',
-        id: 'school',
-        label: 'विद्यालय',
-        commonData: {
-          audio: 'hi-school.mp3'
-        },
-        data: { audioOffset: 0,
-               text: `eraser| रबड़
+      type: 'slides',
+      id: 'school',
+      label: 'विद्यालय',
+      commonData: {
+        audio: 'hi-school.mp3'
+      },
+      data: {
+        audioOffset: 0,
+        text: `eraser| रबड़
 pen| कलम 
 pencil|पेंसिल 
 ruler| पेमाना
-bag| बस्ता 
+schoolbag | बस्ता 
 scissors| कैंची
 sharpener| चोखा करनेवाला 
 waterbottle| पानी की बोतल 
-Calculator | गणना यंत्र
+calculator | गणना यंत्र
 book| किताब
-Color pencils | रंग पेंसिल
+color-pencils | रंग पेंसिल
 crayons| चित्रांकनी `
-     },
-      {
-        type: 'slides',
-        id: 'tools',
-        label: 'उपकरण',
-        commonData: {
-          audio: 'hi-tools.mp3'
-        },
-        data: [ { audioOffset: 0,
-               text: `Axe| कुल्हाडी
+      }
+    },
+    {
+      type: 'slides',
+      id: 'tools',
+      label: 'उपकरण',
+      commonData: {
+        audio: 'hi-tools.mp3'
+      },
+      data: [
+        {
+          audioOffset: 0,
+          text: `axe| कुल्हाडी
 balance| तराज़ू
-Black board | श्यामपट 
+blackboard | श्यामपट 
 hose | रबर का पाइप
 plier| प्‍लास
 microscope| सूछ्म दर्शी
-Needle | सुई
+needle | सुई
 oven | कुठला
 plough | हल
-Saw| आरा`
-          },
-          { audioOffset: 20,
-               text: `Spade| कुदाल
-Stethescope|आला
+saw| आरा`
+        },
+        {
+          audioOffset: 20,
+          text: `spade| कुदाल
+stethescope|आला
 telescope| दूरबीन
-testube | परखनली
+testtube | परखनली
 thermometer| तापमान-यंत्र 
 trowel| करनी
-Compass| दिशा सूचक यंत्र
+compass| दिशा सूचक यंत्र
 easel| चित्रफलक
 hacksaw| लोहा-आरी
 scalpel| छुरी`
-
-          }
-          ]
-},
+        }
+      ]
+    },
     {
       type: 'rightOne',
       label: 'चित्र चुनें',
@@ -394,131 +398,133 @@ scalpel| छुरी`
             'cat, dog, goat, sheep, horse, pig, elephant, lion, monkey, tiger'
         },
         {
-            audio: 'hi-animals.mp3',
-            audioOffset: 20,
-            words:
-              'hippo, rhino, fox, crocodile, bear, camel, fish, frog, Snake, Tortoise'
-          },
-          {
-            audio: 'hi-animals.mp3',
-            audioOffset: 40,
-            words:
-              'Rat, Squirrel, Ant, fly, Bat, Buffalo, Butterfly'
-          },
-          {
-            audio: 'hi-animals.mp3',
-            audioOffset: 54,
-            words: 'leopard, Cow, Donkey, Mongoose, mosquito, lizard, rabbit'
-          },
-          {
-            audio: 'hi-birds.mp3',
-            words: 'crow, duck, hen, parrot, peacock, sparrow, chick, myna, Ostrich'
-          },
-          {
-            audio: 'hi-birds.mp3',
-            audioOffset: 18,
-            words: 'owl, rooster, woodpecker, eagle, sparrow, crane, flamingo, kingfisher'
-          },
-          {
-            audio: 'hi-flowers-bodyParts-kitchen.mp3',
-            audioOffset: 0,
-            words: 'rose, lotus, sunflower, marigold, jasmine, hibiscus'
-          },
-          {
-            audio: 'hi-flowers-bodyParts-kitchen.mp3',
-            audioOffset: 12,
-            words: 'ear, eye, foot, hair, hand, mouth, nose, tooth'
-          },
-          {
-            audio: 'hi-flowers-bodyParts-kitchen.mp3',
-            audioOffset: 28,
-            words: 'stove, fork, spoon, knife, pan, pot, butter'
-          },
-          {
-            audio: 'hi-fruits.mp3',
-            audioOffset: 0,
-            words:
-              'apple, grapes, banana, guava, jackfruit, mango, orange, papaya'
-          },
-          {
-            audio: 'hi-fruits.mp3',
-            audioOffset: 16,
-            words:
-              'pineapple, pomegranate, watermelon, fig, custard-apple, dates, muskmelon'
-          },
-          {
-            audio: 'hi-vegetable.mp3',
-            audioOffset: 0,
-            words:
-              'carrot, beetroot, onion, tomato, potato, cabbage, cauliflower, corn, cucumber, garlic'
-          },
-          {
-            audio: 'hi-vegetable.mp3',
-            audioOffset: 20,
-            words:
-              'ginger, pumpkin, almond, beans, bitter-gourd, brinjal, capsicum, cashewnut, chilli, coconut '
-          },
-          {
-            audio: 'hi-vegetable.mp3',
-            audioOffset: 40,
-            words:
-              'corianderleaf, curryleaf, drumstick, greenpeas, ladies-finger, mintleaf, peanut, raddish, spinach'
-          },
-          {
-            audio: 'hi-vehicles.mp3',
-            audioOffset: 0,
-            words:
-              'aeroplane, boat, bus, car, bicycle, helicopter, motorbike, ship, tractor'
-          },
-          {
-            audio: 'hi-vehicles.mp3',
-            audioOffset: 18,
-            words:
-              'train, ambulance, auto, cart, excavator, fire-engine, lorry, van'
-          },
+          audio: 'hi-animals.mp3',
+          audioOffset: 20,
+          words:
+            'hippo, rhino, fox, crocodile, bear, camel, fish, frog, snake, tortoise'
+        },
         {
-            audio: 'hi-house.mp3',
-            audioOffset: 0,
-            words: 'house, table, bed, candle, chair, shelf, baby, boy'
-          },
-          {
-            audio: 'hi-house.mp3',
-            audioOffset: 16,
-            words:
-              'girl, man, woman, ball, coat, glasses, clock, watch'
-          },
-          {
-            audio: 'hi-others.mp3',
-            audioOffset: 0,
-            words: 'sun, moon, rain, star, milk, ring, brush, bone'
-          },
-          {
-            audio: 'hi-others.mp3',
-            audioOffset: 16,
-            words: 'bell, bird, conch, five, drums, food,  fruits, king'
-          },
-          {
-            audio: 'hi-others.mp3',
-            audioOffset: 32,
-            words: 'kite, ladder, ladoo, leaf, rishi, wheel, whistle, world'
-          },
-          {
-            audio: 'hi-school.mp3',
-            audioOffset: 0,
-            words: 'Eraser, pen, pencil, ruler, bag, Scissors, sharpener, watterbottle, calculator, book, color pencils, crayons '
-          },
-          {
-            audio: 'hi-tools.mp3',
-            audioOffset: 0,
-            words: 'Axe, balance, black board, hose, plier,  microscope, needle, oven, plough, saw'
-          },
-          {
-            audio: 'hi-tools.mp3',
-            audioOffset: 20,
-            words: 'spade, Stethescope, telescope, testube, thermometer, trowel, compass, easel, hacksaw, scalpel '
-          },
-            
-            ]
+          audio: 'hi-animals.mp3',
+          audioOffset: 40,
+          words: 'sat, squirrel, Ant, fly, Bat, Buffalo, Butterfly'
+        },
+        {
+          audio: 'hi-animals.mp3',
+          audioOffset: 54,
+          words: 'leopard, cow, donkey, mongoose, mosquito, lizard, rabbit'
+        },
+        {
+          audio: 'hi-birds.mp3',
+          words:
+            'crow, duck, hen, parrot, peacock, sparrow, chick, myna, Ostrich'
+        },
+        {
+          audio: 'hi-birds.mp3',
+          audioOffset: 18,
+          words:
+            'owl, rooster, woodpecker, eagle, sparrow, crane, flamingo, kingfisher'
+        },
+        {
+          audio: 'hi-flowers-bodyParts-kitchen.mp3',
+          audioOffset: 0,
+          words: 'rose, lotus, sunflower, marigold, jasmine, hibiscus'
+        },
+        {
+          audio: 'hi-flowers-bodyParts-kitchen.mp3',
+          audioOffset: 12,
+          words: 'ear, eye, foot, hair, hand, mouth, nose, tooth'
+        },
+        {
+          audio: 'hi-flowers-bodyParts-kitchen.mp3',
+          audioOffset: 28,
+          words: 'stove, fork, spoon, knife, pan, pot, butter'
+        },
+        {
+          audio: 'hi-fruits.mp3',
+          audioOffset: 0,
+          words:
+            'apple, grapes, banana, guava, jackfruit, mango, orange, papaya'
+        },
+        {
+          audio: 'hi-fruits.mp3',
+          audioOffset: 16,
+          words:
+            'pineapple, pomegranate, watermelon, fig, custard-apple, dates, muskmelon'
+        },
+        {
+          audio: 'hi-vegetable.mp3',
+          audioOffset: 0,
+          words:
+            'carrot, beetroot, onion, tomato, potato, cabbage, cauliflower, corn, cucumber, garlic'
+        },
+        {
+          audio: 'hi-vegetable.mp3',
+          audioOffset: 20,
+          words:
+            'ginger, pumpkin, almond, beans, bitter-gourd, brinjal, capsicum, cashewnut, chilli, coconut '
+        },
+        {
+          audio: 'hi-vegetable.mp3',
+          audioOffset: 40,
+          words:
+            'corianderleaf, curryleaf, drumstick, greenpeas, ladies-finger, mintleaf, peanut, raddish, spinach'
+        },
+        {
+          audio: 'hi-vehicles.mp3',
+          audioOffset: 0,
+          words:
+            'aeroplane, boat, bus, car, bicycle, helicopter, motorbike, ship, tractor'
+        },
+        {
+          audio: 'hi-vehicles.mp3',
+          audioOffset: 18,
+          words:
+            'train, ambulance, auto, cart, excavator, fire-engine, lorry, van'
+        },
+        {
+          audio: 'hi-house.mp3',
+          audioOffset: 0,
+          words: 'house, table, bed, candle, chair, shelf, baby, boy'
+        },
+        {
+          audio: 'hi-house.mp3',
+          audioOffset: 16,
+          words: 'girl, man, woman, ball, coat, glasses, clock, watch'
+        },
+        {
+          audio: 'hi-others.mp3',
+          audioOffset: 0,
+          words: 'sun, moon, rain, star, milk, ring, brush, bone'
+        },
+        {
+          audio: 'hi-others.mp3',
+          audioOffset: 16,
+          words: 'bell, bird, conch, five, drums, food,  fruits, king'
+        },
+        {
+          audio: 'hi-others.mp3',
+          audioOffset: 32,
+          words: 'kite, ladder, ladoo, leaf, rishi, wheel, whistle, world'
+        },
+        {
+          audio: 'hi-school.mp3',
+          audioOffset: 0,
+          words:
+            'Eraser, pen, pencil, ruler, bag, Scissors, sharpener, watterbottle, calculator, book, color pencils, crayons '
+        },
+        {
+          audio: 'hi-tools.mp3',
+          audioOffset: 0,
+          words:
+            'axe, balance, black board, hose, plier,  microscope, needle, oven, plough, saw'
+        },
+        {
+          audio: 'hi-tools.mp3',
+          audioOffset: 20,
+          words:
+            'spade, stethescope, telescope, testube, thermometer, trowel, compass, easel, hacksaw, scalpel '
+        }
+      ]
     }
   ]
 };
