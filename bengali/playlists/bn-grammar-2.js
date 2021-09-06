72d965ffdae05e1464aebd61a336e8537ab5ed77
Atish Sihi <85349281+atishsihi@users.noreply.github.com>
2021-09-06 13:52:31 +0530

    Create bn-grammar-2.js

---
+++ bengali/playlists/bn-grammar-2.js
@@ -0,0 +1,52 @@
+export default {
+    // class-4 content ref-bhasapath-iv
+    label: 'বর্ণ',
+    id: 'bn-grammar-2',
+    img: 'writing',
+    list: [
+  
+      {
+        type: 'group',
+        label: 'ব্যঞ্জনধ্বনি - অল্পপ্রাণ/মহাপ্রাণ ধ্বনি',
+        id: 'classify-sound-type-1',
+        commonData: {
+          title: 'অল্পপ্রাণ ধ্বনি ও মহাপ্রাণ ধ্বনি গুলি সঠিক বক্সে রাখুন ',
+          fontSize: '2rem',
+          printTitle:
+            'Rewrite the words given at the bottom at approprate boxes.',
+          types: ['অল্পপ্রাণ ধ্বনি', 'মহাপ্রাণ ধ্বনি']
+        },
+        data: [
+              [`ক ,চ , ট , ত ,প`, `খ , ছ , ঠ , থ , ফ`],
+              [`গ , জ , ড , দ , ব`, `ঘ , ঝ , ঢ, ধ, ভ`],
+              [`ক ,চ , ট , ত ,প`, `ঘ , ঝ , ঢ, ধ, ভ`],
+              [`গ , জ , ড , দ , ব`, `খ , ছ , ঠ , থ , ফ`],
+              [`ক ,চ , ট , দ , ব`, `ঝ , ঢ, ধ , থ , ফ`],
+              [`গ , জ , ড , ত ,প`, `খ , ছ , ঝ , ঢ, ধ`],
+  
+        ]
+      },
+      {
+        type: 'group',
+        label: 'ব্যঞ্জনধ্বনি - অঘোষ/ঘোষ ধ্বনি',
+        id: 'classify-sound-type-2',
+        commonData: {
+          title: 'অঘোষ ধ্বনি ও ঘোষ ধ্বনি গুলি সঠিক বক্সে রাখুন ',
+          fontSize: '2rem',
+          printTitle:
+            'Rewrite the words given at the bottom at approprate boxes.',
+          types: ['অঘোষ ধ্বনি', 'ঘোষ ধ্বনি']
+        },
+        data: [
+              [`ক ,চ , ট , ত ,প`, `গ , জ , ড , দ , ব`],
+              [`খ , ছ , ঠ , থ , ফ`, `ঘ , ঝ , ঢ, ধ, ভ`],
+              [`ক ,চ , ট , ত ,প`, `ঘ , ঝ , ঢ, ধ, ভ`],
+              [`খ , ছ , ঠ , থ , ফ`, `গ , জ , ড , দ , ব`],
+              [`ক ,চ , ট , খ , ছ `, `ঘ , ঝ , ড , দ , ব`],
+              [`ত ,প , ঠ , থ , ফ`, `গ , জ , ঢ, ধ, ভ`],
+  
+        ]
+      },
+
+    ]
+  };
