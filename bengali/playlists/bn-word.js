00c741b3791be58ff71d6d77ea3c25e4fa175b28
Atish Sihi <85349281+atishsihi@users.noreply.github.com>
2021-09-16 12:58:24 +0530

    Update bn-word.js

--- bengali/playlists/bn-word.js
+++ bengali/playlists/bn-word.js
@@ -246,17 +246,18 @@ export default {
       data: [
         `আজ, Today
         গতকাল, Yesterday
-        আগামীকাল, Tomorrow
-        এখানে, Here
-        ওখানে, There`,
-
-        `তাড়াতাড়ি, Quikly
-        আস্তে, Slowly
-        সর্বদা, Always
+        আগামীকাল, Tomorrow`,
+        
+        `এখানে, Here
+        ওখানে, There
+        তাড়াতাড়ি, Quikly
+        আস্তে, Slowly`,
+        
+        `সর্বদা, Always
         মাঝে মাঝে, Sometime
         কখনো, Never
         প্রায়, Often
-        কদাপি, Seldom`
+        কদাপি, Seldom`,
       ]
     }
   ]
