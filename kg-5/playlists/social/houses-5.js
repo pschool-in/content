e8c64c41cf18daef8fe48ac62520556fd0356cc7
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-09-20 13:18:20 +0530

    Update houses-5.js

--- kg-5/playlists/social/houses-5.js
+++ kg-5/playlists/social/houses-5.js
@@ -112,7 +112,7 @@ export default {
           *Pucca house (Kutcha house)* is made up of stone, concrete, clay tiles, etc.
           Apartments are widely seen in *urban (rural)* areas.
           *Flat roofs (Hip roofs )*are commonly seen on commercial buildings.
-          *Eco-friendly houses* requires less energy consumption.`
+          *Eco-friendly houses (Cottages)* requires less energy consumption.`
         }
       },
       {
