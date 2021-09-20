e06d7ce846b6a39c65fd4e5498b88b2beb5a4c71
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-09-20 22:54:51 +0530

    Update lifeskills-5.js

--- kg-5/playlists/social/lifeskills-5.js
+++ kg-5/playlists/social/lifeskills-5.js
@@ -94,7 +94,7 @@ export default {
             dashWidth: 80
           },
           text: `We must know well about our talents and also our *flaws*.
-          Handling *emotions* in a right way, will help us to lead the happy life.
+          Handling *emotions* in the right way will help us lead a happy life.
           If we know our flaws in our nature, then we should try to *correct* ourselves.
           Being *stubborn* always, will lead to the problem. 
           We should able to do *balance* between the thoughts and emotions.`
