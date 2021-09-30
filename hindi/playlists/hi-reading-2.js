6112305a91b9237f051a73b4bdfd315c0179a298
Atish Sihi <85349281+atishsihi@users.noreply.github.com>
2021-09-30 13:53:49 +0530

    Hindi re-grouping

--- hindi/playlists/hi-reading-2.js
+++
@@ -1,6 +0,0 @@
-export default {
-  label: 'कहानियाँ 2',
-  id: 'hi-reading-2',
-  img: 'reading',
-  list: []
-};
--- hindi/playlists/hi-word-2.js
+++
@@ -1,6 +0,0 @@
-export default {
-  label: 'शब्द 2',
-  id: 'hi-word-2',
-  img: 'word',
-  list: []
-};
