386e396459ef5a47bc50adc4abb44f5040e83ace
Atish Sihi <85349281+atishsihi@users.noreply.github.com>
2021-10-05 16:15:35 +0530

    changed order, kept practice at last
    changed order, kept practice at last

--- hindi/curriculum/morePlaylists.js
+++ hindi/curriculum/morePlaylists.js
@@ -8,14 +8,6 @@ const more = [
     slug: 'hi-grammar-4'
   },
   {
-    label: 'व्याकरण 3',
-    slug: 'hi-grammar-practice'
-  },
-  {
-    label: 'वर्ण 2',
-    slug: 'hi-letter-practice'
-  },
-  {
     label: 'चित्र',
     slug: 'hi-picture-2'
   },
@@ -32,21 +24,29 @@ const more = [
     slug: 'hi-sentence-4'
   },
   {
-    label: 'वाक्य 3',
-    slug: 'hi-sentence-practice'
-  },
-  {
     label: 'शब्द',
     slug: 'hi-word-4'
   },
   {
     label: 'शब्द 2',
     slug: 'hi-word-5'
+  },  
+  {
+    label: 'व्याकरण 3',
+    slug: 'hi-grammar-practice'
+  },
+  {
+    label: 'वर्ण 2',
+    slug: 'hi-letter-practice'
   },
   {
     label: 'शब्द 3',
     slug: 'hi-word-practice'
   }
+  {
+    label: 'वाक्य 3',
+    slug: 'hi-sentence-practice'
+  },
 ];
 
 export default {
