43c9884f2baebeec9f45a0cc5863b2d4743a0455
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2021-10-06 14:41:57 +0530

    Update hi-letter.js

--- hindi/playlists/default/hi-letter.js
+++ hindi/playlists/default/hi-letter.js
@@ -524,7 +524,7 @@ export default {
     {
       type: 'group',
       label: 'स्वर बनाम व्यंजन ',
-      locked: 'true',
+      lockAfter:4,
       id: 'classify-letter-type-hi',
       commonData: {
         title: 'व्यंजन और स्वर सही बॉक्स में डालें ',
@@ -556,7 +556,7 @@ export default {
       id: 'compound-7',
       type: 'match',
       label: 'व्यंजन और स्वर संयोजन (क - घ)',
-      locked: 'true',
+      lockAfter:4,
       commonData: {
         title: 'सही अक्षर का मिलान करें',
         fontSize: '2rem'
