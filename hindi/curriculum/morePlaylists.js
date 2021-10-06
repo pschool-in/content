f5578ba7ccfb643b8e95486468d9d66b6f181e5c
Atish Sihi <85349281+atishsihi@users.noreply.github.com>
2021-10-06 16:44:37 +0530

    Update morePlaylists.js

--- hindi/curriculum/morePlaylists.js
+++ hindi/curriculum/morePlaylists.js
@@ -1,4 +1,29 @@
 const more = [
+
+  {
+    label: 'चित्र',
+    slug: 'hi-picture-2'
+  },
+  {
+    label: 'शब्द',
+    slug: 'hi-word-4'
+  },
+  {
+    label: 'शब्द 2',
+    slug: 'hi-word-5'
+  }, 
+  {
+    label: 'वाक्य',
+    slug: 'hi-sentence-3'
+  },
+  {
+    label: 'वाक्य 2',
+    slug: 'hi-sentence-4'
+  }, 
+  {
+    label: 'कहानियाँ 2',
+    slug: 'hi-reading-4'
+  },
   {
     label: 'व्याकरण',
     slug: 'hi-grammar-3'
@@ -8,45 +33,21 @@ const more = [
     slug: 'hi-grammar-4'
   },
   {
-    label: 'चित्र',
-    slug: 'hi-picture-2'
-  },
-  {
-    label: 'कहानियाँ 2',
-    slug: 'hi-reading-4'
-  },
-  {
-    label: 'वाक्य',
-    slug: 'hi-sentence-3'
-  },
-  {
-    label: 'वाक्य 2',
-    slug: 'hi-sentence-4'
-  },
-  {
-    label: 'शब्द',
-    slug: 'hi-word-4'
-  },
-  {
-    label: 'शब्द 2',
-    slug: 'hi-word-5'
-  },  
-  {
-    label: 'व्याकरण 3',
-    slug: 'hi-grammar-practice'
-  },
-  {
-    label: 'वर्ण 2',
+    label: 'वर्ण अधिक अभ्यास',
     slug: 'hi-letter-practice'
   },
   {
-    label: 'शब्द 3',
+    label: 'शब्द अधिक अभ्यास',
     slug: 'hi-word-practice'
   },
   {
-    label: 'वाक्य 3',
+    label: 'वाक्य अधिक अभ्यास',
     slug: 'hi-sentence-practice'
-  }
+  },
+  {
+    label: 'व्याकरण अधिक अभ्यास',
+    slug: 'hi-grammar-practice'
+  },
 ];
 
 export default {
