bbc19f83b2892eafd6bb514a73bd321799a414fc
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-10-18 23:13:36 +0530

    Update kye-conversation-1.js

--- knowYourEnglish/playlists/kye-conversation-1.js
+++ knowYourEnglish/playlists/kye-conversation-1.js
@@ -33,6 +33,140 @@ export default {
 “Drink your coffee, will you?”
 Coffee should be black as hell, strong as death and sweet as love. Turkish proverb`
       }
+     },
+    {
+      type: 'passage',
+        label: 'Of werewolves and silver bullets',
+        id: '200',
+        data: {
+          title: `Of werewolves and silver bullets`,
+          text: `Where did the word ‘werewolf’ come from?
+          “Hey, good to see you! How are you feeling? Have you recovered completely from …”
+ “Yes, I have, thank you! Luckily, it wasn’t COVID. It was only some sort of viral infection.”
+ “Your parents must have been relieved. So, what did you do while you were recovering?”
+ “Slept a lot, actually. When I was in the mood, watched horror movies. Some had vampires and werewolves in them. Tell me, where does the word ‘werewolf’ come from?”
+ “A werewolf, as you a know, is a man who turns into…”
+ “A werewolf is a man who turns into a wolf on a full moon night. I know that. But…”
+“I’m coming to your question. In Old English, the word ‘were’ meant ‘man’, and wolf, which in the past was spelt ‘w..u..l..f’, meant…”
+“It meant ‘wolf’, of course! So, the word ‘werewolf’ literally means ‘man wolf’.”
+“Exactly! This use of the word ‘were’ to mean ‘man’ is now found in only a few words. For example, some dictionaries include the word ‘werecat’. It refers to…”
+“Is a ‘werecat’ like a werewolf? A man who turns into a cat, instead of a wolf, every now and then?”
+“Very good. That’s exactly what a ‘werecat’ is. I understand that in this case, the man could turn into a domestic cat or any of the animals that are part of the cat family.”
+“Meaning that it’s possible for the man to turn into a lion?”
+“Yes. It could be a lion, tiger or leopard. Any one of those animals.”
+Magic solution
+“While we are on the subject of cats, at yesterday’s meeting, did any member come up with a simple solution to the stray cat problem we’ve been having lately?”
+“There’s no silver bullet for that particular problem, I’m afraid.”
+“Silver bullet? What are you talking about?”
+“You’ve watched a lot of movies about werewolves, haven’t you? So tell me, what’s the only thing that can kill a werewolf?”
+“It’s supposed to be a silver bullet. At least, that’s what they use in the movies – a bullet made of silver.”
+“That’s right! One of the few things that can kill a werewolf or a witch is a silver bullet.”
+“Good grief, are you telling me you’re going to use silver bullets to kill the strays?”
+“Not at all! The expression ‘silver bullet’ can be used in everyday contexts as well. When you say that something is the silver bullet for a problem, what you’re suggesting is that it’s a simple solution to a long-standing or a complicated one.”
+“In other words, it’s a quick solution and it works like magic.”
+“Well, I guess you could say that. Here’s an example. The pharmaceutical company that Sudha is working for is hoping to find a silver bullet that will cure all forms of cancer.”
+“Maybe, you should buy shares in that company. How about this example? The new rules that have been implemented are not a silver bullet. They will not solve the drug problem on campus.”
+“You’re right! They will not solve it. I don’t think there is a silver bullet for that problem.”
+Education is the silver bullet to improve this nation’s standing worldwide…and our teachers know that. Solomon Ortiz`
+      }
+    },
+    {
+      type: 'passage',
+        label: 'Keep the count',
+        id: '300',
+        data: {
+          title: `Keep the count`,
+          text: `“Come on in! Haven’t seen you in a while. Have you been busy?”
+“Extremely! Haven’t you been reading the forwards I’ve been sending you all week?”
+“Sorry, no! I generally make it a point not to waste my time reading a forward.”
+“We’re staging a protest in front of the CM’s office on Friday. We’d like you to be there. Everyone believes it’s going to be a huge success.”
+“The success or failure of the protest is going to depend on how many boots you manage to have on the ground. You’ll need all the…”
+“Boots on the ground? What are you talking about?”
+“The expression was first used to refer to the soldiers who actually participated in a military campaign or operation.”
+“In other words, you’re talking about the soldiers who actually fought.”
+“That’s right! We’re not talking about the Navy personnel at sea or the Air Force pilots. We’re talking about those men and women who were present in the war zone and actively participated in the fighting. The Defence Minister said that, in order to put an end to the recent skirmishes along the border, we require more boots on the ground.”
+Physically present
+“How about this example? According to our boots on the ground, 12 militants were shot dead last night while attempting to cross the border.”
+“Sounds good! America no longer wants to have boots on the ground in Afghanistan.”
+“So, when you said that we should have boots on the ground for the protest, were you suggesting that we should ask soldiers to join us?”
+“Not at all. The expression ‘boots on the ground’ is now being used in everyday contexts as well. All the volunteers, for example, who help political parties during the time of election can be referred to as boots on the ground. These could be people who go door to door to request everyone to vote for their candidate or party.”
+“Or they could be the people who call you up and ask for your vote.”
+“That’s right. If you want your organisation to function efficiently, you need fewer boots on the ground.”
+“Only a small team is involved in planning the protest. But the good news is we have received a positive response to all our messages sent via email and WhatsApp. I think we’ll have quite a few boots on the ground on Friday.”
+“I wouldn’t be so sure. For all you know, many of those who have said ‘Yes’ could be mere clicktivists.”
+“Clicktivists? Never heard that word before. What does it mean?”
+“It’s a combination of ‘click’ and ‘activist’. A clicktivist is someone who supports political or social causes through online petitions. He is very active on the various social media. There is very little to suggest that he is committed to the various causes he claims to support.”
+“In other words, he is not really an activist!”
+“He is an activist, actually — an armchair activist! Never really participates in the protests that he claims to support online. But more about this word later.”
+“We are not retreating — we are advancing in another direction.” General Douglas MacArthur`
+      }
+    },
+    {
+      type: 'passage',
+        label: 'Of shirts and apron strings',
+        id: '400',
+        data: {
+          title: `Of shirts and apron strings`,
+          text: `“Good Lord! Aren’t you ready yet? We’re supposed to be there in another 20 minutes!”
+“I got a call some ten minutes ago saying that the event has been postponed to tomorrow.”
+“Then why didn’t you let me know? You could have called, you know. You always…”
+“Hey, I did call you. Several times, in fact. So, keep your shirt on.”
+“Keep my shirt on? What are you talking about? I’m wearing a kurta!
+“That’s true. Anyway, when you tell someone to keep his shirt on, you’re telling him not to get upset or angry. The expression is frequently used in informal contexts.”
+“In other words, it’s another way of asking the person to chill. To calm down.”
+“Exactly! Keep your shirt on, kids. I’m sure the waiter will bring the ice cream soon.”
+“I’m told that during the negotiations some of the members from the Union had a tough time keeping their shirt on. How’s that?”
+“Sounds good. The kids were playing a practical joke on you, Ram! No need to get upset. Keep your shirt on!”
+“But tell me, what’s the connection between a shirt and not getting angry?”
+“Well, in the old days, when two people decided to fight with their fists, they usually preferred to take their shirt off.”
+“Was it to scare the other guy?”
+“No, shirts were expensive those days. People didn’t own too many. So, removing the shirt was one way of ensuring that it didn’t get torn or dirty in the fight. Whenever a man removed his shirt in public…”
+“It meant he was getting ready to fight. Makes sense, I guess. Did you hear the news about Rahul? He’s got a great job in Kolkata. He joins next month.”
+“There’s no way Rahul is going to Kolkata. He’s tied to his mother’s apron strings.”
+“Tied to his mother’s apron strings? Is that your way of saying that Rahul does whatever his mother tells him to?”
+“It’s a little more serious than that. It means that Rahul is controlled or dominated by his mother. He’s a puppet in her hands. The idiom is mostly used with men who are dominated by a woman. Here’s an example. Rekha wasn’t keen on marrying a man who was still tied to his mother’s apron strings.”
+“Many of the cousins in my family are tied to their wife’s apron strings.”
+“That’s true in most households. The expression can be used with women as well. My sister, for some strange reason, is tied to Aparna’s apron strings. I’ve told her repeatedly she needs to make her own decisions.”
+“But tell me, why apron strings? Why not something else?”
+“I understand that in the past, a housewife sometimes tied a cord around a toddler; this cord she would then fasten to her apron strings. This was her way of making sure that the child stayed close to her, and didn’t get into any mischief.”
+“A practical thing to do!”
+A man who has never made a woman angry is a failure in life. Christopher Morley`
+    }
+},
+{
+  type: 'passage',
+    label: 'Get your act together',
+    id: '500',
+    data: {
+      title: `Get your act together`,
+      text: `“Are you all set for next week’s trekking trip? You must be really…”
+“I’m not really sure if we’ll be going. Two of my friends dropped out when they came to know they would be spending all four days outdoors, and that there would be no hot water for their bath.”
+“I know, it gets really cold over there. But tell your friends, if they wish to have a once-in-a-lifetime experience, they need to cowboy up. They need to…”
+“Cowboy up? I’ve never heard that expression before. What does it mean?”
+“It’s slang, which is mostly used in informal contexts. When you tell someone to cowboy up, you’re asking him to toughen up.”
+“In other words, be tough like a cowboy.”
+“I guess you could say that. You want the person to be a man, and try his best to overcome any obstacle that may come his way.”
+“So, if you are constantly complaining about something, can I tell you to cowboy up?”
+“You certainly can. That would be your way of saying, ‘Stop whining and do your job’! Here’s another example. In the movie that I saw yesterday, a retired police officer cowboys up to track down a serial killer.”
+“When my friend complained about online classes, I told her if she wished to graduate this semester, she needed to cowboy up, and do the best she could.”
+“Since your friend is a girl, it would be more appropriate to say, ‘cowgirl up’!”
+Building anticipation
+“Really! That’s interesting! Tell me, how is the word w…h...e…t pronounced?”
+“One simple way is to pronounce it like the word ‘wet’. Any idea what it means?”
+“Not a clue. I’ve heard people talking about whetting their appetite.”
+“Whet literally means to sharpen. In the past, people sharpened their knives and swords by rubbing them against a stone. This process was called ‘whetting’, and the stone which was used to sharpen the knife was called a ‘whetstone’.”
+“I see. Is it okay to say, I spent the morning helping my father whet an old knife?”
+“Yes, it’s a possible sentence. But I seriously doubt if your father would ever ask you to whet a knife. Nowadays, the word ‘whet’ is mostly used in the expression ‘whet one’s appetite’.”
+“I see. Does it mean to sharpen or increase one’s appetite for something?”
+“Very good! All those ads that are broadcast before the IPL season begins are meant to whet our appetite.”
+“In other words, they are meant to increase our interest in the game of cricket.”
+“Exactly! Here’s another one. I don’t ….”
+“Let me. The starters that Uma ordered whet our appetite for the main course.”
+“That’s a good example. It’s also possible to talk about whet one’s curiosity, pleasure, and so on.”
+“I see. I saw the trailer of my favourite director’s latest horror movie yesterday. It has really whet my curiosity.”
+“No horror movies for me, please. Life in this pandemic is horrible enough.”
+“Adopt the pace of nature: her secret is patience.” Ralph Waldo Emerson`
+}
     }
   ]
 };
