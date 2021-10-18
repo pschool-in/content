dfd2a382f30003753085246a5de5349a5fd3475a
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-10-18 23:12:05 +0530

    Update kye-qa-1.js

--- knowYourEnglish/playlists/kye-qa-1.js
+++ knowYourEnglish/playlists/kye-qa-1.js
@@ -18,6 +18,137 @@ Dictionaries list two ways of pronouncing this French expression. Some people ma
 It’s about time you had a tête-a-tete with your friend. He’s out of control. I thought it would be a romantic tête-a-tete. But she had invited her parents.
 “Wear a smile and have friends; wear a scowl and have wrinkles.” George Eliot`
       }
-    }
-  ]
-};
+    },
+      {
+        type: 'passage',
+        label: 'A spy or a decoy?',
+        id: '200',
+        data: {
+          title: `A spy or a decoy?`,
+          text: `What does ‘ stool pigeon’ mean? Funny in a strange way?
+          What is the meaning and origin of ‘stool pigeon’?
+  The expression is mostly used to show disapproval, and most dictionaries list it as being slang. When you refer to someone as being a ‘stool pigeon’, what you are suggesting is that he is a police informant. This individual is usually a criminal whom the police use to keep track of the activities of gangs. He is a spy whose information makes it possible for those in authority to arrest criminals. Such a person is also referred to as a ‘stoolie’. The word ‘pigeon’, by the way, consists of only two syllables — the first rhymes with ‘fridge’ and ‘bridge, and the final ‘eo’ sounds like the ‘a’ in ‘china’. The word is pronounced ‘PIJ-in’ with the stress on the first syllable.
+  The boss doesn’t trust Jai anymore. He fears that he may be a stool pigeon.
+  If the prisoners find out I’m the warden’s stool pigeon, they won’t hesitate to kill me.
+  There are several theories about the origin of this expression. I will deal with only one here. Most scholars agree that the word ‘stool’ in this context does not refer to the piece of furniture found in most houses; the one that peons used to sit on in government offices. Many believe that the word comes from ‘stola’, meaning the base or stump of a tree. In the past, hunters used to lay traps for hawks and other birds of prey by placing a pigeon on a tree stump. The pigeon was tied to the tree stump and, when a bird of prey landed on the stump, the hunter threw a net over it. The pigeon served as a decoy to entrap other birds; which is why one of the meanings of the expression ‘stool pigeon’ is decoy.
+  What is the difference between ‘funny ha ha’ and ‘funny peculiar’?
+  This is an expression mostly used in informal contexts. Those of us who are in the habit of cracking jokes are fully aware that telling one is fraught with danger, for we don’t always get the kind of response that we are looking for. There are times when a joke has the listeners rolling in the aisles and, at times, it leaves them cold. The joke lays an egg — there is no reaction from the audience. It doesn’t make anyone laugh. The word ‘funny’ has two very different meanings in English. When you say, ‘She’s a funny girl’, it could mean she is someone who has a good sense of humour. It could also mean she is someone who is peculiar or strange. Therefore, when you ask someone before he tells a joke, ‘Is it funny ha ha or funny peculiar?’ what you would like to know is whether the joke will make you laugh (ha ha) or just leave you feeling strange or weird.
+  “Sujatha’s husband is a funny guy.” “Funny ha ha or funny peculiar?”
+  Some of his jokes were funny ha ha. But I found most of them to be funny peculiar.
+  “Criticisms are like homing pigeons. They always return home.” Dale Carnegie`
+        }
+    },
+    {
+      type: 'passage',
+      label: 'Not a pleasant experience',
+      id: '300',
+      data: {
+        title: `Not a pleasant experience`,
+        text: `What is the difference between ‘vomit’ and ‘retch’? 
+  I am sure that all of us have experienced both sometime or the other. Neither is a pleasant experience. When you ‘vomit’, you actually throw up. In other words, you eject, usually rather forcefully, the contents of your stomach through the mouth — making all kinds of strange sounds while you are doing so.
+  After a night of binge drinking, Akash spent the morning vomiting.
+  The sight of all the blood made the policeman vomit.
+  Just like ‘throw up’, it is also possible to say, ‘vomit (something) up’.
+  By early morning, the sea had turned rough. Poor Radha vomited her breakfast up.
+  The dog vomited up the sandwich he’d found on the street.
+  The word ‘retch’ rhymes with the words, ‘sketch’ and ‘fetch’. Retching is very similar to vomiting; you exhibit the same physical reaction as when you are vomiting. Your stomach begins to feel funny, and you begin to make the kinds of sounds that you normally make when you are about to throw up. Physically, there is every indication that you are going to vomit, but nothing actually comes out of your mouth. When you walk into a public bathroom, the foul smell can sometimes make you retch — you come very close to vomiting but, somehow, you manage not to. People also retch when they are forced to eat something that they do not like. The word comes from Old English ‘hracan’, and the original meaning was ‘to clear one’s throat’ or ‘spit’.
+  The smell of stale vomit made Jasmine retch.
+  How can I become a doctor when the sight of blood makes me retch?
+  How is the word ‘facetious’ pronounced?
+  The word consists of three syllables. The ‘a’ in the first and the ‘ou’ in the final are pronounced like the ‘a’ in ‘china’. The ‘ce’ in the second syllable is pronounced like the word ‘see’, and the following ‘ti’ sounds like the ‘sh’ in ‘ship’ and ‘shoe’. The word is pronounced ‘fe-SEE-shes’, with the stress on the second syllable. It comes from the Latin “facetus” meaning “witty”. In English, the word is mostly used to show disapproval. A facetious individual, in his desire to be witty, ends up making people angry or annoyed by his silly or inappropriate comments about a serious matter.
+  I’m in no mood to listen to your facetious remarks.
+  You cannot afford to sound facetious during an interview.
+  Is it okay to say, ‘The man was shot to death’?
+  While it is becoming increasingly common to hear even native speakers of English utter such a sentence, careful users of the language would seriously object to it. They would argue that one doesn’t usually shoot someone to death, one merely shoots someone dead. In their opinion, the correct sentence would be,
+  ‘The man was shot dead’.
+  The police shot dead the three escaped convicts.
+  “To write a diary every day is like returning to one’s own vomit.” Enoch Powell`
+      }
+    },
+    {
+      type: 'passage',
+      label: 'Is it a simple matter or a simplistic one?',
+      id: '400',
+      data: {
+        title: `Is it a simple matter or a simplistic one?`,
+        text: `What is the difference between ‘simple’ and ‘simplistic’?
+ Although the two words mean something quite different, there is a tendency among some people to use them interchangeably. It is, however, wrong to do so. A casual glance at the dictionary suggests that ‘simple’ has several different meanings, and in most contexts, the use of the word suggests approval. When you say that someone has found a simple solution to a problem, what you are suggesting is that he has found a neat or an easy solution. It is a good solution because it is not at all complicated, and it is one that can be easily followed by others, if need be.
+ You need to find a simple way of dealing with the problem.
+ It’s not always easy to come up with simple solutions.
+ ‘Simplistic’, unlike ‘simple’, is normally used to suggest disapproval. When you say that someone’s solution to a problem is rather simplistic, what you are suggesting is that it is oversimplified. The individual has not factored in all the complexities of the problem — it could be because he hasn’t really understood the problem or that he has deliberately chosen to ignore some of the important aspects. As a result, the solution he offers may be rather naïve — one that is unlikely to solve the problem at hand.
+ Most of us give simplistic solutions to the political problems in our country.
+ Vinod has a very simplistic point of view about the current reservation policy.
+ In the past, the word ‘simple’ was used as a noun to refer to medicinal plants and herbs. A person who went about collecting such plants and herbs was called a ‘simplist’. The word seems to have taken on an additional meaning over the years. The Collins Dictionary defines simplist as “a person given to simplistic explanations, theories, etc.”
+Should I use a full stop or a question mark when a sentence begins with ‘I wonder’? 
+During the various phases of our life, we wonder about many things. When we are in school, we wonder if we will do well in the coming exams. Once we graduate, we wonder if we will get a good job. The fact that I haven’t ended the two preceding sentences with a question mark answers your question about ‘I wonder’, doesn’t it? Books on grammar and usage suggest that any sentence beginning with ‘I wonder’ is a statement, and not a question — and being a statement, it should end with a full stop or a period, and not a question mark. The problem is, however, when you ask someone, ‘I wonder if you could help me’, you are in a roundabout or an indirect way asking him a question — as to whether he can help you or not. Such questions are called indirect questions, and they never end with a question mark.
+I wonder if I should have ice cream for dessert.
+I wonder if Sudesh will recognise me after all these years.
+“Do you wonder if there’s life after death? Trespass here and find out.” Anonymous`
+      }
+    },
+    {
+      type: 'passage',
+      label: 'Are you a fan or a fanatic?',
+      id: '500',
+      data: {
+        title: `Are you a fan or a fanatic?`,
+        text: `What is the difference between ‘fan’ and ‘fanatic’?
+Considering the fact that ‘fan’ is actually the abbreviated form of ‘fanatic’, there should be little or no difference in meaning between the two words. This, however, is not the case. People in general prefer to be called a ‘fan’ rather than a ‘fanatic’. Both words suggest that the individual is an enthusiastic or a strong supporter of something. When you say that you are a fan of the Chennai Super Kings or the Mumbai Indians cricket team, what you are suggesting is that you enthusiastically support the team, and enjoy watching them play. During the course of a match, you may cheer every time one of your players does well, and boo the star player from the opposition when he walks out to bat. These are some of the ways you show your support for your team. Similarly, when you claim to be a fan of Roger Federer or Virat Kohli, you are stating that you are a great admirer of the individual. You may have lengthy arguments with your friends as to why you believe that he is the best player of all time. The word ‘fan’, in most cases, has a positive connotation.
+I had a really good time talking to a few Rajesh Khanna fans.
+Radhika is a real science-fiction fan. She’s read all of Asimov’s novels.
+Unlike ‘fan’, the word ‘fanatic’ is usually used to show disapproval. A fanatic is someone who is not just enthusiastic, but excessively enthusiastic about something. This individual is single minded, has very strong beliefs, and at times resorts to violence to achieve his goal. The man who stabbed the tennis star, Monica Seles, in Hamburg in 1993, was definitely a fanatic and not a fan. The word is frequently used nowadays in the context of religion and politics; people who have extreme beliefs are said to be fanatics.
+Please keep your distance from him. The man is a religious fanatic.
+Chethan, like his father, is a cricket fanatic.
+The fact that the word is used in the context of religion should not come as a surprise because ‘fanatic’ comes from the Latin ‘fanum’ meaning ‘of or relating to a temple’. With the passage of time, the word began to refer to the pious people who frequented temples. Later, it began to be associated with the frenzied behaviour that very devout individuals exhibited when they were possessed by the divine. Nowadays, fanatic is used with anyone who is excessively enthusiastic about something — not necessarily with the devout. The first ‘a’ in ‘fanatic’ is like the ‘a’ in ‘china’, while the second is pronounced like the ‘a’ in ‘fan’, ‘ban’ and ‘tan’. The word is pronounced ‘fe-NA-tik’ with the stress on the second syllable. Many people in India put it on the first.
+“The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts.” Betrand Russell`
+      }
+    },
+    {
+      type: 'passage',
+      label: 'Play up or play down?',
+      id: '600',
+      data: {
+        title: `Play up or play down?`,
+        text: `What is the meaning of the expression ‘play up’ in the sentence, ‘The twins played up all morning’?
+The expression ‘play up’ has several meanings. When a newspaper or a news channel ‘plays up’ a story, what it is doing is giving the story undue importance. In other words, it is exaggerating the importance of the story. Political parties, across the world, do this all the time.
+For a few days, the mystery surrounding actress Sridevi’s death was played up by the various media.
+If you wish to win this election, you need to play up your impressive achievements.
+The opposite of ‘play up’ is ‘play down’. When you ‘play down’ your contribution to something, you are minimising it; you are not hyping it up. You are not giving the contribution the importance it merits; you are, for reasons best known to you, deliberately making it seem less important than what it is. You may even be relegating it to the background.
+The Chief Minister is trying to play down the corruption in his cabinet.
+The hospital administration played down the seriousness of the virus.
+‘Play up’ can also be used to mean to cause pain or problems. When you say that your computer is playing up, what you are suggesting is that it is causing problems because it is not working like it is supposed to. When your headache or rheumatism ‘plays you up’, it is causing you a lot of pain - preventing you from leading a normal life - at least, temporarily.
+Our old TV is playing up again. It’s time we got a new one.
+If you ask Dilip to help you with anything, he’ll start telling you how his rheumatism is playing him up.
+As for your question, in British English, the expression ‘play up’ has a very different meaning — especially, in informal contexts. When children behave badly, they are said to be ‘playing up’.
+The Principal wants us to meet her tomorrow. Our daughter has been playing up in school again.
+I need to lie down for a while. The twins played up all morning.
+Is it okay to say, ‘She was discouraged to go to the park’?
+The word ‘encouraged’ can be followed by ‘to’. For example, we can say, ‘The children were encouraged to go to the park’ and ‘Sheba’s parents encouraged her to go abroad to do her PhD’. ‘Discouraged’, on the other hand, is not followed by ‘to’. When the word is used to mean to prevent someone from doing something or dissuade someone, it is usually followed by ‘from’. One is always ‘discouraged from doing something’ — and not ‘discouraged to do something’.
+His wife discouraged Ram from going out with his friends. The children were discouraged from playing cricket in the park.
+“Discouragement and failure are two of the surest stepping stones to success.” Dale Carnegie`
+      }
+    },
+    {
+      type: 'passage',
+      label: 'No soft corner for the covidiot',
+      id: '700',
+      data: {
+        title: `No soft corner for the covidiot`,
+        text: `What is the difference between ‘covidiot’ and ‘vidiot’?
+Of the two, ‘covidiot’ is a relatively new word. ‘Vidiot’ is a combination of two words: ‘video’ and ‘idiot’. According to the Oxford English Dictionary, the word has been around since the 1940s. It became a part of everyday vocabulary, however, only in the 1980s, when watching videos at home became a popular way of spending time. A ‘vidiot’ is someone who spends a lot of his/her time watching videos, without really worrying about the quality of what he/she is watching. This idiot watches indiscriminately; he/she watches whatever he/she can lay his/her hands on. The word can also be used to refer to someone who watches a lot of TV and spends a lot of time playing video games. The stress in this word is on the first syllable -VI-di-et.
+I’m sure all the vidiots out there are looking forward to seeing this movie.
+During weekends, I turn into a vidiot.
+‘Covidiot’, a word that was coined last year, is a combination of Covid and idiot. The covidiot is an idiot who does everything that he/she is not supposed to, when in public. He/she walks around without a mask, does not maintain social distance, and chooses not follow the other norms that we are supposed to in these trying times. The word also suggests that the individual is someone who believes that stores will run out of goods soon, and therefore buys things in bulk, and hoards them. The main stress, in this case, is on the second syllable — co-VI-di-et.
+There were many covidiots in the supermarket today.
+I made the mistake of getting into an argument with a covidiot.
+Is the expression ‘to have a soft corner for someone’ an Indianism?
+Yes, it is. Native speakers of English tend to say, ‘soft spot’ and not ‘soft corner’. When you say that you have a soft spot for someone, what you are suggesting is that you like the individual very much. You may not be able to explain the reason for your fondness; all that you know is that you are fond of the individual, and you don’t really care if your feelings are not reciprocated. In fact, the individual may be unworthy of your affection. So, is it wrong to say, ‘soft corner’? A native speaker may not immediately understand what you are trying to say, but within India, most people will.
+You’ll never say anything against Seema because you have a soft spot for her.
+He has a soft spot for feel good movies.
+What do you call a person who plays the flute?
+Americans call him ‘flutist’— the ‘flut’ is pronounced like the word ‘flute’. The British, on the other hand, call a flute player, ‘flautist’. The ‘au’ sounds like the ‘aw’ in ‘saw’, ‘jaw’ and ‘law’.
+Of course, I’m in shape. Round is a shape. – Anonymous`
+      }
+       }
+     ]
+  };
