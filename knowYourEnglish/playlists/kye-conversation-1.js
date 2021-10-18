e11aa0fb5b33365f92d098f596fe978ba1a9181e
Rethna <rethna2@gmail.com>
2021-10-18 13:01:19 +0530

    added kye

---
+++ knowYourEnglish/playlists/kye-conversation-1.js
@@ -0,0 +1,38 @@
+export default {
+  label: 'Know Your English',
+  id: 'kye-conversation-1',
+  list: [
+    {
+      type: 'passage',
+      label: 'Drink up, will you?',
+      id: '100',
+      data: {
+        title: `Drink up, will you?`,
+        text: `“Here’s your coffee. Better drink it before it gets cold. So, how was your weekend?”
+“It was pretty good, actually. Managed to complete a couple of assignments, and also managed to watch a couple of really good movies.”    
+“Looks like you had a better weekend than I did! Spent most of Saturday cleaning the cupboards. Found a lot of junk to get rid of. On Sunday, I visited Hari and tried to get him to take me to lunch. But you know how close-fisted he is.”  
+“Close-fisted? Does it mean someone who doesn’t like to spend money?”
+“It means stingy. A close-fisted person is someone who hates spending money. He’s a miser. Other words that have more or less the same meaning are ‘tight-fisted’ and ‘hard-fisted’.”
+“In other words, a ‘tight-fisted’ person holds on to his money very tightly.”
+“That’s right! He doesn’t like to let go of it. My mother always told me that my uncle was tight-fisted. But when I got married, he gave me a cheque for one lakh.”
+“Wow! How about this example? My brother is fairly well to do, but there’s no point in going to him for money. He’s close-fisted.”
+“Sounds good. Unlike ‘tight-fisted’ and ‘close-fisted’, the word ‘hard-fisted’ is considered rather old fashioned. My former boss had the reputation of being hard-fisted. Is there something wrong with the coffee? Does it…”
+“The coffee is fine. Why do you ask?”
+“You’ve been nursing it for a pretty long time. You haven’t really….”
+“Nursing my coffee? Doesn’t nurse mean to take care of someone who is ill or is not feeling very well? For example, the old man was nursed back to health by his neighbour.”
+“That’s a good example! The children nursed the sick puppy for several weeks.”
+“I understand that example. But tell me, how I can make a cup of coffee feel better? I mean, how can I nurse a cup of coffee?”
+“You see, the word ‘nurse’, when used as a verb, has several different meanings. One of them, as you have rightly pointed out, is to take care of someone who is ill. It can also be used to mean to drink something very slowly or…”
+“In other words, when you nurse a cup of coffee or tea, you take a long time to drink it.”
+“Exactly! Sometimes, you may not even finish drinking it. You may just hold the cup for a long time without really taking a sip from it. At the party, Ajay sat in a corner nursing a glass of beer.”
+“Meaning that Ajay sat with the glass of beer for a long time. He didn’t drink anything else while he was nursing it.”
+“I guess you could say that.”
+“My friend Dilip is nothing like Ajay. Whenever he goes to a party, he gulps down an entire glass at one go. There’s no question of nursing a glass of beer.”
+“I have a few friends who are like that. But most of mine nurse their drink — especially when they go to a bar.”
+“Maybe it’s because your friends are tight-fisted! They don’t want to spend too much money on their drinks!”
+“Drink your coffee, will you?”
+Coffee should be black as hell, strong as death and sweet as love. Turkish proverb`
+      }
+    }
+  ]
+};
---
+++ knowYourEnglish/playlists/kye-qa-1.js
@@ -0,0 +1,23 @@
+export default {
+  label: 'Know Your English Q & A',
+  id: 'kye-qa-1',
+  list: [
+    {
+      type: 'passage',
+      label: 'Let’s have a tête-a-tete',
+      id: '100',
+      data: {
+        title: `Let’s have a tête-a-tete`,
+        text: `Is there a word for someone who never smiles or laughs?
+Yes, there is! The word that you are looking for is ‘agelast’. It consists of three syllables, and not two as the spelling suggests. The ‘a’ in the first and third, sounds like the ‘a’ in ‘ant’, ‘pant’ and ‘chant’, while the ‘g’ is pronounced like the ‘j’ in ‘jam’, ‘juice’, and ‘jump’. The ‘e’ is like the ‘i’ in ‘bit’, ‘hit’ and ‘kit’. This rather formal word is pronounced ‘A-ji-last’ with the stress on the first syllable. It comes the Greek ‘agelostos’ meaning ‘not laughing, gloomy’. When you refer to a person as being an ‘agelast’, what you are suggesting is that he is someone who doesn’t smile; he has absolutely no sense of humour.
+He tried telling jokes to my boss — a confirmed agelast. Not many people will be familiar with this rather formal word. ‘Joyless’, ‘unsmiling’, ‘gloomy’, ‘glum’, are more common than ‘agelast’.
+What is the opposite of Eve-teasing?
+When men harass women in public places by making comments about them, we refer to it as Eve-teasing. So, common sense suggests that when the roles are reversed — when women harass a man — it should be called ‘Adam-teasing’. It makes sense, for Adam and Eve always go together! Unfortunately, if you look up ‘Adam-teasing’ in any standard dictionary, you are unlikely to find the term listed. You may have better luck with ‘Eve-teasing’, for many dictionaries do include it. What is of interest, however, is that those that do make it very clear that it is an example of Indian English — that the term is used only in India. It is not found in native varieties of English. ‘Eve teasing’, like the word ‘prepone’, is one of the many words that we Indians have contributed to the English language. Considering the fact that the term ‘Adam-teasing’ is frequently used in the Indian media, chances are, that just like ‘Eve-teasing’, it too will soon find a place in standard English dictionaries – perhaps, in a matter of few years. What we in India call ‘Eve-teasing’ and ‘Adam-teasing’, native speakers of English usually refer to it as sexual harassment. Some people believe that the term ‘Eve-teasing’ trivialises the seriousness of the problem.
+How is the word ‘tete-a-tete’ pronounced? 
+Dictionaries list two ways of pronouncing this French expression. Some people make the word ‘tete’ rhyme with ‘late’, ‘hate’ and ‘mate’, while others choose to rhyme it with ‘met’, ‘let’ and ‘set’. The ‘a’ can be pronounced like the ‘a’ in ‘act’ and ‘ant’ or like the ‘a’ in ‘path’ and ‘bath’. The main stress is always on the second ‘tete’— the word is pronounced tete-a-TETE. The expression literally means ‘head-to-head’. When you have a tête-a-tete with someone, you have a face-to-face meeting with the individual. Only two people are involved in this conversation.
+It’s about time you had a tête-a-tete with your friend. He’s out of control. I thought it would be a romantic tête-a-tete. But she had invited her parents.
+“Wear a smile and have friends; wear a scowl and have wrinkles.” George Eliot`
+      }
+    }
+  ]
+};
