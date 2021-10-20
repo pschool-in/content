0d7c31c90bcf110f740050edfdb0cdd2cb6e42ba
Atish Sihi <85349281+atishsihi@users.noreply.github.com>
2021-10-20 10:16:51 +0530

    Update word-problems.js

--- kg-5/playlists/math/word-problems.js
+++ kg-5/playlists/math/word-problems.js
@@ -237,6 +237,54 @@ Cost of 15 candies = ₹ ?1
           ]
         }
       ]
+    },
+    {
+      type: 'numberInput',
+      id: 'word-prob-factors',
+      label: 'Word Problem factors',
+      lockAfter: 1,
+      commonData: {
+        title: 'Answer the word problem.',
+        type: 'word-problem'
+      },
+      data: [
+        {
+          list: [
+            `Ramu has a 21 ft long cloth. He wants to cut the cloth into equal pieces of 3 ft length. How much cloth will be left?
+            solution:
+            Number of 3 ft cloth = ?0 ft
+            Cloth left after cutting = ?1 ft
+            7, 0`,
+
+            `John has 2 clothes of 12 ft and 15 ft long. What could be the lenght ruler scale in ft by which he can measure both the cloth? 
+            solution:
+            The smallest length of the ruler = ?0 ft
+            The biggest length of the ruler = ?1 ft
+            1, 3`,
+
+            `There are 24 bangles. We need to put the bangles in 8 boxes. Can we put them eqully without any bangles being left?
+            solution:
+            Number Bangles in each box = ?0
+            Bangles left = ?1
+            3, 0`,
+
+            `Dave has 20 cookies. He wants to keep the cookies in jar. He has 2 different types of Jars which can contant 4 cookies or 3 cookies.
+            If he want to keep equal amount of cookies in each Jar, how many Jars he will require?
+            solution:
+            Number of Jars required = ?0
+            Each Jar have = ?1 cookies
+            5, 4`,
+
+            `Teacher has 20 Pencils & 10 eraser. These can be divided equally to maximum how many students?
+            solution:
+            Each studetns will get = ?0 Pencils
+            Each studetns will get = ?1 Erasers
+            Number of Students = ?2
+            2, 1, 10`
+          ]
+        },
+
+      ]
     }
   ]
 };
