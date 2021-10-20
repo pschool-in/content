81997c9e5028f14262f4597bf3dc0742f3d9a2ba
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-10-20 23:53:17 +0530

    Update kye-qa-2.js

--- knowYourEnglish/playlists/kye-qa-2.js
+++ knowYourEnglish/playlists/kye-qa-2.js
@@ -22,6 +22,106 @@ No, it is not. Usually, ‘according to’ and ‘opinion’ do not go together.
 According to most residents, sound pollution has become a major problem.
 In the opinion of most residents, sound pollution has become a major problem.
 Lawyers are like rhinoceros: thick skinned, short sighted and always ready to charge. David Mellor`
+            }
+         },
+      {
+        type: 'passage',
+        label: 'Don’t fall between two stools',
+        id: '200',
+        data: {
+          title: `Don’t fall between two stools`,
+          text: `How is the word ‘mischievous’ pronounced?
+          The ‘i’ in the first syllable and the ‘ie’ in the second sound like the ‘i’ in ‘kit’, ‘hit’ and ‘pit’. The ‘ch’ is pronounced like the ‘ch’ in ‘chips’, ‘change’ and ‘cheap’; and the final ‘ou’ sounds like the ‘a’ in ‘china’. The word consists of three syllables, and it is pronounced ‘MIS-chi-ves’ with the stress on the first syllable. Someone who takes delight in causing trouble is usually referred to as being ‘mischievous’. The aim, in this case, is to have fun at someone’s expense, not to cause him/her serious problems.
+Sunny gave her son a mischievous grin as she handed him his present.
+I plan to write a novel about the mischievous antics of my students.
+What is the meaning of ‘fall between two stools’?
+The expression is mostly limited to British English. There are times when we attempt to multitask — try to do two things at the same time, and fail miserably at both. This inability to achieve your goal is what ‘fall between two stools’ and ‘caught between two stools’ refer to. This is just one of the meanings of the expressions.
+You can’t be both their boss and their friend, Shiva. If you try, you’ll get caught between two stools.
+I signed up for swimming and tennis. With little time to devote to either, I feel I am falling between two stools.
+Is it okay to say, ‘The premises is cleaned every day’?
+First, let us deal with the pronunciation of ‘premises’. The ‘em’ in the first syllable rhymes with the ‘em’ in ‘them’, ‘gem’ and ‘hem’, and the two vowels that follow sound like the ‘i’ in ‘sit’, ‘bit’ and ‘chit’. The word is pronounced ‘PRE-mi-siz’ with the stress on the first syllable. The second syllable is not pronounced like the word ‘my’. The word ‘premises’ is a plural noun, and is therefore followed by a plural verb. It is mostly used to refer to the buildings and land owned by an organisation.
+The premises are cleaned every day.
+You cannot use the phone on school premises.
+Please escort these troublemakers off the premises.
+What is the difference between ‘loveable’ and ‘lovable’?
+There is no difference in meaning between them; the two words can be used interchangeably in all contexts. Both can be used to mean ‘easy to love’. ‘Lovable’ seems to be the preferred spelling nowadays.
+The lovable puppy had everyone in splits.
+Suchi was such a loveable child.
+Live fast, have fun, be a bit mischievous. Louis Tomlinson`
+        }
+    },
+    {
+      type: 'passage',
+      label: 'Are you a good Samaritan?',
+      id: '300',
+      data: {
+        title: `Are you a good Samaritan?`,
+        text: `What is the meaning of ‘like a deer in the headlights? 
+The expression is frequently heard in American English in informal contexts. I understand that when a deer sees the lights of an approaching vehicle, instead of running away, it remains frozen. The lights of the car/truck scare the animal so much that it stands paralysed. Therefore, when you compare someone to a ‘deer in the headlights’, what you are suggesting is that like the animal, the individual is very surprised or frightened; he is so confused that he is incapable of thinking — and is therefore unable to react in that situation. It is also possible to say, ‘like a deer caught in the headlights’.
+At the interview, he sat there like a deer caught in the headlights — couldn’t answer a single question.
+When she accused me of stealing her watch, instead of shouting at her, I just stood there like a deer in the headlights.
+How is the word ‘Samaritan’ pronounced?
+There are different ways of pronouncing this word. One way is to pronounce the ‘a’ in the first and final syllable like the ‘a’ in ‘china’. The second ‘a’ sounds like the ‘a’ in ‘fan’, ‘ban’ and ‘tan’, and the following vowel is like the ‘i’ in ‘bit’, ‘hit’ and ‘kit’. The word is pronounced ‘se-MA-ri-ten’ with the stress on the second syllable. It was initially used to refer to a person who lived in the district of Samaria in Palestine. Nowadays, the word is mostly found in the expression ‘a good Samaritan’, meaning someone who helps others in their time of need. There is a story in the Bible about a man from Samaria who helps an individual who has been beaten and robbed.
+Being a good Samaritan, Venu dropped us off at the bus stop.
+Don’t worry. Some good Samaritan will help you with the luggage.
+Is it okay to say ‘hit up’? 
+Yes, it is. The expression is frequently used in American English in informal contexts. When you ‘hit someone up’ for something, you usually want something from them — most of the time, it is money.
+Hema hit up Raju for another ₹20,000.
+Chandu, don’t waste your time trying to hit me up for a loan.
+Nowadays, the expression has acquired another meaning. When you ‘hit up’ someone, you contact the person - usually by phone.
+I’m leaving my number. But don’t hit me up unless it’s an emergency.
+Shoba hits me up whenever there’s a problem with her computer.
+What is the difference between ‘brat’ and ‘kid’? 
+‘Kid’ is the more common of the two words. When you say that someone is a ‘kid’, what you are suggesting is that he is a child or is very young. This individual’s behaviour can be either good or bad. ‘Brat’, on the other hand, is always associated with something negative. When you refer to a small child as being a ‘brat’, what you are suggesting is that the individual behaves horribly; he annoys you. The word is mostly used to show your contempt for the individual. ‘Brat’ comes from the Old English ‘bratta’ meaning ‘coarse cloth’. It was actually used to refer to the bib that a child wore while eating.
+Shalini’s kids are well behaved. They are not brats.
+Children really brighten up a household. They never turn the lights off. Ralph Bus
+  `
+      }
+    },
+    {
+      type: 'passage',
+      label: 'Cut off from the rest',
+      id: '400',
+      data: {
+        title: `Cut off from the rest`,
+        text: `How is the word ‘incommunicado’ pronounced?
+The ‘i’ in the first and fourth syllable is pronounced like the ‘i’ in ‘bit’, ‘kit’ and ‘sit’. The ‘o’ in the second is like the ‘a’ in ‘china’, and the following ‘mu’ sounds like the ‘mu’ in ‘music’ and ‘mute’. The ‘ca’ is pronounced like the ‘ca’ in ‘car’, ‘cart’ and ‘calm’, and the final ‘do’ rhymes with ‘so’, ‘go’ and ‘no’. The word is pronounced ‘in-ke-myoo-ni-KAA-do’ with the stress on the fifth syllable. It comes from the Spanish ‘incomunicar’ meaning ‘deprive of communication’. When you remain ‘incommunicado’, you do not stay in contact with anyone; you cut off all forms of communication with others. This could be because you yourself have chosen not to stay in touch with others or it could be because someone or something is preventing you from making contact. When a suspected terrorist is arrested, he is usually held incommunicado. The police make sure that he does not make phone calls or talk to others.
+During the week-long break, Sandeep didn’t answer the phone. He remained incommunicado.
+The terrorists were held incommunicado for over three months in an Army prison.
+What is the meaning of ‘drop-dead gorgeous’? 
+The expression ‘drop-dead’ is usually used before adjectives in informal contexts to emphasise a particular quality. For example, when you say that someone is ‘drop-dead gorgeous’, what you are suggesting is that the person is extremely or amazingly attractive; she is absolutely stunning. Your heart starts pounding so fast when you see her, you nearly collapse.
+Even without her make-up, the ageing superstar looked drop-dead gorgeous.
+For the reception, the bride wore a saree that was drop-dead gorgeous.
+What is the difference between ‘skirmish’ and ‘fight’?
+First, let us deal with the pronunciation of ‘skirmish’. The first syllable is pronounced like the ‘skir’ in ‘skirt’, and the second rhymes with ‘dish’ and ‘fish’. The word is pronounced ‘SKER-mish’ with the stress on the first syllable. A ‘skirmish’ is a minor fight lasting for a short period. This fight could be either physical or verbal — it could be an argument between two people about something. The word is frequently used by the military to refer to a fight that occurs between small groups of enemy soldiers, the fight itself is not serious, and it usually takes place far away from the main scene of action.
+No one was injured in the skirmish that ensued between the police and the students.
+There was a short skirmish between the couple about which restaurant they should go to.
+Is it okay to say, ‘The troops invaded the enemy’?
+No, it is not. One can invade a country or a place, but not a person. Of course, one can invade a person’s ‘privacy’, but not the individual himself/herself. When you ‘invade’ a country, you go there in large numbers in order to capture it. When you ‘invade’ someone’s property, you enter it illegally, usually in large numbers, in order to damage it. In the case of the sentence you have given, ‘attack’ would a better word to use. The troops attacked the enemy.
+“It’s not the size of the dog in the fight, but the size of the fight in the dog.” Mark Twain`
+      }
+    },
+    {
+      type: 'passage',
+      label: 'Air headed?',
+      id: '500',
+      data: {
+        title: `Air headed?`,
+        text: `What is the difference between ‘mock’ and ‘tease’?
+‘Tease’ is the more general of the two words. When you ‘tease’ someone, what you are trying to do is to have fun at the other person’s expense. You may say or do something in order to annoy him. For example, we usually tease a friend about his terrible haircut or about his terrible dress sense. We are having harmless fun at the other person’s expense — it is quite possible that both the teaser and the person being teased enjoy the experience. The word can also be used to suggest that you are deliberately saying things in order to hurt the feelings of the other person. The word ‘mock’ is mostly limited to formal contexts, and the use of the word suggests that the intent in this case is to laugh at and not laugh with the other individual. In this case, you say or do things in order to show your contempt for the person — the intent is to ridicule the individual. Unlike ‘tease’, one can mock an individual by imitating him/her.
+Shalini is very sensitive. Please don’t even think about teasing her.
+No one was amused when Rishi mocked the Principal’s stutter.
+How is the word ‘chic’ pronounced?
+The ‘ch’ sounds like the ‘sh’ in ‘sheet’, ‘ship’ and ‘sharp’, and the following ‘ic’ is like the ‘eek’ in ‘cheek’, ‘peek’ and ‘seek’. This word of French origin is pronounced ‘SHEEK’. ‘Chic’ can be used both as a noun and an adjective. When you say that someone is ‘chic’, what you are suggesting is that the person is very stylish; he/she is elegantly fashionable. One can talk about someone’s clothes being chic or about someone’s shoes being ‘chic’. The word suggests that the item in question is expensive.
+The Mathurs have bought a chic apartment in Malabar Hills.
+I just love your new glasses. They look so chic.
+Is it okay to call someone a ‘blockhead’?
+If the person in question is very stupid, then you certainly can. Should you call the person a blockhead to his face...well, I leave that to you. The word is mostly limited to informal contexts; though many people consider it to be rather old fashioned, it continues to be used even today — other words that have more or less the same meaning are ‘dunce’, ‘knucklehead’ and ‘bonehead’.
+Sushma is marrying Kedar? What does that girl see in that blockhead?
+Keep the language simple. I want to make sure that the blockheads in the Sales
+Department can make sense of the document.
+The word has an interesting origin. In the old days in Europe, it was common practice for both men and women to wear wigs — whether you had hair or not, it was fashionable to sport a wig. Rich people, of course, had many wigs. At night-time, the wig was placed on a block of wood that was shaped like the wig wearer’s head — this was to ensure that the wig did not lose its shape. This block of wood was called a ‘blockhead’. With the passage of time, the word began to mean someone who had nothing inside his head — just like the original blockhead.
+“Scientists say that the world is made of protons, neutrons and electrons. They forgot to mention morons.” Unknown`
       }
     }
   ]
