8abd4111693c7e1aa2f9bc03aee198aeba01568f
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-10-23 00:04:37 +0530

    Update kye-qa-3.js

--- knowYourEnglish/playlists/kye-qa-3.js
+++ knowYourEnglish/playlists/kye-qa-3.js
@@ -1,1 +1,30 @@
-
+export default {
+    label: 'Know Your English Q & A',
+    id: 'kye-qa-3',
+    list: [
+      {
+        type: 'passage',
+        label: 'Find your lodestar',
+        id: '100',
+        data: {
+          title: `Find your lodestar`,
+          text: `How is the word ‘acquiesce’ pronounced?
+The first syllable sounds like the ‘ack’ in ‘sack’, ‘pack’ and ‘back’; the following ‘qui’ sounds like the ‘wi’ in ‘wit’, ‘will’ and ‘win’. The final syllable is pronounced like the ‘es’ in ‘yes’, ‘mess’ and ‘chess’. The word is pronounced ‘ak-wi-ES’ with the stress on the third syllable. It comes from the Latin ‘acquiescere’ meaning ‘to become quiet’ or ‘remain at rest’. Nowadays, the word is mostly used in formal contexts to mean to agree to something a person has said. You do what the person tells you to, although very reluctantly. You make no attempt to protest; you remain silent.
+The new Vice Chancellor refused to acquiesce to the students’ demands.
+All three police officials acquiesced to the Minister’s plans.
+Can the word ‘snake’ be used as a verb?
+Yes, it can. The word ‘snake’ can be used as a noun and a verb. When used as a verb, it suggests that you are moving like a snake — you are not moving in a straight line, but are twisting and turning quite frequently. Think of the people driving a motorcycle or a scooter through traffic in our country — and you get the picture. The expressions ‘snake around’, ‘snake through’, and so on, are mostly used to suggest that there are many twisting curves. When you say that the road snakes its way through the mountain, what you are suggesting is that it is a winding road with a lot of curves.
+There was a long queue to get in. It snaked around the stadium.
+What is the meaning of ‘lodestar’?
+This word has been in the news ever since the anonymous op-ed against the U.S. President Donald Trump was published in The New York Times. There is speculation that the author could be Vice President Pence because ‘lodestar’ is a word that he frequently uses in his conversations and speeches. The first syllable, by the way, is pronounced the same way as ‘load’. The term ‘lodestar’ was first used to refer to the polestar or the North Star. In the past, sailors used it for navigation. The word ‘lode’ originally meant ‘way’ or ‘course’. Sailors used the lodestar as a reference point to find their way home. Nowadays, the word is used to refer to someone or something that is seen as a model or guide. Like the actual star, this individual guides people; he sets an example for others to follow. The word can be used with things as well; the beliefs that you have which often determine your action can be referred to as your lodestar.
+A few people sniggered when the Minister said that serving people had been his lodestar all through his life.
+Federer still continues be the lodestar for many tennis enthusiasts.
+Which is correct: We are going on holiday/holidays to France?
+The standard expression is ‘on holiday’. For example, you can say, ‘The children went on holiday to Kashmir’. If you wish to use ‘holidays’, then you normally say, ‘The children went to Kashmir for the holidays’.
+Where is Prithi going for the holidays?
+We’re planning to go to the Maldives on holiday.
+There are two kinds of travel: first class and with children. Robert Benchley`
+        }
+      }
+     ]
+  };
