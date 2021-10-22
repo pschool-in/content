5648b045bcb527771c748320d6022f43c8bf514c
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-10-22 23:10:17 +0530

    Update kye-conversation-2.js

--- knowYourEnglish/playlists/kye-conversation-2.js
+++ knowYourEnglish/playlists/kye-conversation-2.js
@@ -228,7 +228,168 @@ Insufficient funds
 “The Department of Health in our country has always had to operate on a shoestring budget.”
 “Sad, but true.”
 “Cinderella is proof that a new pair of shoes can change your life.” Unknown`
-           }
-         }
-      ]
+          }
+      },
+   {
+    type: 'passage',
+      label: 'Bank on it',
+      id: '700',
+      data: {
+        title: `Bank on it`,
+        text: `“I voted for the first time today. What about you? Did you vote for the same person as you had, last time?”
+“I didn’t get a chance to vote because my name hadn’t been included in the voter’s list. I don’t know how that happened.”
+“It happened to quite a few people, actually. Do you think your candidate will win?”
+“She will definitely win. You can take it to the bank.”
+“Why would I take her to the bank? I am sure she has better things to do.”
+“When you make a statement and then follow it up with ‘You can take it to the bank’, what you are suggesting is that the statement will definitely come true.”
+“So, what you are saying is that in this election, your candidate will definitely win.”
+“Exactly! What I am saying is that I am positive she’ll win. Her win is more or less guaranteed.”
+“In other words, you are extremely confident of her winning. How about this example? Manas will complete the project by Saturday. You can take it to the bank.”
+“Sounds good. In our State, no party will win a majority. You can take it to the bank.”
+Make money
+“That’s what everyone is saying, but I am not so sure. The good thing is that in the last two weeks a lot of people seem to have made a lot of money.”
+“That’s true. My maid’s family made bank.”
+“She made a bank? What are you talking about? Why would her family...”
+“The expression ‘to make bank’ is used in American English in informal contexts. When you ‘make bank’, you make a lot of money — usually, in a very short period of time. Sunny made bank when the prices of shares soared.”
+“My friend Kamini has got a new job. She said she’s making bank.”
+“That’s a good example. Given your qualification, I am sure you’ll be making bank as well. You can take that to the bank.”
+“That is nice of you to say. Tell me, where did the word ‘bank’ come from?”
+“Well, according to some scholars, it comes from the Italian word ‘banca’ being ‘bench’.”
+“Bench? That’s strange. What is the connection between the modern bank and a bench? It’s true that in the old days we used to find tables and benches in all banks, but...”
+“In the old days, if people needed money, they usually went to a moneylender. And this person used to transact business sitting on a ‘banca’.”
+“I see. In other words, the moneylender sat on a bench when he did business.”
+“That’s right! In the past, every moneylender had his own ‘banca’ or bench. With the passage of time, the word began to be used to refer to the place where business was carried out and not the bench itself.”
+“That is understandable, I guess. But tell me, is there a connection between ‘bank’ and ‘bankrupt’? I mean are the two words....”
+“The word ‘rupt’ comes from the Italian ‘rupta’ meaning ‘broken’.”
+“So ‘bankrupt’ literally means ‘broken bench’!”
+“Very good. That is exactly what it means. According to scholars, whenever people felt that the moneylender was cheating them, they became angry and very often broke his bench.”
+“And without his bench, the moneylender couldn’t transact any business — and this led to his financial ruin.”
+“That is one theory, anyway.”
+I finally know what distinguishes man from beasts - financial worries. Jules Renard`
+     }
+   },
+  {
+    type: 'passage',
+      label: 'Get set from the get-go',
+      id: '800',
+      data: {
+        title: `Get set from the get-go`,
+        text: `“Come in, come in! You’re all wet. Do you need a towel to dry off?”
+“That would be nice. It’s bright and sunny outside, but at the same time, it’s raining. Is there a word to describe the phenomenon — this mix of sunshine and rain?”
+“Yes, there is. People refer to it as ‘sunshower’. Some dictionaries spell it as one word and others as two — ‘sun shower’. Sunshowers don’t usually last for a long time.”
+“Well, this one lasted long enough for me to get completely soaked.”
+“I wouldn’t complain too much. It’s a nice way of cooling off in this heat, isn’t it?”
+“Who cares about the heat? Why don’t people just stop complaining...”
+“What’s the matter with you? Why do you look so unhappy? Did someone say something about your terrible haircut?”
+“You’re the first person to say it’s terrible. I just heard some bad news. Shravasti told me that our wonderful physics teacher has quit.”
+“That was to be expected, I guess. After all, she and your Principal didn’t get along from the get-go. They were always at...”
+“Get-go? What does it mean?”
+“The expression ‘from the get-go’ is mostly used in informal contexts to mean ‘from the very beginning’. Your teacher and your Principal just didn’t get along....”
+“That’s true. They didn’t see eye to eye about anything from the beginning. How about this example? Unlike most of my classmates, Sunitha didn’t fall in love with physics from the get-go.”
+“That’s understandable. Not everyone likes physics. I certainly didn’t when I was in school. From the get-go, my boss made it very clear that he would be making sweeping changes in the department.”
+“Was everyone in the department able to deal with the changes from the get-go?”
+Get familiar
+“Most of us managed to. But like it usually happens, there were some who had a tough time getting a handle on the changes.”
+“Handle? Which or whose handle are you talking about? Are you okay?”
+“When you say that someone has ‘got a handle on a subject’, what you’re suggesting is that they have an understanding of it.”
+“In other words, it’s an informal way of saying that you’re familiar with something.”
+“I guess you could say that. It’s also possible to say that you ‘have a handle on something’. It took the students and the staff nearly two weeks to get a handle on the new software.”
+“Whenever I’m unable to solve a problem in physics, I usually go to Jai. He has a good handle on the subject.”
+“Clementine said that she wouldn’t be making any immediate recommendations. She wants to get a handle on the problem before doing so.”
+“That seems a sensible thing to do. How about this example? Can you read this article and see if you can get a handle on what the author is trying to say?”
+“Sounds good to me. What are your plans for the summer? Are you going anywhere?”
+“Yes, I am. I’m considering to go to Arunachal Pradesh. Care to join me?”
+“One doesn’t normally say, ‘considering to go’. It’s always ‘consider going’. For example, Suresh is considering going to Shimla.”
+“The Principal is considering taking voluntary retirement next month.”
+“You should consider going home before you catch a cold.”
+“That’s a good idea!”
+Some people feel the rain. Others just get wet. Bob Marley`
+      }
+    },
+  {
+    type: 'passage',
+      label: 'Don’t be a Twitterholic',
+      id: '900',
+      data: {
+        title: `Don’t be a Twitterholic`,
+        text: `“Bala doesn’t have to worry about money. He comes from a wealthy family. He is the .... How do you pronounce h..e..i..r?”
+“It is pronounced like the word ‘air’. Unlike the word ‘hair’, the ‘h’ is silent in this case. Do you know what ‘heir’ means?”
+“Of course, I do. Isn’t he the person who receives money or property when someone in his family dies — usually, when the father or the mother dies?”
+“It needn’t be just the parents. It could be anyone.”
+“My friend Bala has nothing to worry about because he is the heir to his uncle’s fortune.”
+“That’s a good example. Unfortunately, my uncle has no children. He has no heir.”
+“Really? Then, why don’t you get him to adopt you? If he doesn’t like you, he could consider me. I am also available for adoption!”
+Extremely wealthy
+“Very funny! If he chooses to adopt you, you will come into some serious money.”
+“Serious money? What are you saying? How can money be serious about anything?”
+“Native speakers of English often use the word ‘serious’ before nouns in informal contexts to mean ‘substantial’ or ‘considerable’.”
+“So, in this case, you are emphasising the fact that your uncle’s heir will come into a lot of money. A large amount!”
+“That’s right. During the winter break, Mala did some serious partying.”
+“Meaning that Mala did a lot of partying. Perhaps a little too much?”
+“Yes, it could suggest that it was extreme. Here’s another...”
+“Let me give it a try. Even after the surgery, Naresh was experiencing serious pain.”
+“Sounds good. What’s wrong with the biscuits? Don’t you like them?”
+“Of course, I like them. What makes you think I don’t?”
+“I have seen you eat biscuits before. If it is something you like, you usually finish it in two bites. The way you have been nibbling at the one in your hand suggests that you are not...”
+“Nibbling? Does it mean to eat something reluctantly?”
+“No, that is not what it means. When you ‘nibble on’ or ‘nibble at’ something, you take small bites of it. It can be something you like or dislike.”
+“In my case, I nibble when I eat something that I don’t really like. When I eat something that I enjoy, I just gobble it up.”
+“I know! I have seen the way you eat samosas and bajjis. Anyway, here is an example. Shanthi sat there nibbling at her cheese sandwich.”
+“Ram was definitely worried about something. He sat on the sofa nibbling on a carrot.”
+“Ram really hates carrots. He must have read something on Twitter that upset him.”
+“Do you have a Twitter account?”
+“I do. But unlike most of my friends, I don’t spend a lot of time on it. I am not what you might call a Twitterholic.”
+“Twitterholic? Sounds like ‘alcoholic’ and ‘workaholic’. So, is a ‘Twitterholic’ someone who loves to spend a lot of time on Twitter?”
+“Yes, the person is addicted to Twitter. He tries to spend as much time on it as he can.”
+“I know someone who is a Twitterholic. Donald Trump! He is constantly tweeting.”
+“That is true! I am told Bala is a Twitterholic as well.”
+“With the kind of money he has, he doesn’t need to work. He can spend all the time he wants on Twitter.”
+On Twitter, we get excited when someone follows us. In real life, we get really scared and run away. Unknown`
+     }
+   },
+  {
+    type: 'passage',
+      label: 'A windbag bores',
+      id: '1000',
+      data: {
+        title: `A windbag bores`,
+        text: `“I ran into one of your old teachers at the supermarket. Can you guess who?”
+“Just tell me who it was. It’s too early in the morning to play guessing games.”
+“You seem to be in a bad mood. It was your maths teacher, Mr. Das.”
+“Ah, Das! Terrible teacher. He was a real stick-in-the-mud. He couldn’t...”
+“Did you say that he was stuck in the mud? When did he get...”
+“I said ‘stick-in-the-mud’. When you say that someone is a stick-in-the-mud, what you’re suggesting is that the person is rather old fashioned. He’s very resistant to change.”
+“In other words, the person is rather narrow minded. He is very set in his ways.”
+“He is very stuck in his ways, I’d say. He’s like a person whose feet are stuck in mud...”
+“And is therefore unable to move. Can’t make any progress.”
+“That’s right! The expression also carries with it the sense that the individual is rather dull or slow. He’s someone who doesn’t know how to enjoy himself.”
+“He probably doesn’t let others enjoy themselves, either. I guess the expression is mostly used to show disapproval.”
+“Very good! My father’s uncle was a real stick-in-the-mud.”
+“I don’t think I’ll ever be a stick-in-the-mud.”
+“You never know! The students came up with interesting ideas for the annual event. Unfortunately, the stick-in-the-mud Principal shot them all down.”
+Too much talk
+“Gopi, Mr. Das’ son, was with him as well. He’s working for some bank now.”
+“Gopi was fun. He was bit of a windbag, but fun. Unlike his father.”
+“Windbag? Does it mean the same thing as ‘gasbag’? Someone who...”
+“That’s exactly what it means! When you refer to someone as a ‘windbag’, what you’re suggesting is that he talks a lot. Unfortunately, there’s very little substance in what he says. Usually, a windbag bores people.”
+“In other words, he’s like a politician. He talks a lot, but has nothing interesting to say.”
+“I guess you could say that. Some of the people in my office are boring windbags.”
+“Our college annual day was ruined because the Master of Ceremonies was a windbag. His so-called jokes fell flat.”
+“I’m sorry to hear that. I have a nice group of friends. None of them is a gasbag.”
+“Lucky you! We have quite a few windbags in my family.”
+“Every family has one or two gasbags, I guess. So, you changed your mind about going to the annual day event, did you?”
+“I wasn’t planning to go, but my friends insisted that I go with them.”
+An estimate will do
+“Was there a big crowd? How many people turned up? Any idea?”
+“I really don’t know. I don’t know the exact number.”
+“Oh, I don’t need the exact number. Just give me a ballpark figure.”
+“Ballpark figure? Does it mean to give a rough estimate?”
+“That’s right! The expression is mostly used in informal contexts in American English. The word ‘ballpark’ is frequently used to refer to a stadium.”
+“And since it’s difficult to figure out the exact number of people present in a stadium...”
+“The expressions ‘ballpark figure’ and ‘ballpark estimate’ are used to mean ‘an approximate figure’. For example, the flat will cost you ₹60 lakhs. It’s a ballpark estimate.”
+“There must have been over a thousand people at the annual event. It’s a ballpark figure.”
+Money is like the sixth sense — and you can’t make use of the other five without it. Somerset Maugham`
+        }
+      }
+    ]
   };
