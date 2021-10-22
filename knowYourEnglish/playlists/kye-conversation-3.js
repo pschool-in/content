3015031521900ddc05bab64790d228b1db76b5e1
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-10-22 23:17:35 +0530

    Update kye-conversation-3.js

--- knowYourEnglish/playlists/kye-conversation-3.js
+++ knowYourEnglish/playlists/kye-conversation-3.js
@@ -1,1 +1,45 @@
-
+export default {
+    label: 'Know Your English',
+    id: 'kye-conversation-3',
+    list: [
+      {
+        type: 'passage',
+        label: 'Without a hitch',
+        id: '100',
+        data: {
+          title: `Without a hitch`,
+          text: `“Ever since my mother put my father on a diet, he’s been in a terrible mood. He gets angry about things very quickly. It’s very difficult to understand...”
+“I don’t think the poor man is just angry. He’s probably ‘hangry’!”
+“Hangry! I’ve never heard that word before. Is it a new word?”
+“Believe it or not, it’s been around for nearly 70 years. It’s a combination of ‘hungry’...”
+“And ‘angry’, I suppose! Does it refer to the anger that you feel when you’re hungry?”
+“Very good. The hunger puts you in a foul mood. Everything irritates or angers you.”
+“We’ve all experienced it, I guess. How about this example? Because he’d skipped lunch, Jai was pretty hangry by dinnertime.”
+“Sounds good. I could see that Bala was getting hangry. So we went to a restaurant and had a couple of sandwiches and a cup of coffee. He felt much better after that.”
+Taking it easy
+“Talking about feeling much better, how are you feeling after your trip to Goa?”
+“I’m feeling great. I had a wonderful time lolling around the beach, and...”
+“Lolling around? Does it mean lying down on the...?”
+“When you say that you were ‘lolling around’ or ‘lolling about’, what you’re suggesting is that you were sitting or lying down somewhere. The expression suggests you were relaxed and were really enjoying yourself. For example, my grandmother lolls about in her rocking chair in the mornings.”
+“Once I get a job, I’m going to be lolling around all day in my pyjamas on Sundays.”
+“Sounds like a plan. When my boss saw some of us having a conversation near the water cooler, he told us to stop lolling about and get back to work.”
+“That’s what our Principal does as well. Tell me, how was the conference in Goa?”
+“It was a total waste of time and money. If you ask me, I think all conferences should be banned. Nothing ever happens.”
+“Why do you say that? Didn’t the delegates at the conference like your paper?”
+Sailing through
+“Most of the people who came wanted to spend all their time on the beach. So when it came to the paper presentation, most speakers just whizzed through and...”
+“What did you say? Whiz through? Does it mean to finish something very quickly?”
+“Exactly! When you ‘whiz through’ something, you do it very quickly — without too much trouble. I’d expected the test to be difficult. But I just whizzed through it.”
+“How about this example? My mother gave me several things to do this morning. I whizzed through them and then went to see a movie.”
+“That’s a good example. And knowing you, that’s all it is — an example. I’m sure you haven’t even started on anything.”
+“As usual, you’re right! Like many people you know, I find it difficult to whiz through household chores.”
+“Federer whizzed through the first set, and won the second in a tie-breaker.”
+“Tell me, do the expressions ‘whiz through’ and ‘waltz through’ mean the same thing?”
+“When you ‘waltz through’ something, you finish it quickly because you find it easy to do. For example, most of the students waltzed through the test.”
+“Mala had trained so well that she waltzed through the first three rounds of the tournament.”
+“Good for her!”
+If voting made any difference, they wouldn’t let us do it. Mark Twain`
+          }
+       }
+    ]
+  };
