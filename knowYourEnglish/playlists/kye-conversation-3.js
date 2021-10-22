6b1b7980d60c6a36eeb5dbe8d3a5493dde41f32d
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-10-23 00:05:39 +0530

    Update kye-conversation-3.js

--- knowYourEnglish/playlists/kye-conversation-3.js
+++ knowYourEnglish/playlists/kye-conversation-3.js
@@ -39,6 +39,82 @@ Sailing through
 “Mala had trained so well that she waltzed through the first three rounds of the tournament.”
 “Good for her!”
 If voting made any difference, they wouldn’t let us do it. Mark Twain`
+            }
+         },
+      {
+        type: 'passage',
+          label: 'Cut out the toxicity',
+          id: '200',
+          data: {
+            title: `Cut out the toxicity`,
+            text: `“I ran into Anjali’s brother yesterday. He said that Anjali and Rahul are getting a divorce. Don’t you find that strange? They’re such a...”
+“No, I don’t find it strange at all! I’m happy she’s getting out of that toxic relationship.”
+“Toxic relationship? Doesn’t ‘toxic’ mean poisonous or something?”
+“Yes, one of the meanings of ‘toxic’ is poisonous. One can talk about ‘toxic waste’, ‘toxic chemicals’, and so on. But the word can also be used to refer to something that is very bad or harmful. Something that causes a lot of unpleasantness and...”
+“In other words, something that causes unhappiness.”
+“Exactly! And this unhappiness or unpleasantness usually lasts for some time. Ever since the workers went on strike last month, the situation at the office has been toxic.”
+“Anjali’s husband doesn’t have too many friends because of his toxic sarcasm.”
+In good books
+“That’s a good example. Now that he’s divorcing Anjali, he won’t have any friends left. If he wants to keep some of them, he’ll need to be nice to them. Butter them up, if he...”
+“Butter them up? What does it mean?”
+“It’s an expression mostly used in informal contexts. When you ‘butter someone up’, you flatter them. You do this because you want to...”
+“You want something from them. You’re being nice to them because you need a favour.”
+“Very good! In schools and colleges, it’s very common to see students trying to butter their teachers up.”
+“A little bit of buttering up goes a long way in getting a student a few extra marks.”
+“That’s true. If you want to tide over the crisis, you need to butter up all the investors. If you don’t, the company will definitely collapse.”
+“You have a point there. But sometimes, it backfires. I tried to butter up my grandfather by cleaning and rearranging his room. Instead of being happy, he became extremely angry. Had a difficult time finding his things.”
+“So by trying to butter him up, you succeeded in cheesing him off!”
+“First ‘butter’, and now ‘cheese’! Does ‘cheese off’ mean to annoy someone?”
+“When you ‘cheese someone off’, you make the person very angry or irritated. Like ‘butter up’, it’s used in informal contexts. My father was really cheesed off with his boss.”
+Annoyed
+“Something that the Principal said at the meeting seems to have cheesed most teachers off. How does that example sound?”
+“Good! Two of my best friends are really cheesed off with each other. They are...”
+“So, what are you doing to do to bring the fight to an end? What are you doing to help them patch up?”
+“Absolutely nothing! I don’t want to get involved, so I’m avoiding them both. I’m keeping my head down.”
+“Keeping your head down? Are you doing that because you don’t want to be seen?”
+“The expression is mostly used to mean to avoid being seen — to make sure that no one notices you. It can also be used to mean not to get involved in the things happening around you.”
+“I see. Radha decided to keep her head down while her two brothers argued about which restaurant they should go to on their parents’ anniversary.”
+“I don’t get involved in office politics. I keep my head down and do my job.”
+“Sensible thing to do.”
+Keep your head up in failure and your head down in success. Jerry Seinfeld`
+        }
+      },
+      {
+        type: 'passage',
+          label: 'Avoid a meltdown',
+          id: '300',
+          data: {
+            title: `Avoid a meltdown`,
+            text: `“Your friend Akash tells me that the man who came to your school yesterday spoke really well. He had some interesting things to say about...”
+ “Akash may have liked it. But most of us didn’t. The man used too many jargons.”
+“Jargon. The word ‘jargon’ is mostly used...”
+“I know what ‘jargon’ means. It refers to the special terms or expressions used in a profession or field. For example, LBW, forward short leg, leg slip, and so on, are jargons used in cricket.”
+“That’s right! But what I wanted to say was ‘jargon’ is an uncountable noun. The plural is ‘jargon’ and not ‘jargons’. I don’t understand the jargon of law.”
+“I don’t think anyone does. The speaker used too much jargon yesterday. How was the annual party, by the way? Did you have a good time?”
+Eventful
+“Most of us were having a good time till the Marketing Manager had a major meltdown.”
+“A major meltdown? Does it mean he became rather emotional and started crying?”
+“It could. The expression ‘meltdown’ is mostly used in informal contexts. When an individual has a ‘meltdown’, he becomes very upset about something.”
+“Usually when people get upset, they shout at others.”
+“That’s right! A meltdown suggests an emotional outburst of some kind. You could either shout at somebody or break down because...”
+“In other words, the person is no longer in control of his emotions. He loses his self-control. Serena had a major meltdown during the recent US Open final.”
+“She did, didn’t she? Here’s another example. Taking care of triplets can’t be easy. I’m sure Revathi has a meltdown every now and then.”
+“Why don’t you ask her husband? When I was young, my father used to have a major meltdown whenever he saw my report card. Was it the same case with your father?”
+“In my case, it was my mother who used to have the meltdown. My father, on the other hand, didn’t really care. He was pretty laid-back.”
+Composed
+“Laid-back? Does it mean a lazy person? Someone who doesn’t really care about anything?”
+“That’s not what it means. A laid-back person is someone who doesn’t get upset about things very easily. He seems very calm and relaxed all the time.”
+“In other words, someone who doesn’t let the pressure get to him. Nothing really worries him. My physics teacher has a laid-back approach to everything.”
+“I enjoy being in the company of people who are laid-back. When you’re in their company, you begin to relax. The term can be used with things as well. For example, compared to Mumbai, Hyderabad is a very laid-back city.”
+“Compared to Mumbai, any city in India would seem laid-back. Raju says that the company that he’s working for now is pretty laid-back. They don’t have a dress code or anything. The only thing they care about is meeting deadlines.”
+“Isn’t your uncle coming to your place today?”
+“That’s right. He said that he’d be arriving in our place around six in the evening.”
+“Arriving ‘at’, not ‘in’. You normally use ‘arrive in’ when you have a city or country in mind. For example, the children arrived in Hyderabad last week.”
+“The President will be arriving in Singapore this afternoon.”
+“Otherwise, it’s usually ‘arrive at’. We arrived at the airport a little after one.”
+“By the time we arrived at the bus stop, the bus had left.”
+“That sounds like you. You’re always late for everything.”
+Happiness is not a state to arrive at, but a manner of travelling. Margaret Lee Runbeck`
           }
        }
     ]
