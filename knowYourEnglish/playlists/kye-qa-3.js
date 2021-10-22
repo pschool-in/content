760ce5e371a0e899d8393554b39cb4b13663686a
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-10-23 00:19:29 +0530

    Update kye-qa-3.js

--- knowYourEnglish/playlists/kye-qa-3.js
+++ knowYourEnglish/playlists/kye-qa-3.js
@@ -24,6 +24,28 @@ The standard expression is ‘on holiday’. For example, you can say, ‘The ch
 Where is Prithi going for the holidays?
 We’re planning to go to the Maldives on holiday.
 There are two kinds of travel: first class and with children. Robert Benchley`
+         }
+       },
+      {
+        type: 'passage',
+        label: 'The thief is a history-sheeter',
+        id: '200',
+        data: {
+          title: `The thief is a history-sheeter`,
+          text: `What is the difference between ‘populous’ and ‘popular’? 
+Both words come from the Latin ‘populus’ meaning ‘people’. Of the two, ‘popular’ is much more frequently used than ‘populous’. When you say that someone is ‘popular’, what you are suggesting is that the person is liked or admired by many people. A restaurant that is ‘popular’ is frequented by many people; a lot of people eat there. ‘Populous’, on the other hand, is mostly used with a place — it could be a country, state, area, etc. When you say that a place is ‘populous’, what you are suggesting is that it is full of people; it is ‘densely populated’. India, for example, has a very big population — it is a populous country.
+These types of shoes are popular with teenagers.
+I don’t wish to live in a populous metropolitan city after I retire.
+By the way, the word ‘pop’ in ‘pop music’ — the kind of music that a lot of people like to listen to — is actually a contraction of ‘popular’.
+How is the word ‘emolument’; pronounced? 
+The first vowel sounds like the ‘i’ in ‘bit’, ‘sit’ and ‘kit’, while the second is like the ‘o’ in ‘cot’, ‘got’ and ‘hot’. The ‘u’ sounds like the ‘ou’ in ‘could’; and ‘would’ and the final ‘e’ is pronounced like the ‘a’ in ‘china’. The word is pronounced ‘i-MOL-yu-ment’; with the stress on the second syllable. Like ‘popular’ and ‘populous’, this word too is of Latin origin. It comes from ‘emolumentum’ meaning ‘profit’ or ‘gain’. In Latin ‘emolere’ means ‘grind out’; the word emolument was first used to refer to the money that people paid the miller for grinding their corn. Nowadays, of course, the word is used to refer to the payment one receives for the work one has done — it could be in the form of cash or something else.
+What is the meaning of ‘history sheeter’?
+Not all standard dictionaries list the terms ‘history sheeter’ and ‘rowdy sheeter’; those that do include them make it clear that they are examples of Indian English. The terms are used in our country to refer to a person with a criminal record — someone who has been arrested by the police. Native speakers of English refer to the sheet that the police use to list the crimes committed by an individual as ‘rap sheet’. In India, it is called ‘history sheet’. It contains the history of the crimes that the individual has committed or has been accused of committing. The term ‘history sheeter’ and ‘rowdy history sheeter’ were coined to refer to someone who has a criminal record —- someone who has a ‘history sheet’. Native speakers, however, do not refer to someone with a ‘rap sheet’ as a ‘rap sheeter’.
+Chances are, the well-known history sheeter, will be our next CM.
+The police refused to file an FIR against the rowdy history sheeter.
+Which is correct ‘discharge from hospital’ or ‘release from hospital’? 
+Dictionaries on collocations suggest that ‘discharge’ is the appropriate word. When you say that someone was released from hospital, it suggests that the authorities forced the individual to stay — much against his will. I suppose this is possible in the case of psychiatric patients. In most cases, people would use ‘discharge’.
+A hospital should also have a recovery room adjoining the cashier’s office — Francis Owalsh`
         }
       }
      ]
