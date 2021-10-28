93dbc3ad7b91e68023b27411bc7518f9226d91fd
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-10-28 23:39:08 +0530

    Update kye-conversation-5.js

--- knowYourEnglish/playlists/kye-conversation-5.js
+++ knowYourEnglish/playlists/kye-conversation-5.js
@@ -1,1 +1,155 @@
-
+export default {
+    label: 'Know Your English',
+    id: 'kye-conversation-5',
+    list: [
+      {
+        type: 'passage',
+        label: 'We’ve put our plans on the back burner',
+        id: '100',
+        data: {
+          title: `We’ve put our plans on the back burner`,
+          text: `“How do you pronounce c..l..e..r..k? Does the ‘er’ rhyme with the ‘ar’ in ‘dark’ and ‘park’?”
+“Well, that’s the way the British pronounce the word. The Americans, on the other hand, pronounce the ‘er’ like the ‘er’ in ‘perk’ and ‘jerk’. Both pronunciations are acceptable.”
+“That’s good to know. I always get confused about the pronunciation…”
+“Did you know that ‘clerk’ actually comes from ‘cleric’? It’s…”
+“Cleric? You mean a priest? What’s the connection between…”
+“In the old days, priests were among the few people who could read and write. So, they were often hired by kings and noblemen to write letters and maintain accounts.”
+“I’m going to tell Sujatha this when I meet her on next Friday. She’ll be...”
+“When you use ‘next’ and ‘last’ with the days of the week, you don’t use ‘on’. I met Kapil last Monday. You don’t say, I met Kapil on last Monday.”
+“I’ll be meeting Sujatha next Friday. I think I’ve understood. What did your boss think of your proposal? Did he say it was terrible or did...”
+“On the contrary, he liked it. He said that some of the ideas proposed were exciting. But he felt that they were not going to fly. Not right now, anyway.”
+“What are you talking about? Ideas don’t fly, do they?”
+“When you say that something is ‘not going to fly’, what you’re suggesting is that it’s not likely to succeed. It’s unlikely to work in a given situation.”
+“I see. How about this example? You’ve given me a good reason why you deserve a raise. Do you think it’ll fly with your boss?”
+Risky business
+“I don’t think it’ll fly with the boss. My cousin Suresh is in the film business. Writers send him scripts. He says that most of them just won’t fly.”
+“Making films is a risky business. So, what’s your boss going to do with your proposal? Is he going to throw it away or…?”
+“Since he likes it, he might just decide to put it on the back burner. You know just…”
+“Put something on the back burner? Does it mean keep it in cold storage?”
+“Something like that. When you put something on the back burner, you’re delaying doing something. You’re not going to act on it because it doesn’t require your immediate attention. My cousins are coming over next weekend. So my plans of going to Kodaikanal with my friends have been put on the back burner.”
+“It doesn’t mean you’re cancelling your trip, right?”
+“No! It means that I’m merely postponing it. I won’t be going next week, but sometime later. Both ‘not going to fly’ and ‘on the back burner’ are used in informal contexts.”
+“Because the students have been on strike, teachers have been compelled to put the unit tests and the assignments they give at this time of the month on the back burner.”
+“Good example.”
+“But tell me, what does this back burner refer to? Is it…”
+“It refers to the burners on a stove. Sometimes, you have two burners in the front and two in the back. Items that don’t require the cook’s immediate attention are usually put on the back burner.”
+“Could you make use of one of the front burners to make me a cup of coffee, please?”
+“When nothing is going right, go left.” — Anonymous`
+           }
+         },
+      {
+        type: 'passage',
+          label: 'Is bling in?',
+          id: '200',
+          data: {
+            title: `Is bling in?`,
+            text: `“That was a pretty long conversation. Were you talking to Sujatha?”
+“No, it was my cousin in Chennai. She’s leaving for the States tomorrow. Tell me, when someone picks up the phone, do you say, ‘May I speak to so and so’ or ‘May I speak with so and so’?”
+“Both are correct. You can ‘speak to’ or ‘speak with’ someone. In India, people....”
+“I see. But is there a difference in meaning between the two?”
+“Not really. ‘Speak to’ is much more common in India — the reason being the British use it quite a bit. Americans, on the other hand, tend to say ‘speak with’.”
+“I see. You look pretty tired. Was your flight from Kolkata delayed again?”
+“No, no! The flight was on time for a change. But we had to wait for our luggage...”
+“We? Who did you go with? Was it Jeevan from R&D?”
+“This was a sales conference. I went with Saroja Devi. She was...”
+“Saroja Devi? Is she the one who wears expensive jewellery and all kinds of...”
+“I’ve never seen Saroja wear bling. I think the person you have in mind...”
+“Bling? What are you talking about? What’s bling?”
+“In informal contexts, the word is used to refer to the expensive clothes or jewellery that people sometimes wear to parties...and on special occasions.”
+“So a bling is something that is easily noticeable, I guess.”
+“That’s right! People sometimes wear bling just to let others know that they have a lot of money.”
+“A status symbol! At the Oscars, some of the actors wore a lot of bling.”
+“Good example. It’s also possible to say ‘bling-bling’. The little girls were fascinated by the bling-bling that the young bride was wearing.”
+“If you had a lot of money, would you go to parties wearing all your bling?”
+“It’s difficult to say. I might...”
+“It’s difficult to picture you wearing bling. But stranger things have happened. Ganesh, for example, got his PhD recently. He can’t write and...”
+“He can’t speak either! But he’s got a PhD in English literature! Isn’t that amazing?”
+“I thought getting a PhD was difficult. How did the supervisor and the examiner....”
+“Ganesh’s supervisor was smart. She had the university send his dissertation to three of her friends — all teaching in other 
+universities.”
+“And all three okayed the dissertation?”
+Quid pro quo
+“Yes. It happens all the time. It’s a case of you scratch my back and I’ll scratch yours.”
+“Scratch your back? What are you talking about?”
+“It’s an expression mostly used in informal contexts. When you tell someone that you’ll scratch his back if he scratches yours, what you’re saying is that you’ll help him, provided he helps you in return.”
+“In other words, the help is conditional. How about this example? I’ll clean the car provided you use your influence and get me tickets to see Baahubali. You scratch my back and I’ll scratch yours.”
+“That’s a good example. Balan and Kavita are not great friends. But they do help each other once in a while. He scratches her back and she scratches his.”
+“I don’t think I’m keen on scratching anyone’s back right now.”
+I have a simple philosophy. Fill what’s empty. Empty what’s full. Scratch where it itches. — Alice Roosevelt Longworth`
+        }
+      },
+      {
+        type: 'passage',
+          label: 'Don’t beat around the bush',
+          id: '300',
+          data: {
+            title: `Don’t beat around the bush`,
+            text:`“What a downpour we had yesterday! The concert you were planning to attend must have been cancelled. There was        no way....”
+“Not a chance! Several hundred fans braved the rain to be...”
+“Braved the rain? Does it mean hundreds of fans were brave enough to show up... although it was pouring?”
+“You could say that. When you ‘brave something’, you face an unpleasant or a difficult situation. You deal with it courageously. You show no fear. For example, Indira braved the cold and took a dip in the freezing water.”
+“How about this example? My grandmother has stopped driving. She says she can no longer brave the chaotic traffic in our city.”
+“Sheba braved the wrath of her parents by choosing to major in Economics.”
+“They wanted her to become a doctor, didn’t they? Understandable, I guess. Both parents are doctors.”
+“So, are we going out tomorrow? Last week we’d planned to...”
+“I’m not really sure. I want to, you know that. But the problem is that there....”
+“When will you learn to stop beating about the bush? I need an answer right...”
+“Stop beating ‘about’ the bush? I always thought it was beat ‘around’ the bush.”
+“Both are correct. When you beat around/about the bush, you never get to the point. You talk about all kinds of things without really talking about the most important thing.”
+“Nobody in my family beats around the bush. If they have something to say, they say it.”
+“That’s not the case in my family. Many of my cousins never get to the point. They beat about the bush quite a bit — especially when they have to give you bad news.”
+“Our politicians beat around the bush all the time. It’s probably because...”
+“Forget about politicians. When will you let me know about...”
+“By this evening, definitely. Did you enjoy yourself last evening? Did you...”
+“Yes, it was quite enjoyable. They sang all my favourite songs. Unfortunately, a gaggle of students behind me made a lot of...”
+Noisy group
+“Gaggle of students? I thought ‘gaggle’ was used to refer to a flock of geese.”
+“You’re absolutely right! But in informal contexts, ‘gaggle’ can be used to refer to a group of noisy people — a very disorderly lot. The gaggle of reporters made it impossible to hear what the star was saying.”
+“How about this example? I went to the park for some peace and quiet. But a gaggle of children made it impossible to relax.”
+“That’s a pretty good example. The gaggle of protesters wouldn’t allow us to enter the building.”
+“I wanted to tell the kids to stop shouting... but they were having so much fun. I didn’t want to feel guilty of spoiling their fun.”
+“One doesn’t usually ‘feel guilty of’ something. One usually feels guilty ‘about’ something. For example, Sneha feels very guilty about not having visited her uncle when he was in hospital.”
+“I didn’t help Advaith when he was in trouble. I feel very guilty about it.”
+“I wish our politicians would feel guilty about taking bribes.”
+“No chance of that happening!”
+Conscience is what makes a boy tell his mother before his sister does. — Evan Esar`
+        }
+       },
+      {
+        type: 'passage',
+          label: 'Does this blook sit right with you?',
+          id: '400',
+          data: {
+            title: `Does this blook sit right with you?`,
+            text: `“Sorry I’m late. The traffic was really bad today, and I had a horrible time trying to...”
+“No need to explain. Tried reading this book I found on your table, and I...”
+“The book on the table? Oh, you mean the ‘blook’?”
+“Blook? I’ve never come across that word before.”
+“It’s a combination of ‘blog’ and ‘book’. A blook is actually...”
+“Let me guess. It’s a blog that’s been published as a book. It is...”
+“Very good! It may not be the entire blog. Sometimes, it’s just a few selections from it.”
+“This blook I’m reading is pretty bad. The author uses a lot of words I’ve never come across before. Tell me, how is ‘c..u..s..s..e..d pronounced?”
+“The ‘cuss’ rhymes with ‘bus’ and ‘fuss’ and the following ‘ed’ is like the ‘id’ in ‘hid’, ‘bid’ and ‘did’. The word is pronounced ‘CUSS-id’. Any idea what it means?”
+“That was my next question, actually! What does it mean? The writer talks about a cussed problem.”
+“When you refer to a person as being ‘cussed’, what you’re suggesting is that he is pretty annoying. He’s stubborn and rather unreasonable. This is just one of the meanings of this informal word. Nobody wants to work with Laxman because he is plain cussed.”
+“It’s very difficult to work with people who are cussed all the time. Last summer, we didn’t have power for three to four hours in the mornings. It became a cussed nuisance.”
+Disapproval
+“No power for four hours! I’m sure it didn’t sit right with your mother.”
+“Sit right with my mother? What are you talking about? What does...”
+“When someone does something and it doesn’t sit right with you, it means that you don’t approve of what he did. You’re rather unhappy. The expression can also be used to disagree with someone.”
+“How about this example? I tried telling my teacher why I hadn’t done my homework. The excuse I offered, however, didn’t sit right with her.”
+“That’s a good example. It’s also possible to say ‘to sit well with someone’. My sense of humour doesn’t sit well with some of my aunts and uncles.”
+“That’s understandable. You don’t have a sense of humour. The idea of going to parties and staying out late doesn’t sit well with my parents.”
+“It doesn’t sit right with most parents. So tell me, what do you...”
+“The fact that this blook is a bestseller doesn’t sit right with me.”
+“I don’t think the writer really cares what you think. Thanks to that blook, he’s got money coming out of his ears.”
+“Money coming out of his ears? Does it mean he’s making a lot of money?”
+“In that sentence that’s exactly what it means. When you say that ‘something is coming out of one’s ears’, it means that the person has a lot of it. The expression is mostly used in informal contexts.”
+“I see. Federer and Nadal have talent coming out of their ears.”
+“Very nice example. My neighbour is very inquisitive. He has questions coming out of his ears.”
+“Just like you!”
+Sure, reading a book under a tree is peaceful, but imagine how stressful it is for the tree to see a bunch of its dead friends in your hand. — Kyle Lippert`
+          }
+        }
+     ]
+  };
