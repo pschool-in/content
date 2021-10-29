af52d054b62185f0093ffaed1b8a87588c47cdff
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-10-30 01:46:33 +0530

    Update kye-qa-6.js

--- knowYourEnglish/playlists/kye-qa-6.js
+++ knowYourEnglish/playlists/kye-qa-6.js
@@ -1,1 +1,82 @@
-
+export default {
+    label: 'Know Your English Q & A',
+    id: 'kye-qa-6',
+    list: [
+      {
+        type: 'passage',
+        label: 'What’s your lucky charm?',
+        id: '100',
+        data: {
+          title: `What’s your lucky charm?`,
+          text: `What is the meaning of ‘dime a dozen’?
+This is an expression that is mostly used in informal contexts in American English. When you say that something is ‘dime a dozen’, you mean it is very common; it is abundant, and therefore of very little value. In the US, a ‘dime’ is a ten-cent coin. If you can buy a dozen items for ten cents, then the things that you are buying are very cheap indeed. The expression became popular in the 1930s.
+I can easily find someone to replace Vikram. Workers like him are dime a dozen.
+In our country, crooked politicians are dime a dozen.
+What is the meaning of ‘talisman’?
+First, let us deal with the pronunciation of this word. The first vowel sounds like the ‘a’ in ‘can’, ‘fan’ and ‘ban’, and the following ‘lis’ rhymes with ‘fizz’, ‘biz’ and ‘Ms.’. The final ‘a’ is like the ‘a’ in ‘china’. The word is pronounced ‘TA-liz-men’ with the stress on the first syllable. A talisman is your lucky charm; it is an object that you wear or keep because you believe that it has magical powers. It is something that will bring you good luck; keep you safe by warding off evil spirits. The object can be anything - ring, watch, stone, etc.
+When I fell ill, my grandmother kept a talisman under the pillow.
+I use this pen whenever I have an exam. It is my talisman.
+How is the word ‘recalcitrant’ pronounced? 
+The vowels in the first and third syllables are pronounced like the ‘i’ in ‘sit’, ‘bit’ and ‘kit’. The ‘cal’ in the second syllable rhymes with ‘pal’, ‘shall’ and ‘gal’. The final ‘a’ sounds like the ‘a’ in ‘china’. The word is pronounced ‘ri-KAL-sit-rent’ with the stress on the second syllable. ‘Re’ means ‘back’ and the Latin ‘calcitrare’ means ‘to kick’. The word literally means to ‘kick back’ or ‘kick with the heel’. Nowadays, ‘recalcitrant’ is mostly used to refer to a person who is difficult to handle or deal with. This individual does not like to be told to do things, and as a result, seldom obeys orders. He remains defiant. The word can be used with things as well.
+It’s time that we started disciplining the recalcitrant students.
+I don’t want on my team people who have the reputation of being recalcitrant.
+What is the difference between ‘count on someone’ and ‘count in someone’?
+The normal expression is ‘count someone in’ and not ‘count in someone’. When you count someone in, you include the person in a particular activity. The opposite is ‘count someone out’. When you ‘count on’ or ‘count upon’ someone, you are depending or relying on them for something. You expect them to be there for you.
+Ganesh is counting on his father to lend him the money.
+Please don’t let me down, Shekar. I’m counting on you.
+If you’re going trekking, you can count Bala out. He won’t come.
+Harish was looking for volunteers to help him clean the park. I told him to count me in.
+Every man has his follies – and often, they are the most interesting things he has got. — Josh Billings`
+         }
+       },
+        {
+        type: 'passage',
+        label: 'Gorilla in the room',
+        id: '200',
+        data: {
+          title: `Gorilla in the room`,
+          text: `What is the meaning of the expression ‘elephant in the living room’?
+ An elephant is a relatively large animal; it will be impossible to miss, especially in the confines of a living room. You can pretend to ignore it, but you will always be aware of its presence. The expression ‘elephant in the (living) room’ is mostly used to refer to a serious problem — one which people are aware of, but choose to ignore. They think that if they do not talk about it, the problem will somehow go away. Another expression which has the same meaning is ‘gorilla in the room’.
+Dowry — the elephant in the living room that neither party was keen on discussing.
+Does the word ‘cellfish’ exist? 
+Yes, it does; the word is a combination of ‘cell phone’ and ‘selfish’. When people get a call on their cell phone, they sometimes get so involved in their conversation that they forget their surroundings. We have seen drivers stop their vehicle in the middle of the road to answer the phone — ignoring the honking and swearing of their fellow drivers behind them. In our country, some people, as soon they board the train, take out their cell phone and proceed to talk loudly. These individuals are not really bothered if they are disturbing those around them. Using the cell phone in this manner, when you completely disregard the wishes and feelings of those around you, is described as ‘cellfish’.
+I told the idiot in the opposite berth to stop being cellfish. Who wants to listen to his marital problems in the middle of the night?
+How is the word ‘vide’ pronounced?
+There are several ways of pronouncing this word of Latin origin. One way is to pronounce the first vowel like the ‘ee’ in ‘feel’, ‘peel’ and ‘heel’, and the following ‘de’ like the word ‘day’. The word can be pronounced ‘VEE-day’ with the stress on the first syllable. It is mostly used in formal contexts - especially in academic writing - to mean ‘look at’ or ‘refer to’. When an author uses this word, he means that he would like the reader to refer to a particular book, document or page for additional information. In India, formal letters referring to an earlier correspondence usually contain this word.
+The comic genius has also played serious roles in films – vide The Truman Show.
+Is it okay to say ‘I heart you’?
+The expression seems to be used in the United States. We know that world over the shape of the heart is used as a symbol of love and affection.
+It should not come as a surprise, therefore, that the Americans use the expression ‘I heart you’ to mean ‘I love you’; they believe that it is a better way of saying the same thing. ‘I heart you’ is used by people who are reluctant to use the word ‘love’ when they wish to declare their feelings to their partner.
+According to some scholars, this expression became quite popular in 2012 when the R&B artist, Tony Braxton, released her single ‘I heart you’.
+People are prisoners of their phones, that’s why they’re called cell phones. - Anonymous`
+         }
+       },
+      {
+        type: 'passage',
+        label: 'Planning to regift this festive season?',
+        id: '300',
+        data: {
+          title: `Planning to regift this festive season?`,
+          text: `What is the meaning of ‘regift’? 
+We often receive gifts on our birthday that we do not particularly like or care for. Sometimes, two people give us the same gift. What do we do with them? Some people throw these gifts into their cupboards and forget all about them. The smart ones gift them to their friends or members of their family on their birthdays. This act of passing on a gift that you have received to someone else is called ‘regifting’. The friend/family member does not realise that you are merely getting rid of a gift that you do not like. The American comedy show, ‘Seinfeld’, was instrumental in popularising this word. ‘Regift’ can be used both as a noun and a verb. A person who regifts frequently is called a ‘regifter’.
+Harish was honest about it. He told me that the pen was a regift.
+Anil is planning to regift the ugly painting he got from Mala.
+What is the difference between ‘tiring’ and ‘tiresome’?
+When you say that an activity that you performed was rather ‘tiring’, what you are suggesting is that it made you extremely tired. In other words, it was exhausting; the activity left you rather fatigued. When you say that making conversation with someone is rather ‘tiresome’, what you are suggesting is that you find it rather boring or annoying. The word has a negative connotation.
+I’ve had a long, tiring day. So, let’s not go anywhere this evening.
+Watching the same movie every day can become quite tiresome.
+Filling out all these application forms is proving to be a tiresome process.
+How is the word ‘broach’ pronounced?
+The word rhymes with ‘coach’ and ‘poach’. When you ‘broach a subject’, you very hesitantly bring it up during a conversation or discussion. You are hesitant because you probably realise that it is a delicate matter; one that could either upset you or the person you are talking to. So, when you ‘broach’ something, you are introducing something new with a great deal of reluctance.
+The student wanted to change her supervisor. But she didn’t know how to broach the subject with the Dean.
+After dinner, Jaidev broached the subject of marriage with his daughter.
+What is the meaning and origin of ‘at the helm’?
+Drivers make use of the steering wheel to steer a vehicle in a particular direction. In the case of a boat or a ship, it is the ‘helm’ that functions as a steering wheel. The person who is ‘at the helm’ is referred to as the ‘helmsman’.
+Since he determined in which direction the ship would go, the expression ‘at the helm’ began to be used to refer to a leader or someone who was in charge of something. Why ‘at the helm’ and not ‘at the steering wheel’? People were using boats and ships long before motorcars had been invented.
+The company is doomed. The Board has decided that Sashi will be at the helm.
+Prema has been requested to be at the helm till she retires in July.
+Christmas is the season when you buy this year’s gifts with next year’s money. — Unknown`
+        }
+      }
+     ]
+  };
