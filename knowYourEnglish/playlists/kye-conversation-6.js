7cb3deb49a9a42be78fa2b807e8aed1cf6601d14
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-10-30 01:44:53 +0530

    Update kye-conversation-6.js

--- knowYourEnglish/playlists/kye-conversation-6.js
+++ knowYourEnglish/playlists/kye-conversation-6.js
@@ -1,1 +1,86 @@
-
+export default {
+    label: 'Know Your English',
+    id: 'kye-conversation-6',
+    list: [
+      {
+        type: 'passage',
+        label: 'Who’s your Man Friday?',
+        id: '100',
+        data: {
+          title: `Who’s your Man Friday?`,
+          text: `Hey, come on in! Good to see you. Happy New Year!”
+“How can it be a happy one when I have assignments and tests hanging over my head? It’s just crazy how many....”
+“Are you planning to be a grouch this year?”
+“A what?”
+“Grouch! It rhymes with ‘pouch’ and ‘crouch’. Do you know what grouch means?”
+“Someone who is grouchy. Someone who is in a terrible mood all the time.”
+“In other words, someone like you! A grouch is also a habitual complainer. For example, they tell me that Sandeep is the office grouch.”
+“No way! Why would they think that Sandeep is a grouch? My uncle is a grouch.”
+“According to my cousins, I’m the certified grouch in my family.”
+“Are they crazy? You’re no grouch. Why would they think that you....”
+“What’s gotten into you? Why so many questions? Is it because of the tests or....”
+“No, I’m just curious. Why would your cousins think that you....”
+“Curious, eh? Remember, curiosity killed the cat.”
+“Curiosity killed the cat? What are you talking about?”
+“It’s an expression used to warn someone. You’re telling the person not to poke his nose into other people’s business. Not to ask too many questions.”
+“In other words, not to get too curious about things.”
+“Exactly! If you do, chances are you’ll get into trouble. Whenever Sujatha asks too many personal questions, I remind her that curiosity killed the cat.”
+“Curiosity killed the cat! That’s what I’ll say the next time you ask a lot of questions. Tell me, how is your office grouch doing?”
+“Oh, Sandeep is doing fine. Getting used to his new job. Whenever he gets into trouble, his Man Friday helps him out.”
+“Man Friday? What are you talking about?”
+“The expression is normally used to refer to your right hand man. Someone...”
+“Someone who does whatever you want him to, and who can be trusted.”
+“Exactly! Man Friday was the name that Robinson Crusoe gave the man he rescued from cannibals. This man became his trusted servant and later his friend.”
+“In many of PG Wodehouse’s novels, Jeeves is Bertie Wooster’s Man Friday.”
+“That’s a good example. Nowadays, the expression is used to refer to one’s office assistant. At the office, Manjunath is my Man Friday.”
+“If the person helping you is a woman, then....”
+“You can refer to her as your ‘Girl Friday’. Pallavi is my boss’ Girl Friday.”
+“Mrs. Shyamala Rao is the Principal’s Girl Friday. But if you ask him, he’ll deny to it.”
+“One doesn’t ‘deny to’ something. One usually denies it. Sashi was accused of cheating people, but he denied it.”
+“The children denied they had taken money from their father’s wallet.”
+“Good example. On the witness stand, the Minister denied he taken a bribe.”
+“Do you think his Man Friday took it for him? It’s possible...”
+“Remember what happened to the cat who got too curious?”
+“May you spend lesser time online this New Year and more with the real people who fill up your world. Happy New Year!” Unknown`
+           }
+         },
+      {
+        type: 'passage',
+          label: 'Living in la–la land?',
+          id: '200',
+          data: {
+            title: `Living in la–la land?`,
+            text: `“We’re planning to see ‘La La Land’ in Prasad’s today. Does the title mean anything?”
+“La-la land is an expression used in informal contexts in American English. When you say that someone is in ‘la-la land’, you mean that the person has no clue what is going on around him. He is out of touch with reality.”
+“Like some of our politicians, I guess! How about this example? If you think that cricket is still a gentleman’s game, you’re living in la-la land.”
+“Sounds good! It’s difficult to talk to Ganesh. Most of the time he’s off in la-la land.”
+“I think I understand what la-la land means. Can we...”
+“The expression is also used to refer to the city of Los Angeles.”
+“Los Angeles? But what’s the connection between....”
+“Los Angeles is usually abbreviated to LA. And one of the things that LA is famous for is Hollywood.”
+“Hollywood movies. The world of make believe.”
+“That’s right. By the way, my friends and I went to the new restaurant you’d recommended. The food was okay, but the place had no zing.”
+“No zing? What are you talking about?”
+“There was no life in the place. It was very dull, absolutely no energy.”
+“I see. How about this example? Virat Kohli brings a lot of zing into the team.”
+“He certainly does, doesn’t he? On New Year’s Eve, my friends and I like to be in a place with a lot of zing in it.”
+“So, have you decided where you’ll be going this year? Two days back, you were....”
+“How many times do I have to tell you? It’s not two days back, but two days ago. I met my cousin six months ago. Not six months back.”
+“I went to Chennai a year ago. Now tell me, are you going to Goa....”
+“That’s where I’d like to go. But right now, among my friends, Bali seems to be the flavour of the month.”
+“Flavour of the month? Does it mean the most popular vacation spot? The one...”
+“When you say that something is the flavour of the month, what you mean is that it is the most popular thing at the moment. The expression can be used with people as well. As far as tennis is concerned, Del Potro seems to be the flavour of the month.”
+“My father tells me that bell bottoms were the flavour of the month in the early 70s.”
+“That’s a good example. Sindhu is the flavour of the month with my boss. He thinks....”
+“What about you? Were you ever the flavour of the month?”
+“Never! I made the mistake of zinging his very first proposal, and since then...”
+“Zinging his proposal? Does it mean you enthusiastically supported the proposal?”
+“I did the exact opposite. The word ‘zing’ can be used as a verb to mean to criticise. The Opposition is taking great delight in zinging the policies of the PM.”
+“President-Elect Trump gets zinged quite a bit on the late-night comedy shows.”
+“In Parliament, our politicians waste their time and our money zinging each other.”
+“That’s true!
+In Los Angeles, everything is 100 per cent organic, except the people. — Kris Kidd`
+       }
+     }
+    ]
+  };
