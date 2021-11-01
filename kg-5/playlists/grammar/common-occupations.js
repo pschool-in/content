dbb53d3fa00026ff8fe4df8b680b3c71592428ad
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-11-01 23:01:59 +0530

    Update common-occupations.js

--- kg-5/playlists/grammar/common-occupations.js
+++ kg-5/playlists/grammar/common-occupations.js
@@ -1,1 +1,337 @@
+export default {
+    id: 'Clauses',
+    label: 'Clauses',
+    pdf: '',
+    list: [
+      {
+        label: 'Common Occupations-1',
+        type: 'passage',
+        id: 'common-occupations-1-reading',
+        data: {
+          title: 'Common Occupations-1',
+          text: `# 1. Teacher
+          A teacher helps us acquire knowledge. A good teacher has a very important role in a student's life. They lay foundations of their values, knowledge and principles. We must always respect our teachers.
 
+# 2. Engineer
+Engineers are experts in designing, analytics, machines, gadgets, software, etc. It is one of the most popular profession among science students. Engineers work in technology companies, start-ups, fields, etc.
+
+# 3. Potter
+A potter is a craftsman who is an expert in pottery. They shape their pottery on a potter's wheel. Pottery is anything that is made of clay.
+
+# 4. Mechanic
+A mechanic is an expert who repairs and maintains vehicles, television, refrigerator and other machines. We can go to a mechanic's shop for repairing or can call them at our home for the same.
+
+# 5. Farmer
+A farmer is the backbone of agriculture. Farmers work in fields and grow crops. They sell cultivated crops to the vendors and then we can buy them. Farmers are the reason we get food. Do you know that agriculture is a major occupation in India?
+
+# 6. Photographer
+A photographer is a person who is an expert at taking perfect pictures. There are different types of areas where photographers work like weddings, company events, wildlife, travel, film industry, modeling industry, etc.
+
+# 7. Actor
+An actor/actress is a person who has skills for acting. They can easily play the role of any character assigned to them. Actors work in performing arts like theatre, movies, plays, shows, street plays, etc.
+
+# 8. Dancer
+A dancer is a person who is very good at dancing. A dancer knows or can learn different forms of dancing such as Kathak, classical, hip-hop, pop, freestyle, contemporary, etc. Dancing is a part of performing arts.
+
+# 9. Watchman
+A watchman or a guard is a person who ensures the safety of a building. They carry a stick, torch and sometimes a gun for safety. They work at banks, apartment buildings, bungalows, companies, etc. They protect the places from thieves and robbers.
+
+# 10. Gardener
+A gardener is a person who takes care of the gardens. They know exactly which plants can grow well in the garden, depending upon the season. They take good care of the plants by watering them with the right amount, adding necessary manures and fertilizers in the soil, etc. The credit for a good lawn always goes to the gardener.
+
+# 11. Doctor
+A doctor works in the health sector of the country. Doctors take care of our health and attend to any problems related to it. We can find doctors in clinics and hospitals. They're trained to do the best they can to make people healthy and disease-free.
+
+# 12. Barber
+We go to a barber each time we need a haircut. Barbers are experts who have the skills to cut our hair perfectly. They also help us style our hair better and make us look good. Barbers work at saloons.
+
+# 13. Tailor
+A tailor sews clothes. They can make shirts, pants, suits, salwar, uniforms, jeans and even skirts. They take our body measurements and start working on our clothes. We just need to give them a piece of cloth and tell them how we want it.
+
+# 14. Weaver
+A weaver is an expert in weaving. There are two types of weavers. Handweavers do weaving by hand. As a result, they have control over the quality of the final product. The second type of weaver is the operator. They operate the weaving machines in factories.
+
+# 15. Goldsmith
+A goldsmith is a person who specializes in working with gold and other precious metals. They mainly specialize in making jewelry and other decoratives. They do this by cutting, melting, beating and polishing the precious metals and stones.`
+        }
+      },
+      {
+        label: 'Common Occupations-2',
+        type: 'passage',
+        id: 'common-occupations-2-reading',
+        data: {
+          title: 'Common Occupations-2',
+          text: `# 1. Nurse
+A nurse provides care to the patients in the hospitals. They help them get rid of their pain and discomfort. They also educate them about their health condition and what they can do to get better. They also educate them on how to stay healthy.
+
+# 2. Singer
+A singer has a melodious voice. They are trained in the field of music. They know sounds and rythmns. They work in the film industry, temples, as music teachers, in schools etc. Many famous singers organize their concerts where people go to listen to them live.
+
+# 3. Plumber
+A plumber is a person who has the skills to repair and replace the taps and pipelines. They can fix the leaking or running taps, can change damaged pipelines, can detect the problems we're unaware of, etc.
+
+# 4. Shepherd
+This is one of the oldest occupations. A shepherd is a person who has a flock of sheep. He rears them for wool and milk. A shepherd works in the agriculture sector and animal husbandry.
+
+# 5. Fisherman
+A fisherman captures fishes and other animals from water bodies. They also gather shells from the shores. They sell the caught fishes in the market, to the local vendors, in the food industry or they simply keep it for themselves.
+
+# 6. Police
+The police are a group of policemen. They're trained to protect the citizens from threats, accidents, thieves, robbery, etc. They wear khaki clothes. It's their uniform. They maintain the law and order in the country.
+
+# 7. Cobbler
+A cobbler is a person who makes and repairs shoes. They replace torn soles. They can stitch the torn part of shoes. They can also repair heels and slippers.
+
+# 8. Carpenter
+Carpenters are people skilled with wooden framework. They are experts in making furniture, wooden roofs, wooden floors, tree houses, etc. Their work area is usually construction and furniture companies.
+
+# 9. Shopkeeper
+A shopkeeper is a person who owns a shop or works in a shop. They assist us each time we visit. They show us variety in what we want. They help us choose the products that would be best for us. They take care of the shop and maintain it.
+
+# 10. Electrician
+An electrician has complete knowledge about the electric wires. They come in handy each time we face fluctuations in current or powercut. Electricity can be very dangerous and therefore we must always contact an electrician if we face any problem.
+
+# 11. Baker
+A baker is a person who works at a bakery. They sell and bake all the baked food like cakes, pastries, pancakes, muffins, donuts, etc.
+
+# 12. Writer
+Being a writer is a very creative job. A writer is very innovative. Their choice of words makes the stories and poems very interesting. A good writer can have one or multiple styles of expressing their ideas. The most common styles are novels, poems, dramas, plays, and stories.
+
+# 13. Driver
+A driver is skilled in driving. They drive public vehicles like cabs, buses, trucks, tractors, trains, airplanes, etc. Many people hire drivers to drive their personal vehicles as well.
+
+# 14. Salesman
+A salesman is also called a sales representative. They basically sell services or products to businesses or customers. They can be door-to-door sellers or freelancers.
+
+# 15. Blacksmith
+A blacksmith is a person who makes different things out of scrap metals like iron or steel. They do this by hammering, melting, shaping, etc.`
+      }
+    },
+    {
+        id: 'match-1',
+        label: 'Match the following',
+        type: 'match',
+        data: {
+          text: `Doctor, Hospital
+          Shopkeeper, Shop
+          Fisherman, Sea
+          Driver, Vehicles
+          Teacher, School`,
+          title: 'Match the pairs.'
+        }
+      },
+      {
+        id: 'match-2',
+        label: 'Match the following',
+        type: 'match',
+        data: {
+          text: `Blacksmith, Scrap
+          Salesman, Sells
+          Writer, Books
+          Baker, Bakery
+          Electrician, Wires`,
+          title: 'Match the pairs.'
+        }
+      },
+      {
+        id: 'match-3',
+        label: 'Match the following',
+        type: 'match',
+        data: {
+          text: `Carpenter, Wood
+          Cobbler, Shoes
+          Police, Safety
+          Shepherd, Flock
+          Plumber, Pipes`,
+          title: 'Match the pairs.'
+        }
+      },
+      {
+        id: 'match-4',
+        label: 'Match the following',
+        type: 'match',
+        data: {
+          text: `Nurse, Hospital
+          Singer, Melody
+          Goldsmith, Gold
+          Barber, Haircut
+          Tailor, Clothes`,
+          title: 'Match the pairs.'
+        }
+      },
+      {
+        id: 'match-5',
+        label: 'Match the following',
+        type: 'match',
+        data: {
+          text: `Weaver, Weaving
+          Gardner, Lawns
+          Potter, Clay
+          Farmer, Field
+          Dancer, Performance`,
+          title: 'Match the pairs.'
+        }
+      },
+      {
+        label: 'True or False',
+        id: 'TF-1',
+        type: 'classifySentence',
+        data: {
+          title: 'State whether these statements are true or false.',
+          types: [
+            {
+              name: 'True',
+              text: `A cobbler is a person who repairs shoes.
+              Shepherd is one of the oldest occupations.
+              A singer has a melodious voice.`
+            },
+            {
+              name: 'False',
+              text: `Dancers work in hospitals.
+              Shopkeeper helps the patients.
+              An actor is an expert in dancing.`
+            }
+          ]
+        }
+      },
+      {
+        label: 'True or False',
+        id: 'TF-2',
+        type: 'classifySentence',
+        data: {
+          title: 'State whether these statements are true or false.',
+          types: [
+            {
+              name: 'True',
+              text: `A farmer is the most important unit of agriculture.
+              We have no threat of robbery if we have a watchman.
+              A potter makes pottery on a spinning wheel.`
+            },
+            {
+              name: 'False',
+              text: `Gardner takes care of the law.
+              A weaver sews our clothes as per our demands.
+              Goldsmith makes things out of scrap.`
+            }
+          ]
+        }
+      },
+      {
+        label: 'True or False',
+        id: 'TF-3',
+        type: 'classifySentence',
+        data: {
+          title: 'State whether these statements are true or false.',
+          types: [
+            {
+              name: 'True',
+              text: `Blacksmith moulds the scrap into usable products.
+              A tailor sews our clothes as per our demands.
+              Barber styles our hair.`
+            },
+            {
+              name: 'False',
+              text: `A carpenter works with shoes.
+              A baker serves people at a restaurant.
+              A plumber helps to repair vehicles.`
+            }
+          ]
+        }
+      },
+      {
+        label: 'Multiple Choice Questions',
+        id: 'mcq',
+        type: 'mcq',
+        data: {
+          title: 'Choose the correct answer.',
+          questions: [
+            {
+              qText: 'Who assists patients in the hospital?',
+              options: 'Nurse, Teacher, Watchman'
+            },
+            {
+              qText: 'Who is an expert in jewelry-making?',
+              options: 'Goldsmith, Blacksmith, Cobbler'
+            },
+            {
+              qText:
+                'Who is skilled with the wooden frameworks?',
+              options: `Carpenter, Blacksmith, Cobbler`
+            },
+            {
+              qText: 'Who makes cakes and pastries?',
+              options: 'Baker, Nurse, Teacher'
+            },
+            {
+              qText: 'Who maintains law and order?',
+              options: 'Police, Conductor, Salesman'
+            }
+          ]
+        }
+      },
+      {
+        id: 'Jumbled-words-1',
+        type: 'sequence',
+        label: 'Jumbled Words',
+        data: {
+          title: 'Identify these occupations.',
+          text: 'Police, Doctor, Driver, Tailor, Barber'
+        }
+      },
+      {
+        id: 'Jumbled-words-2',
+        type: 'sequence',
+        label: 'Jumbled Words',
+        data: {
+          title: 'Identify these occupations.',
+          text: 'Weaver, Baker, Nurse, Writer, Actor'
+        }
+      },
+      {
+        label: 'Fill in the blanks. (Fill by drag)',
+        type: 'matchByDragDrop',
+        id: 'drag-drop-1',
+        data: {
+          isPractice: false,
+          title: 'Fill in the blanks by dragging the correct option.',
+          styles: {
+            fontSize: '1.3rem',
+            dashWidth: 80
+          },
+          text: `A *teacher* works in schools.
+          A *singer* has a melodious voice.
+          A *writer* is very creative.
+          A *barber* styles our hair.
+          A *salesman* sells products and services.`
+        }
+      },
+      {
+        id: 'fillup',
+        label: 'Fill up with options',
+        type: 'fillupOptions',
+        data: {
+          text: `A *blacksmith (goldsmith)* works with scrap.
+          A *doctor (nurse)* prescribes medicines.
+          A *teacher (dancer)* teaches values.
+          A *shopkeeper (salesman)* sells products in a shop.
+          An *electrician (plumber)* deals with wires.`,
+          title: 'Tap on each blank to view options.'
+        }
+      },
+      {
+        type: 'rightOne',
+        label: 'Correct Spelling',
+        id: 'correct-spelling',
+    
+        data: {
+          title: 'Identify the correct spelling.',
+          text: `Police, Poliece
+          Dancer, Danser
+          Barber, Barbar
+          Writer, Riter
+          Cobbler, Cobblar`
+        }
+      }
+    ]
+  };
+        
