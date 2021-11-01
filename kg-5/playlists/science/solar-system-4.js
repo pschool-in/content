b4c975b9e29fdad068d93e206bf3d4c0d2f5875a
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2021-11-01 13:56:35 +0530

    Update solar-system-4.js

--- kg-5/playlists/science/solar-system-4.js
+++ kg-5/playlists/science/solar-system-4.js
@@ -54,7 +54,7 @@ export default {
       id: 'true-false',
       type: 'classifySentence',
       data: {
-        title: 'Classify the below sentences as true and false',
+        title: 'Classify the below sentences as true or false',
         types: [
           {
             name: 'True',
