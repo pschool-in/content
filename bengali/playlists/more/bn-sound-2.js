bb2a5224462aeaa6aca0c0923badbd3fa6f6f4b2
Atish Sihi <85349281+atishsihi@users.noreply.github.com>
2021-11-02 18:53:17 +0530

    Update bn-sound-2.js

--- bengali/playlists/more/bn-sound-2.js
+++ bengali/playlists/more/bn-sound-2.js
@@ -1,13 +1,13 @@
 export default {
-  label: 'ধ্বনি',
+  label: 'ধ্বনি - 2',
   id: 'bn-sound-2',
-  img: 'sound',
-  lockAfter: 7,
+  //img: 'sound',
+  // lockAfter: 7,
   list: [
     {
       type: 'rightOne',
       label: 'ধ্বনি শুনে শব্দ শনাক্ত করুন - ফল, ফুল, শাকসবজি',
-      id: 'pick-word-fruitveg',
+      id: 'pick-word-fruitveg-1',
       lockAfter: 4,
       commonData: {
         title: 'ধ্বনি শুনে শব্দ ক্লিক করুন',
@@ -15,41 +15,46 @@ export default {
       },
       data: [
         {
+          audio: 'bn-flowers-bodyParts-kitchen.mp3',
+          audioOffset: 0,
+          words: 'গোলাপ, পদ্ম, সূর্যমুখী, গাঁদা, জবা, জুঁই'
+        },
+        {
           audio: 'bn-fruits.mp3',
           audioOffset: 0,
           words:
-            'apple, grapes, banana, guava, jackfruit, mango, orange, papaya'
+            'আপেল, আঙুর, কলা, পেয়ারা, কাঁঠাল, আম, কমলা লেবু, পেঁপে'
         },
         {
           audio: 'bn-fruits.mp3',
           audioOffset: 16,
           words:
-            'pineapple, pomegranate, watermelon, fig, custard-apple, dates, muskmelon'
+            'আনারস, ডালিম, তরমুজ, ডুমুর, আতা, খেজুর, খরবুজ'
         },
         {
           audio: 'bn-vegetables.mp3',
           audioOffset: 0,
           words:
-            'carrot, beetroot, onion, tomato, potato, cabbage, cauliflower, corn, cucumber, garlic'
+            'গাজর, বিট, পেঁয়াজ, টমেটো, আলু, বাঁধাকপি, ফুলকপি, ভুট্টা, শসা, রসুন'
         },
         {
           audio: 'bn-vegetables.mp3',
           audioOffset: 20,
           words:
-            'ginger, pumpkin, almond, bitter-gourd, brinjal, cashewnut, chilli, coconut, corianderleaf, curryleaf'
+            'আদা, কুমড়, বাদাম, করলা, বেগুন, কাজুবাদাম, লঙ্কা, নারকেল, ধনেপাতা, কারিপাতা'
         },
         {
           audio: 'bn-vegetables.mp3',
           audioOffset: 40,
           words:
-            'drumstick, greenpeas, ladies-finger, mintleaf, peanut, raddish, spinach'
+            'সজনেডাঁটা, মটরশুটি, ভিন্ডি, পুদিনা, চিনাবাদাম, মূলো, পালং শাক'
         }
       ]
     },
     {
       type: 'rightOne',
       label: 'ধ্বনি শুনে শব্দ শনাক্ত করুন - পশু, পাখি',
-      id: 'pick-word-animalbird',
+      id: 'pick-word-animalbird-1',
       lockAfter: 4,
       commonData: {
         title: 'ধ্বনি শুনে শব্দ ক্লিক করুন',
@@ -59,45 +64,45 @@ export default {
         {
           audio: 'bn-animals.mp3',
           words:
-            'cat, dog, goat, sheep, horse, pig, elephant, lion, monkey, tiger'
+            'বিড়াল, কুকুর, ছাগল, ভেড়া, ঘোড়া, শূকর, হাতি, সিংহ, বাঁদর, বাঘ'
         },
         {
           audio: 'bn-animals.mp3',
           audioOffset: 20,
           words:
-            'hippo, rhino, fox, crocodile, bear, camel, shark, whale, dolphin, dragonfly'
+            'জলহস্তী, গণ্ডার, শিয়াল, কুমির, ভালুক, উট, হাঙ্গর, তিমি মাছ, শুশুক, ফড়িং'
         },
         {
           audio: 'bn-animals.mp3',
           audioOffset: 40,
           words:
-            'fish, frog, crab, snake, squirrel, tortoise, rat, winged-termites, ant, fly'
+            'মাছ, ব্যাঙ, কাঁকড়া, সাপ, কাঠবিড়ালি, কচ্ছপ, ইঁদুর, উইপোকা, পিঁপড়ে, মাছি'
         },
         {
           audio: 'bn-animals.mp3',
           audioOffset: 60,
-          words: 'bat, buffalo, butterfly, cheetah, cow, donkey'
+          words: 'বাদুড়, মোষ, প্রজাপতি, চিতাবাঘ, গরু, গাধা'
         },
         {
           audio: 'bn-animals.mp3',
           audioOffset: 72,
-          words: 'mongoose, mosquito, wolf, cockroach, lizard, rabbit'
+          words: 'বেজি, মশা, নেকড়ে বাঘ, আরশোলা, টিকটিকি, খরগোশ'
         },
         {
           audio: 'bn-birds.mp3',
-          words: 'crow, dove, duck, hen, parrot, peacock, sparrow, chick, crane'
+          words: 'কাক, পায়রা, হাঁস, মুরগি,টিয়া পাখি, ময়ূর, চড়ুই, মুরগিছানা, সারস পাখি'
         },
         {
           audio: 'bn-birds.mp3',
           audioOffset: 18,
-          words: 'kingfisher, myna, ostrich, owl, rooster, vulture, woodpecker'
+          words: 'মাছরাঙ্গা, মায়না, উটপাখি, পেঁচা, মোরগ, শকুন, কাঠঠোকরা'
         }
       ]
     },
     {
       type: 'rightOne',
       label: 'ধ্বনি শুনে শব্দ শনাক্ত করুন - যানবাহন, অন্যান্য',
-      id: 'pick-word-others',
+      id: 'pick-word-others-1',
       commonData: {
         title: 'ধ্বনি শুনে শব্দ ক্লিক করুন',
         type: 'words'
@@ -107,50 +112,46 @@ export default {
           audio: 'bn-vehicles.mp3',
           audioOffset: 0,
           words:
-            'aeroplane, boat, bus, car, ship, train, auto, cart, excavator, fire-engine'
+            'বিমান, নৌকা, বাস, কার, জাহাজ, রেল গাড়ি, অটো, গোরুর গাড়ি, খননকারী যন্ত্র, দমকল'
         },
 
         {
           audio: 'bn-others.mp3',
           audioOffset: 0,
-          words: 'house, table, bed, candle, chair, shelf, baby, boy, girl'
+          words: 'ঘর, টেবিল, বিছানা, মোমবাতি, কেদারা, তাক, বাচ্চা, ছেলে, মেয়ে'
         },
         {
           audio: 'bn-others.mp3',
           audioOffset: 18,
           words:
-            'man, woman, glasses, clock, watch, pen, scissors, waterbottle, book, color-pencils'
+            'পুরুষ, মহিলা, চশমা, ঘড়ি, হাতঘড়ি, কলম, কাঁচি, জলের বোতল, বই, রঙ পেন্সিল'
         },
         {
           audio: 'bn-others.mp3',
           audioOffset: 38,
-          words: 'sun, moon, rain, star, milk, ring, bone, bell, bird, conch'
+          words: 'সূর্য, চাঁদ, বৃষ্টি, তারা, দুধ, আংটি, হাড়, ঘণ্টা, পাখি, শঙ্খ'
         },
         {
           audio: 'bn-others.mp3',
           audioOffset: 58,
-          words: 'drums, five, food, fruits, king, kite, ladder'
+          words: 'ঢাক, পাঁচ, খাবার, ফল, রাজা, ঘুড়ি, মই'
         },
         {
           audio: 'bn-others.mp3',
           audioOffset: 72,
-          words: 'ladoo, leaf, rishi, wheel, whistle, world'
-        },
-        {
-          audio: 'bn-flowers-bodyParts-kitchen.mp3',
-          audioOffset: 0,
-          words: 'গোলাপ, পদ্ম, সূর্যমুখী, গাঁদা, জবা, জুঁই'
+          words: 'লাড্ডু, পাতা, ঋষি, চাকা, বাঁশি, পৃথিবী'
         },
         {
           audio: 'bn-flowers-bodyParts-kitchen.mp3',
           audioOffset: 12,
-          words: 'ear, eye, foot, hair, hand, mouth, nose, tooth'
+          words: 'কান, চোখ, পা, চুল, হাত, মুখ, নাক, দাঁত'
         },
         {
           audio: 'bn-flowers-bodyParts-kitchen.mp3',
           audioOffset: 28,
-          words: 'stove, fork, knife, pan, spoon, pot, butter'
+          words: 'চুলা, কাঁটাচামচ, ছুরি, চাটু, চামচ, পাত্র, মাখন'
         },
-    }
+        ]
+      }
   ]
 };
