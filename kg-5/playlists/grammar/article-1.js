f2a09456f0d358746ac628c63eaf4f35b9510fd0
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-11-02 17:55:20 +0530

    Update article-1.js

--- kg-5/playlists/grammar/article-1.js
+++ kg-5/playlists/grammar/article-1.js
@@ -1,1 +1,155 @@
+export default {
+    id: 'article-1',
+    label: 'Article-1',
+    pdf: '',
+    list: [
+      {
+        label: 'Article-1',
+        type: 'passage',
+        id: 'article-1-reading',
+        data: {
+          title: 'Articles',
+          text: `The words that help define the naming words (nouns) as specific and non-specific are called as articles. The choice between a and an is determined by sound. These are called as indefinite articles because it leaves the person or the thing spoken of unclear. For example a doctor, that means any doctor.
 
+          an is used before the naming words that begin with a,e,i,o,u which are called as vowels.
+           For example:
+          1. an animal
+          2. an umbrella
+          3. an owl
+          4. an eagle
+          5. an ice-cream
+
+          a is used before the naming word that begins with consonants or the other remaining letters.
+          For example:
+          1. a tree
+          2. a bird
+          3. a cup
+          4. a pen
+          5. a dog`
+        }
+      },
+      {
+        id: 'fill-up-1',
+        label: 'Addition of Articles',
+        type: 'fillup',
+        commonData: {
+          title: `Click on the blank and choose a or an before the following naming words`
+        },
+        data: [
+          `*a(an)* lion
+          *an(a)* orange
+          *a(an)* peacock
+          *a(an)* tailor 
+          *an(a)* eye`,
+
+          `*a(an)* fish
+          *a(an)* docotor
+          *an(a)* engine
+          *a(an)* ball
+          *an(a)* egg`,
+
+          `*a(an)* ballon
+          *an(a)* ant
+          *an(a)* aeroplane
+          *a(an)* mat
+          *an(a)* engine`,
+  
+          `*a(an)* banana
+          *a(an)* car
+          *a(an)* paper
+          *a(an)* bat
+          *an(a)* axe`,
+
+          `*a(an)* flower
+          *an(a)* inkpot
+          *a(an)* parrot
+          *a(an)* box
+          *an(a)* elbow`,
+
+          `*a(an)* woman
+          *an(a)* officer
+          *a(an)* bottle
+          *a(an)* baby
+          *an(a)* urn`,
+
+          `*a (an)* guitar
+          *a (an)* pony tail
+          *an (a)* apple
+          *a (an)* pencil
+          *an (a)* elastic`,
+        ]
+      },
+      {
+        id: 'fill-up-2',
+        label: 'Addition of naming words',
+        type: 'fillup',
+        commonData: {
+          title: `Click on the blank and add the correct naming word in front of the articles.`
+        },
+        data: [
+          `an *umbrella (cat)*
+          an *ice-cream (pot)*
+          an *engineer (pen)*
+          a *toy (eagle)*
+          an *accident(ring)*`,
+
+          `a *lion (elephant)*
+          an *hour(clock)*
+          an *honest man(bad man)*
+          a *car(owl)*
+          a *cherry(apple)*`,
+
+         `an *Eagle (crow)*
+         an *honour(dishonour)*
+         an *apron(coat)*
+         a *lamp (inkpot)*
+         an *egg (hen)*`
+        ]
+      },
+      {
+        id: 'fill-up-3',
+        label: 'Fill in the blanks',
+        type: 'fillup',
+        commonData: {
+          title: `Click on the blank and choose the correct article in the sentence`
+        },
+        data: [
+          `There is *a (an)* dog in the backyard.
+          I see *an (a)* owl on the tree!
+          Mom has *a (an)* blue dress.
+          There is *a(an)* cup on the table.
+          Manish eats *an (a)* orange for breakfast.`,
+
+          `Meera is *an (a)* intelligent student.
+          Dad wants me to wear *a (an)* cap.
+          Reema has *a(an)* jewelry box.
+          We put up *a (an)* Christmas tree for holidays.
+          Grandma read *a (an)* story to all my friends.`,
+
+          `I want to eat *an (a)* apple.
+          I bought *an (a)* umbrella for my brother.
+          Aladdin had *a (an)* magic lantern.
+          I saw *a (an)* big lion at the zoo.
+          She has written *a (an)* story.`,
+
+          `Rakesh's father is *a (an)* lawyer.
+          This is *a (an)* ball.
+          I saw *an (a)* old carpet.
+          We have *a (an)* red car.
+          Saanvi is *an(a)* honest girl.`,
+
+          `There is *a (an)* beehive.
+          Pass me *an(a)* apple.
+          Mohan has *a (an)* bike.
+          Ritu is *an (a)* Indian.
+          Krishna wants *an(a)* orange.`,
+  
+          `Mayukha bought *a (an)* flower.
+          They live in *a(an)* small hut.
+          She is not *an(a)* old lady.
+          My dad works in *a(an)* factory.
+          My brother has *a (an)* toy laptop.`
+        ]
+      }
+    ]
+  };
