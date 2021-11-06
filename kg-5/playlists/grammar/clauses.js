9fc4b58bb7b747c87cdbec1c346490bb35f3bc35
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-11-06 16:21:48 +0530

    Update clauses.js

--- kg-5/playlists/grammar/clauses.js
+++ kg-5/playlists/grammar/clauses.js
@@ -1,1 +1,145 @@
+export default {
+    id: 'clauses',
+    label: 'Clauses',
+    pdf: '',
+    list: [
+      {
+        label: 'Clauses - Reading',
+        type: 'passage',
+        id: 'clauses-reading',
+        data: {
+          title: 'Clauses',
+          text: `A clause is a group of words that consists of a subject and a verb. For example- In the following sentences, the groups of words that are in italics are clauses.
+1. People who pay their debts are trusted.
+2. We cannot start while it is raining.
+3. think that you have made a mistake.
 
+#Types of Clauses
+There are two types of clauses
+1. Independent Clause
+2. Dependent Clause
+
+Independent Clauses express a complete thought and has a subject and a verb. They can stand alone and can be used as a simple sentences.
+
+Examples: I want some cereal. Marie likes cats. Joseph is a good football player.
+
+Dependent Clauses has a subject and a verb, but cannot be used as a simple sentence. A dependent clause is introduced by a dependent word (subordinating conjunction). See the following list :
+
+After, although, as, as if, because, before even if, even though, ever since, how, if in order that, since, so, so that, than, that though, unless, until, what, whatever, when whenever, where, whereas, wherever, whether which, whichever, while, whom, whose, why
+
+Whenever a clause begins with one of the above dependent words (except questions) it is called a dependent clause. For example if we take an independent clause and add a dependent word like -
+
+We finished the game - Independent clause (IC) After we finished the game- dependent clause ( DC) Although we finished the game. (DC) Before we finish the game. (DC)`
+        }
+      },
+      {
+        type: 'selectWord',
+        label: 'Identify the clause',
+        id: 'select-word',
+        commonData: {
+          title: 'Select the clause in the below sentence.'
+        },
+        data: [
+`*He is standing* on the roof.
+*The cat is sleeping* under the table.
+*She is drawing a map* on the wall.
+*He is waiting* at the door.
+*He is dancing* on the floor.`,
+
+`*She arrived to work on time* in spite of leaving home so late.
+*I was standing* near a wall.
+*She made tea* for the guests.
+*He bought a book* for a friend.
+*She was dancing* in the rain.`,
+
+`*The people are dancing* at the party.
+*He reached school* in time.
+*I will meet him* in my office.
+*She wrote a poem* on a piece of paper.
+*Kiran poured the mango juice* in the bottle.`,
+        ]
+      },
+      {
+        type: 'selectWord',
+        label: 'Identify the independent clause',
+        id: 'select-word-2',
+        commonData: {
+          title: 'Select the independent clause (IC) from each sentence'
+        },
+        data: [
+`*I am saving my money* because I wanted to buy a new Ipad. 
+When I arrived, *Jimmy was surprised to see me*.
+Because I don't know how to do my homework, * I need to call Kiran for help*. 
+*Cindy loves to read*.
+Although we were late, *we decided to watch the movie anyway*.`,
+
+`*I forgot most of the groceries I needed* because I forgot my list. 
+*Tomorrow is going to be an exciting day* because I am going to a movie. 
+If you want to ride with Meena and Ravi, *you need to let them know*. 
+*Nobody knows the answer to that question*. 
+*I went to the park*.`,
+        ]
+      },
+      {
+        type: 'selectWord',
+        label: 'Identify the Dependent Clause',
+        id: 'select-word-3',
+        commonData: {
+          title: 'Select the dependent clause from each sentence'
+        },
+        data: [
+        `My mother is a nurse *since she likes to help people*. 
+        *After the storm* there were branches in the streets.
+        I asked the question *that no one else wanted to ask*.
+        Ravi eats carrots *when he wants to have a snack*.
+        *If you want to be my friend* you will tell me the truth.`,
+
+        `I am not cleaning the dishes *unless pavan helps*.
+        I made good money *until I lost my job*. 
+        *After swimming in the pool*. 
+        *Because he ran* he was able to catch the bus. 
+        *Until the sun sets*, we are going to play.`,
+
+        `*As the lights turn off*, I will go to sleep. 
+        *Wherever he might go*, I'll follow him. 
+        *Since I went back to school* I am feeling happy.
+        *Whenever you come to visit*, make sure to bring food. 
+        *Unless you have the right size * don't try it. 
+        *When it will start to snow* we will go skiing. 
+        *If the dress is on sale* she will buy it.`,
+      ]
+    },
+    {
+      type: 'classifySentence',
+      label: 'Classify',
+      id: 'identify-type',
+      data: {
+        title: 'Identify the underlined part of the sentence as a dependent clause (DC) or independent clause (IC)',
+        types: [
+          {
+            name: 'Dependent Clause',
+            text: `Mahita bought some flowers *which she wanted to give her mother*.
+            He drove fast *so that he could reach the airport on time*.
+            I don't know the reason *why she dislikes me*.
+            The gardner said *that all the plants had been pruned neatly*. 
+            I wondered *if she was free*.
+            The boys *who misbehaved in the class* were suspended.
+            This is the homework assignment *that you missed last week*.
+            *When I went to the zoo*, I looked at the animals
+            The teacher *who lives next door to Raj* is Mrs. Iyer.`
+          },
+          {
+            name: 'Independent Clause',
+            text: `*We went for a walk* after the rain stopped. 
+            Until the sun sets *I will stay out and play*. 
+            Because the heavy rains knocked out the power, *school will be closed on Friday*.
+            *The movie was good* although it was too long. 
+            Though he didn't receive formal schooling, *he is clever and smart*.
+            *Our dog will run away* if the gate is left open.`
+            }
+          ]
+        }
+      }
+    ]
+  };
+        
