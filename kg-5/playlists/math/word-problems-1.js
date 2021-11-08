241f7b6b52e904a0bab1c1c9c268deb0694a6e96
Atish Sihi <85349281+atishsihi@users.noreply.github.com>
2021-11-08 10:27:43 +0530

    Create word-problems-1.js

---
+++ kg-5/playlists/math/word-problems-1.js
@@ -0,0 +1,75 @@
+export default {
+  label: 'Word Problems',
+  id: 'word-problems-1',
+  //word problems for class 1 students
+  list: [
+
+    {
+      
+      type: 'numberInput',
+      id: 'word-prob-1-1',
+      label: 'Answer the Word Problem -1',
+      lockAfter: 2,
+      commonData: {
+        title: 'Answer the word problem.',
+        type: 'word-problem'
+      },
+      data: [
+        // Word Problems for (1-9) addition - Class 1
+        `You have 2 chocolates and your sister has 3 chocolates, together how many chocolates do you have? | 5
+        Ramu likes to eat Idly. He eats 2 Idlis for breakfast, 3 for Lunch and 2 for Dinner. In a day what is the total number of Idlis does Ramu eat? | 6
+        David cycles 4 KMs everyday. How many KMs in total will David cycle on Saturday and Sunday? | 8
+        You have 3 ladoos, your best friend gave another 3 to you. How many total ladoos do you have now? | 6
+        Riya likes to fill water in a water bottle. She has 3 water bottles of 1,2,3 litres . If she feels all the bottles then how much total water has she filled? | 6`,
+        
+        // Word Problems for (1-9) Subtraction - Class 1
+        `Susan had 8 apples in the basket. He ate 2 apples. How many apples are left? | 6
+        Saniya loves to collect coins. She collected 9 new coins but lost 4 of them. How many new coins are left with her? | 5
+        There are 8 Pencils in a packet. Teacher distributed 6 pencils to the students. How many are left now in the packet? | 2
+        Jane’s school is 5 KMs away from her home. She goes to school by cycle. One day after cycling 3 KMs towards school, her cycle got punctured. How many KMs does she need to walk to reach school? | 2
+        Riya has 7 toys with her. She has 3 friends. She has gifted 1 toy each to her 3 friends. How many toys are left with Riya now? | 4 `,
+
+        //Word Problems for (1-9) miscellaneous - Class 1
+        `There are 3 wickets on each side of the cricket pitch. If 1 wicket is broken while playing, how many are left in good condition? | 5
+        You have 10 bananas with you. You gave 2 to your brother and 2 to your friend. How many are left with you now? | 6
+        You have filled 6 bottles of 1 litres with water. By mistake one bottle of water has fallen down. How much total water (in litre) is left? | 5
+        Priya has ₹1 , ₹2 and ₹5 each. How much total money in ₹ Priya has with her? | 8
+        Gopal took his 4 cows for grazing. Each cow eats 2 baskets of grass. How many baskets of grass are required to feed all the cows? | 8`,
+
+      ]
+    },
+    {
+      
+      type: 'numberInput',
+      id: 'word-prob-1-2',
+      label: 'Answer the Word Problem -2',
+      lockAfter: 2,
+      commonData: {
+        title: 'Answer the word problem.',
+        type: 'word-problem'
+      },
+      data: [
+        // Word Problems for (10-20) addition - Class 1
+        `Ram has 4 mangoes and Sita has 7 mangoes. How many mangoes do they have together? | 11
+        John’s home to office distance is 10 KMs away. How many KMs John travels (in KM) each day to go to the office and come back? | 20
+        You have 10 marbles and your brother has 6 marbles. How many total marbles do you both have together? | 16
+        Rina has 3 science books, 4 mathematics books and 6 science books. Total number of books Rina has? | 13
+        A cricket team has 5 bowlers, 5 batsmen and 1 wicket keeper. What is the total number of players the team has? | 11`,
+
+        // Word Problems for (10-20) Subtraction - Class 1
+        `You have ₹15 with you. You bought one chocolate for ₹5. How much money is left with you now? | ₹10
+        A water bottle which can contain 20 litres of water. If you fill 2 bottles of 1 litres each for the water bottle, how much water is left in the water bottle? | 18
+        Jack has a stick 13 feet long. He broke it to get a 2 feet long stick. What is the length of the remaining stick? | 11
+        Aina collected 20 marbles. She has given 4 marbles to her friends. How many are left with her? | 16
+        A squirrel had 15 nuts. The squirrel  ate 12 of them already. How many are left? | 3`,
+
+        // Word Problems for (10-20) miscellaneous - Class 1
+        `You had ₹10 with you. Your dad gave you another ₹10. You bought one pen of ₹7 from the money. How much money is left with you now? | 13
+        Janny bought 19 kg potatoes. She used 4 kg potatoes to cook fries for her friends. How many kg of potatoes are left with Jenny now? | 15
+        Sita needs to travel 15 kms to reach her school. One day she cycled 2 kms towards school then she remembered that she had left the tiffin box at home. She went back home again, took the tiffin box and reached school. How many kms she cycle that day? | 19
+        Raghu loves to eat sweets. One day he ate 4 ladoos, 5 rasgullas and 10 gulab jamuns. What is the total number of sweets Raghu ate that day? | 19
+        `,
+      ]
+    },
+  ]
+};
