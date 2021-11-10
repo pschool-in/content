986563a7d45c24509978f894b06808480320a749
Reagrover <89007361+Reagrover@users.noreply.github.com>
2021-11-10 17:40:46 +0530

    Update pronouns-1.js

--- kg-5/playlists/grammar/pronouns-1.js
+++ kg-5/playlists/grammar/pronouns-1.js
@@ -41,7 +41,7 @@ Throw *those* rolls away.`,
 `*This* is my mother’s saree.
 *That* looks like a rat.
 *These* are nice flowers.
-*Those* are mine clothes.`,
+*Those* are my clothes.`,
 
 `I would like *those*, please.
 I am not sure *that* is how you do it.
@@ -176,12 +176,17 @@ Could you help me move *these*?
         id: 'personal-pronouns-reading',
         data: {
           title: 'Personal Pronouns',
-          text: `A personal pronoun is a word that takes place of naming words. Personal pronouns are he, she, it and they. For example: Rahul is a good boy. 'Rahul' is a noun in this sentence. So, it can be replaced with the personal pronoun 'he'. So this sentence can also be written as, He is a good boy.
+          text: `A personal pronoun is a word that takes place of naming words. Personal pronouns are he, she, it and they. 
+          
+          For example: 
+          Rahul is a good boy. 
+          'Rahul' is a noun in this sentence. So, it can be replaced with the personal pronoun 'he'. So this sentence can also be written as, 
+          He is a good boy.
 
 Some more examples of personal pronouns are:
 1. He eats grapes.
 2. She likes pear.
-3. You can have it
+3. You can have it.
 4. They like fruits.
 5. He runs fast.
 6. She jumps high.
