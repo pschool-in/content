6f9520ae4867eb95c1ad394e9c6cb0e478ebbe15
Atish Sihi <85349281+atishsihi@users.noreply.github.com>
2021-11-12 12:12:35 +0530

    Update word-problems.js

--- kg-5/playlists/math/word-problems.js
+++ kg-5/playlists/math/word-problems.js
@@ -456,7 +456,18 @@ With a 200 gm paneer packet we can get 20 equal sizes of small paneer cubes. If 
         A merchant has 57 kg 250 g of wheat with him. He sells 15 kg 250 g of wheat to one customer and 13 kg 500 g to another customer. What is the weight of wheat remaining with him? | ?0 kg ?1 g | 38,500
         The yield of wheat per acre is 1 quintal 50 kg. Raju has 30 acres of field. What is the total yield of paddy? | ?0 quintals | 45
         18 kg 400 g of sugar is to be packed in 4 bags equally. What is the weight of each bag ? | ?0 kg ?1 g | 4,600
-        A farmer gets 60 kg 400 g of onion from one field and 66 kg 800 g from another field. If he sells 97 kg 200 g of onion, find the weight of onions left with him. | ?0 kg | 30`
+        A farmer gets 60 kg 400 g of onion from one field and 66 kg 800 g from another field. If he sells 97 kg 200 g of onion, find the weight of onions left with him. | ?0 kg | 30`,
+
+        `A glass contains 500 ml of water and another glass contains 250 ml of water. If water from both the glasses are poured into a jug, what is the quantity of water in the jug? | ?0 ml | 750
+        A milkman has 6 l 700 ml of milk. Out of this he sells 1 l 150 ml of milk to one person and 2 l 550 ml to another person. Find the quantity of milk left with the milkman. | ?0 l | 3
+        A bottle contains 250 ml of milk. What is the total quantity of milk (in litre) contained in 12 such bottles? | ?0 l | 3
+        1 l of sugarcane juice is distributed among 4 friends equally. What quantity of sugarcane juice does each one get? | ?0 ml | 250`,
+
+        `Shabina buys 4 l 250 ml of groundnut oil while returning from her office. Her mother also bought 2 l 500 ml of groundnut oil on the same day. What is the total quantity of groundnut oil bought by them? | ?0 l ?1 ml | 6,750
+        A milkman has 27 l of milk. He sells 16 l 650 ml of it. Find the quantity of milk left with him. | ?0 l ?1 ml | 10,350
+        A bottle contains 1 l 750 ml of mustard oil. It has to be filled into bottles of 250 ml each. How many bottles are required? | 7
+        An oil can contains 15 l of Saffola oil. Out of this Rani uses 3 l 400 ml in the first month and 5 l 350 ml in the second month. Find the quantity of oil left in the oil can. ?0 l ?1 ml | 6,250
+        A vessel contains 3 l 300 ml of tea. It is poured into 12 glasses of the same size equally. What is the quantity of tea contained in each glass? ?0 ml | 275`,
 
       ]
     },
