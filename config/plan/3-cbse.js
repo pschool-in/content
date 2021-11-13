a28f3e7ba5aed206ab0faaa645071cd3388518d0
Rethna <rethna2@gmail.com>
2021-11-13 11:19:41 +0530

    added config files

---
+++ config/plan/3-cbse.js
@@ -0,0 +1,61 @@
+export default {
+  vocabulary: [
+    'animal-words',
+    'rhyming-words',
+    'word-from-picture',
+    'word-from-picture-2',
+    'connect-letters',
+    '4-letter-words',
+    'antonyms-1',
+    'antonyms-2',
+    'antonyms-3',
+    'synonyms-1',
+    'synonyms-2',
+    'synonyms-3',
+    'spelling-2',
+    'compound-word',
+    'prefix-suffix',
+    'confusing-words-2'
+  ],
+  grammar: [
+    'article',
+    'sentence',
+    'negative-sentence',
+    'noun',
+    'countable-noun',
+    'verb',
+    'helping-verb',
+    'pronoun',
+    'preposition',
+    'right-word',
+    'adverb-3',
+    'modal',
+    'clause'
+  ],
+  reading: ['simple-reading', 'stories', 'poems-collection'],
+  math: [
+    'tables-3',
+    'arithmetic-3',
+    'balance-numbers',
+    'word-problems',
+    'fraction',
+    'measurement',
+    'place-value',
+    'geometry'
+  ],
+  science: [
+    'matter-3',
+    'force-3',
+    'plants-3',
+    'food-3',
+    'water-3',
+    'everyday-science-3',
+    'air-3',
+    'humanbody-3',
+    'animals-3',
+    'environment-3',
+    'computer-3'
+  ],
+  social: ['india', 'health-3', 'family-3', 'job-3', 'time-3', 'safety-3'],
+  gk: ['largest', 'inventions', 'countries', 'leaders']
+};
