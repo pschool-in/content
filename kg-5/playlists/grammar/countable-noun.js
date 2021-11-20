956a58b3e04e5b60fe474b26ace02c3547c33767
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-11-21 01:17:59 +0530

    Update countable-noun.js

--- kg-5/playlists/grammar/countable-noun.js
+++ kg-5/playlists/grammar/countable-noun.js
@@ -235,17 +235,16 @@ The *streets* are very quiet these days!`
       ]
     },
     //Newly added
-    {
+     {
       label: 'Singular-Plural - Reading',
       type: 'passage',
       id: 'singular-plural-reading',
       data: {
         title: 'Singular-Plural',
-        text: `In simple words, singular means only one. Plural means more than one. We generally add 's' at the end of the nouns to make them plural. Some examples are:
+    text: `In simple words, singular means only one. Plural means more than one. We generally add 's' at the end of the nouns to make them plural. Some examples are:
 1. The plural for an ant will be ants.
-2. The plural for a cat will be cats.
+2. The plural for a cat will be cats
 3. The plural for a bird will be birds.
-
 Some nouns use different words as plural. Some examples are:
 1. The plural for a child is children.
 2. The plural for a tooth is teeth.
@@ -253,43 +252,77 @@ Some nouns use different words as plural. Some examples are:
       }
     },
     {
-      type: 'group',
-      label: 'Drag and Drop',
-      id: 'classify-singular-plural',
-      commonData: {
-        title: 'Identify the singular and plural words.',
-        types: ['Singular', 'Plural']
-      },
-      data: [
-        [`Snake, Bat, Bear, Duck`, `Feet, Children, Teeth, Mice`],
-        ['School, Pencil, Doctor, Market', 'Teachers, Windows, Lamps, Bosses'],
-        ['Box, Wish, Plate, Berry', 'Skies, Tomatoes, Heroes']
-      ]
+    type: 'group',
+    label: 'Drag and Drop',
+    id: '100',
+    commonData: {
+      title: 'Identify the singular and plural words.',
+      types: ['Singular', 'Plural']
     },
-    {
-      id: 'match',
-      label: 'Match the following',
-      type: 'match',
-      commonData: {
-        title: 'Match the singular words with their plurals'
-      },
-
-      data: [
-        `Child, Children
+    data: [
+      [
+        `Snake, Bat, Bear, Duck`,
+        `Feet, Children, Teeth, Mice`
+      ],
+      [
+        'School, Pencil, Doctor, Market',
+        'Teachers, Windows, Lamps, Bosses'
+      ],
+      [
+        'Box, Wish, Plate, Berry',
+        'Skies, Tomatoes, Heroes'
+      ],
+      [
+        'Girl, Boy, Apple, Mango',
+        'Boxes, Classes, Dogs, Shops'
+      ],
+      [
+        'Spoon, Sister, Key, Orange',
+        'Flowers, Phones, Knees, Leaves'
+      ]
+    ]
+  },
+  {
+  id: '200',
+  label: 'Match the following',
+  type: 'match',
+  commonData: {
+      title: 'Match the singular words with their plurals.'
+  },
+ data: [
+   `Child, Children
    Foot, Feet
    Tooth, Teeth
    Shelf, Shelves
    Knife, Knives`,
 
-        `Mouse, Mice
+   `Mouse, Mice
    Man, Men
    Plate, Plates
    Chef, Chefs
-   Safe, Safes`
-      ]
-    },
-    {
-      id: 'fill-up',
+   Safe, Safes`,
+
+   `Dress, Dresses
+   Bush, Bushes
+   Lunch, Lunches
+   Punch, Punches
+   Box, Boxes`,
+
+   `Fox, Foxes
+   Bus, Buses
+   Dish, Dishes
+   Brush, Brushes
+   Wish, Wishes`,
+
+   `Woman, Women
+   Family, Families
+   Ox, Oxen
+   Baby, Babies
+   Country, Countries`,
+    ]
+  },
+  {
+      id: '300',
       label: 'Fill up with options',
       type: 'fillup',
       commonData: {
@@ -300,13 +333,23 @@ Some nouns use different words as plural. Some examples are:
         My mom bought 1 packet of *cherries (cheerys)*.
         I love eating french *fries (frys)*.
         I like *cats (caties)*.
-        Dogs have 4 *legs (leges)*.`,
+        Dogs have 4 *legs (leges)*.
+        I like eating *berries (berrys)*.
+        He divided the chocolate in two *halves (halfs)*.
+        My *teeth (tooths)* are white.
+        The police caught three *thieves (thiefs)*.
+        The *children (childs)* are playing in the park.`,
 
         `My aunt doesn't like *babies (babys)*.
         *Monkeys (Monkeies)* climb trees very fast.
         *Flies (Flys)* are very common insects.
         There are a lot of *apples (appless)* in the tree.
-        Bees live in *hives (hivies)*.`
+        Bees live in *hives (hivies)*.
+        There were many *people (persons)* in the room.
+        Human beings have two *feet (foots)*.
+        There are so many green *leaves (leafs)* in the tree.
+        There are twenty *sheep (sheeps)* in the farm.
+        I saw two black *fish (fishes)* in the pond.`
       ]
     }
   ]
