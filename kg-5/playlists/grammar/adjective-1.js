b091c50feb63ab8acc1a10409daa70f77666c513
Rethna <rethna2@gmail.com>
2021-11-21 11:35:49 +0530

    merge commit 2

--- kg-5/playlists/grammar/adjective-1.js
+++ kg-5/playlists/grammar/adjective-1.js
@@ -20,45 +20,36 @@ export default {
 8. Sweet food
 9. Soft toy
 10. Loving parents`
-}
-},
-{
-  type: 'group',
-  label: 'Drag and Drop',
-  id: 'classify-positive-negative',
-  commonData: {
-    title: 'Identify these positive and negative describing words.',
-    types: ['Positive', 'Negative']
-  },
-  data: [
-    [
-      `Smart, Pretty, Beautiful, Soft`,
-      `Ugly, Bad, Hard, Stupid`
-    ],
-    [
-      `Happy, Perfect, Wise, Wonderful`,
-      `Cruel, Sad, Boring, Dull`
-    ],
-    [
-      `Talented, Natural, Caring, Amazing`,
-      `Jealous, Moody, Rude, Selfish`
-    ]
-  ]
-},
-{
-  id: 'jumbled-words',
-  type: 'sequence',
-  label: 'Jumbled Words',
-  commonData: {
-      title: 'Identify these describing words.'
+      }
     },
-  data: [
-      `Big, Pretty, Small, Round, Sweet, Soft`,
-`Hard, Brave, Great, Good, Brown, Red`,
-`Smart, Two, Loving, Bad, Strong, Weak`,
-     ]
-  },
-  {
+    {
+      type: 'group',
+      label: 'Drag and Drop',
+      id: 'classify-positive-negative',
+      commonData: {
+        title: 'Identify these positive and negative describing words.',
+        types: ['Positive', 'Negative']
+      },
+      data: [
+        [`Smart, Pretty, Beautiful, Soft`, `Ugly, Bad, Hard, Stupid`],
+        [`Happy, Perfect, Wise, Wonderful`, `Cruel, Sad, Boring, Dull`],
+        [`Talented, Natural, Caring, Amazing`, `Jealous, Moody, Rude, Selfish`]
+      ]
+    },
+    {
+      id: 'jumbled-words',
+      type: 'sequence',
+      label: 'Jumbled Words',
+      commonData: {
+        title: 'Identify these describing words.'
+      },
+      data: [
+        `Big, Pretty, Small, Round, Sweet, Soft`,
+        `Hard, Brave, Great, Good, Brown, Red`,
+        `Smart, Two, Loving, Bad, Strong, Weak`
+      ]
+    },
+    {
       type: 'selectWord',
       label: 'Select the describing words',
       id: 'select-word',
@@ -78,7 +69,7 @@ export default {
         A lion roars very *loudly*.
         Goods train is very *long*.`,
 
-       `A *poor* man was sitting on roadside.
+        `A *poor* man was sitting on roadside.
        This is a *pleasant* weather.
        A *small* rat lives in our house.
        I have a *purple* dress.
@@ -89,51 +80,50 @@ export default {
       type: 'rightOne',
       label: 'Correct Spelling',
       id: 'correct-spelling',
-  
+
       commonData: {
-          title: 'Identify the correct spelling of descibing words.'
-        },
-        data: [ 
-       `Ugly, Ugley
+        title: 'Identify the correct spelling of descibing words.'
+      },
+      data: [
+        `Ugly, Ugley
        Brown, Browne
        Loud, Loude
        Clean, Chleane
        Tall, Tale`,
 
-       `Long, Longe
+        `Long, Longe
        Fast, Fest
        Small, Smell
        Big, Beg
        Smart, Smert`
-        ]
-      },
+      ]
+    },
     {
       id: 'match',
       label: 'Match the following',
       type: 'match',
       commonData: {
-          title: 'Match the pairs.'
+        title: 'Match the pairs.'
       },
-     
+
       data: [
-       `Big, Elephant
+        `Big, Elephant
        Dark, Night
        Small, Ant
        Pleasant, Weather
        Loud, Lion`,
-       
-       `Clean, Room
+
+        `Clean, Room
        Smart, Student
        Long, Train
        Ugly, Pig
        Round, Ball`,
 
-<<<<<<< HEAD
         `Red, Colour
-         Two, Hands
-         Soft, Toy
-         Fast, Horse
-         Sweet, Dish`
+       Two, Hands
+       Soft, Toy
+       Fast, Horse
+       Sweet, Dish`
       ]
     },
     {
@@ -147,11 +137,11 @@ export default {
         {
           questions: [
             {
-              qText: 'Neha likes to ride in______cars.',
+              qText: 'Neha likes______cars.',
               options: 'fast, the, all'
             },
             {
-              qText: '_______trees are easy to climb.',
+              qText: 'I can climb_______trees.',
               options: 'Small, Big, Large'
             },
             {
@@ -176,7 +166,7 @@ export default {
             },
             {
               qText: 'We went down the_______slide.',
-              options: 'giant, round, steep'
+              options: 'long, round, steep'
             },
             {
               qText: 'My little sister loves_______toys.',
@@ -220,96 +210,3 @@ export default {
     }
   ]
 };
-=======
-       `Red, Colour
-       Two, Hands
-       Soft, Toy
-       Fast, Horse
-       Sweet, Dish`,
-           ]
-         },
-         {
-          label: 'Multiple Choice Questions',
-           id: 'mcq-adjectives-1',
-          type: 'mcq',
-          commonData: {
-            title: 'Choose the correct describing words.'
-          },
-          data: [
-            {
-      questions: [
-            {
-              qText: 'Neha likes______cars.',
-              options: 'fast, the, all'
-            },
-            {
-              qText: 'I can climb_______trees.',
-              options: 'Small, Big, Large'
-            },
-            {
-              qText: 'My mom is a______lady.',
-              options: 'pretty, bad, old'
-            },
-            {
-              qText: 'The dog found a______stick.',
-              options: 'long, smart, round'
-            },
-            {
-              qText: 'Disha caught the______ball.',
-              options: 'red, far, near'
-            }
-            ]
-          },
-        {
-      questions: [
-          {
-              qText: 'I love my______puppy.',
-              options: 'little, bad, slow'
-            },
-            {
-              qText: 'We went down the_______slide.',
-              options: 'long, round, steep'
-            },
-            {
-              qText: 'My little sister loves_______toys.',
-              options: 'soft, sweet, fast'
-            },
-            {
-              qText: 'The_______sun warms our house.',
-              options: 'bright, dull, small'
-            },
-            {
-              qText: 'That is an_______bug.',
-              options: 'ugly, sweet, long'
-            }
-            ]
-          },
-        {
-      questions: [
-          {
-              qText: 'Snowbell is a______cat.',
-              options: 'nice, dull, long'
-            },
-            {
-              qText: 'There are_________trees in the garden.',
-              options: 'beautiful, ugly, bad'
-            },
-            {
-              qText: 'My_______brother is in his room.',
-              options: 'little, bad, smart'
-            },
-            {
-              qText: 'The elephant has______ears.',
-              options: 'big, small, little'
-            },
-            {
-              qText: 'My______bicycle is purple.',
-              options: 'new, big, small'
-            }
-            ]
-          }
-        ]
-      }
-    ]
-  }; 
->>>>>>> eeeebfd3b78c6fc1237cd499a0b5eb72a93f46f2
