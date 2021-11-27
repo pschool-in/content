a8c50e6241b83d120bf529d1a1ea54b51e67055b
priya2786 <priyahh123@gmail.com>
2021-11-27 17:54:57 +0530

    tamil update

--- tamil/playlists/more/ta-picture-3.js
+++ tamil/playlists/more/ta-picture-3.js
@@ -8,20 +8,589 @@ export default {
       label: 'விலங்குகள்',
       id: 'dragDropImgLabel',
       commonData: {
-        title: 'Drag and Drop the name on the images.'
+        title: 'விலங்குகளின் படங்களுடன் பெயரைப் பொருத்துக'
       },
       data: [
+
         `cat| பூனை 
         dog| நாய் 
         goat| ஆடு
         sheep| செம்மறி ஆடு  
-        horse| குதிரை 
-        pig| பன்றி
-        elephant| யானை 
+        horse| குதிரை
+        pig| பன்றி`,
+
+        `elephant| யானை 
         lion| சிங்கம் 
         monkey| குரங்கு
-        tiger| புலி `
+        tiger| புலி
+        hippo| நீர்யானை
+        rhino| காண்டாமிருகம்`,
+
+        `fox| நரி 
+        crocodile| முதலை
+        zebra| வரிக்குதிரை
+        bear| கரடி
+        camel| ஒட்டகம்
+        shark| சுறாமீன்`,
+
+        `whale| திமிங்கிலம்
+          frog| தவளை 
+          crab| நண்டு
+          snake| பாம்பு
+          squirrel| அணில்
+          tortoise|  ஆமை`,
+
+        `rat| எலி 
+        winged-termites| ஈசல் 
+        ant| எறும்பு
+        fly| ஈ
+        bat| வௌவால்
+        buffalo|  எருமை`,
+
+          `butterfly|  வண்ணத்துப்பூச்சி
+          cow|  மாடு
+          donkey| கழுதை
+          giraffe| ஒட்டகச்சிவிங்கி
+          kangaroo| கங்காரு
+          leopard| சிறுத்தை`,
+
+          `mongoose| கீரிப்பிள்ளை
+    mosquito| கொசு 
+    wolf| ஓநாய்
+    cockroach| கரப்பான்பூச்சி
+    garden-lizard| ஓணான்
+    lizard| பல்லி`,
+
+    `rabbit| முயல்
+    boar| காட்டுப்பன்றி
+    hyena| கழுதைப் புலி
+    polarbear| பனிக்கரடி
+    antelope| மான்
+    bug| வண்டு`,
+
+         `jackal| நரி
+    prawn| இறால்
+    baboon| வாலில்லாக் குரங்கு
+    bull| காளை
+    dinosaur| டைனோசர்
+    python| மலைப்பாம்பு`,
+
+    `bison| காட்டெருமை
+    chameleon|பச்சோந்தி
+    porcupine| முள்ளம்பன்றி
+    bee| தேனீ
+    goldfish| தங்கமீன்
+    seahorse| கடற்குதிரை`,
+    
+    `hamster| வெள்ளெலி
+    spider| சிலந்தி
+    caterpillar| கம்பளிப்பூச்சி
+    panda| பாண்டா
+    starfish| நட்சத்திர மீன்`,
+
+    `dragonfly| தட்டான்
+    fish| மீன் 
+    earthworm| மண்புழு
+    hedgehog| முள்ளெலி
+    stingray| திருக்கை மீன்`
       ]
-    }
+    },
+    {
+      type: 'dragDropImgLabel',
+      label: 'பறவைகள்',
+      id: 'dragDropImgLabel1',
+      commonData: {
+        title: 'பறவைகளின் படங்களுடன் பெயரைப் பொருத்துக'
+      },
+      data: [
+        
+          `crow| காகம் 
+      dove| புறா   
+      duck| வாத்து
+      hen| கோழி 
+      parrot| கிளி`,
+
+      `sparrow| சிட்டுக்குருவி
+        chick| கோழிக்குஞ்சு
+        crane| கொக்கு
+        eagle| கழுகு
+        kingfisher| மீன்கொத்தி`,
+
+      `ostrich| நெருப்புக்கோழி
+      owl| ஆந்தை
+      rooster| சேவல்
+      woodpecker| மரங்கொத்தி
+      cuckoo| குயில்`,
+
+      `swan| அன்னப்பறவை
+      spotted-dove| மணிப்புறா
+      robin| கருஞ்சிட்டு
+      swallow|  தகைவிலாங் குருவி
+      seagull| கடற்பறவை
+      goose| வாத்து`,
+
+      `falcon| பருந்து
+      myna| மைனா
+      peacock| மயில்
+      turkey| வான்கோழி
+      parakeet| பெரிய பச்சைக்கிளி`
+              ]
+    },
+        {
+      type: 'dragDropImgLabel',
+      label: 'பழங்கள்',
+      id: 'dragDropImgLabel2',
+      commonData: {
+        title: 'பழங்களின் படங்களுடன் பெயரைப் பொருத்துக'
+      },
+      data: [
+        `apple| ஆப்பிள்
+        grapes| திராட்சை
+        banana| வாழைப்பழம்
+        avocado| வெண்ணைப்பழம்
+        guava| கொய்யாப்பழம்`,
+
+        `jackfruit| பலாப்பழம் 
+        mango| மாம்பழம்
+        orange| ஆரஞ்சு
+        papaya| பப்பாளி
+        pineapple| அன்னாசி`,
+
+        `pomegranate| மாதுளை 
+        watermelon| தர்பூசணி 
+        fig| அத்திப்பழம்
+        custard-apple| சீதாப்பழம்
+        plum| ஊட்டி ஆப்பிள்`,
+
+        `dates| பேரிச்சை 
+        dragon-fruit| விருத்திரப்பழம்
+        sweet-potato| சர்க்கரை வள்ளி கிழங்கு 
+        jamun| நாவல் பழம்
+        pear| பேரிக்காய்`,
+
+        `jujube| இலந்தைப்பழம்
+        sapotta| சப்போட்டா
+        sugarcane| கரும்பு 
+          tapioca| மரவள்ளிக்கிழங்கு
+          blueberry| அவுரிநெல்லி`
+            ]
+    },
+            {
+      type: 'dragDropImgLabel',
+      label: 'காய்கறிகள்',
+      id: 'dragDropImgLabel3',
+      commonData: {
+        title: 'காய்கறிகளின் படங்களுடன் பெயரைப் பொருத்துக'
+      },
+      data: [
+        `onion| வெங்காயம்
+        tomato| தக்காளி
+        potato| உருளைக்கிழங்கு
+        cabbage| முட்டைகோசு
+        corn| மக்காச்சோளம்
+        cucumber| வெள்ளரிக்காய்`,
+
+        `garlic| பூண்டு
+        ginger| இஞ்சி
+        pumpkin| பரங்கிக்காய்
+        almond| பாதாம்
+        bitter-gourd| பாகற்காய்
+        brinjal| கத்திரி`,
+
+        `elephant-yam| சேனைக்கிழங்கு
+        capsicum| குடைமிளகாய்
+        cashewnut| முந்திரி 
+        chilli| பச்சைமிளகாய்
+        coconut| தேங்காய்
+        corianderleaf|கொத்தமல்லி`,
+ 
+          `drumstick | முருங்கைக்காய்
+        greenpeas| பச்சைபட்டாணி
+        ladies-finger| வெண்டைக்காய்
+        mintleaf | புதினா
+        peanut| வேர்க்கடலை
+        raddish| முள்ளங்கி`,
+
+        `spinach| கீரை
+        chickpea| கொண்டைக்கடலை
+        mustard-greens| கடுகு கீரை 
+        turmeric| மஞ்சள்
+        fenugreek| வெந்தயக்கீரை
+        tamarind| புளி`,
+
+        `curryleaf| கருவேப்பிலை
+        broad-beans| அவரைக்காய் 
+        ashgourd| பூசணிக்காய் 
+        ridgegourd| பீர்க்கங்காய்
+        raw-banana| வாழைக்காய்
+        drumstick-leaves| முருங்கை கீரை`,
+
+        `tulsi| துளசி
+        banana-flower| வாழைப்பூ 
+        bottlegourd| சுரைக்காய்
+        cluster-beans| கொத்தவரங்காய்
+        ivygourd| கோவைக்காய்
+        kohlrabi| நூல்கோல்`,
+
+        `mushroom| காளான்
+        snake-gourd| புடலங்காய்
+        spring-onion| வெங்காயத்தாள்
+        banana-stem| வாழைத்தண்டு`
+              ]
+    },
+      {
+      type: 'dragDropImgLabel',
+      label: 'பூக்கள்',
+      id: 'dragDropImgLabel4',
+      commonData: {
+        title: 'பூக்களின் படங்களுடன் பெயரைப் பொருத்துக'
+      },
+      data: [
+        `lotus| தாமரை
+        rose| ரோஜா
+        sunflower| சூரியகாந்தி 
+        marigold| சாமந்தி 
+        hibiscus| செம்பருத்தி`,
+        
+        `jasmine| முல்லை 
+        magnolia| செண்பகப்பூ
+        periwinkle| நித்தியகல்யாணிப்பூ
+        water-lily| அல்லி`
+              ]
+    },
+    {
+      type: 'dragDropImgLabel',
+      label: 'வாகனங்கள்',
+      id: 'dragDropImgLabel5',
+      commonData: {
+        title: 'வாகனங்களின் படங்களுடன் பெயரைப் பொருத்துக'
+      },
+      data: [
+        `aeroplane| விமானம்
+        boat| படகு 
+        bus| பேருந்து 
+        car| மகிழுந்து 
+        cycle| மிதிவண்டி
+        bullock-cart| மாட்டு வண்டி`,
+
+        `ship| கப்பல்
+        train| தொடர்வண்டி
+        tractor| இழுவை இயந்திரம்
+        ambulance| அவசரஊர்தி
+        fire-engine| தீயணைப்பு வாகனம்
+        horse-carriage| குதிரை வண்டி`,
+
+        `cart| கட்டை வண்டி
+        lorry| சரக்குந்து
+        bulldozer| நிலச்சமன் பொறி
+        rocket| ஏவுகனை
+        submarine| நீர்மூழ்கிக் கப்பல்`
+              ]
+    },
+    {
+      type: 'dragDropImgLabel',
+      label: 'தொழில்',
+      id: 'dragDropImgLabel6',
+      commonData: {
+        title: 'படங்களுடன் பெயரைப் பொருத்துக'
+      },
+      data: [
+        `astronaut|விண்வெளி வீரர்
+    clown| கோமாளி
+    fisherman| மீனவர்
+    postman| தபால்காரர்
+    teacher| ஆசிரியர்`,
+
+    `blacksmith| கொல்லன்
+    cobbler| செருப்புத் தொழிலாளி
+    goldsmith| பொற்கொல்லர்
+    potter| குயவன்
+    weaver| நெசவாளர்`,
+
+    `carpenter| தச்சர்
+    diver| நீரில் மூழ்குவோர்
+    warrior| போர்வீரன்
+    nurse| செவிலியர்
+    sailor| மாலுமி`,
+
+    `chef| சமையல்காரர்
+    doctor| மருத்துவர்
+    police| காவல்
+    soldier| சிப்பாய்
+    driver| ஓட்டுநர்`
+      ]
+    },
+    {
+      type: 'dragDropImgLabel',
+      label: 'கூடுதல் பயிற்சி',
+      id: 'dragDropImgLabel7',
+      commonData: {
+        title: 'படங்களுடன் பெயரைப் பொருத்துக',
+              },
+      data: [
+
+        `sun| சூரியன்
+    moon | நிலா
+    rain| மழை
+    star| நட்சத்திரம்
+    bird| பறவை
+    palanquin| பல்லக்கு`,
+
+        `vegetables| காய்கறிகள்
+    fruits| பழங்கள்
+    leaf|  இலை
+    world| உலகம்
+    nest| கூடு
+    twig| கிளை`,
+
+    `water-drop| நீர்த்துளி
+      waterfall| அருவி
+      ring| மோதிரம்
+      brush| தூரிகை
+      bone| எலும்புத்துண்டு
+      auvai| ஔவை`,
+
+    `bell| மணி
+    five| ஐந்து
+    food| உணவு
+    king| அரசன்
+    kite| பட்டம்
+    ladder| ஏணி`,
+
+    `rishi| முனிவர்
+    wheel| சக்கரம்
+    whistle| ஊதல்
+    knight| மாவீரர்
+    chariot| தேர்
+    park| பூங்கா`,
+
+    `garden| தோட்டம்
+    touch-me-not| தொட்டாற் சிணுங்கி
+    yagna| யாகம்
+    yarn-ball| நூல் கண்டு
+    bow| வில்
+    arrow| அம்பு`,
+
+`fountain| நீரூற்று
+pouch| சுருக்குப்பை
+top| பம்பரம்
+hexagon| அறுகோணம்
+trident| திரிசூலம்
+stick| குச்சி`,
+      ]
+    },
+{
+  type: 'dragDropImgLabel',
+  label: 'உடை',
+  id: 'dragDropImgLabel8',
+  commonData: {
+    title: 'உடைகளின் படங்களுடன் பெயரைப் பொருத்துக'
+  },
+  data: [
+
+`dress| உடை
+shoe| காலணி
+sock| காலுறை
+pyjama| இரவு உடுப்பு
+gloves| கையுறைகள்
+scarf| கழுத்துத்துண்டு`,
+
+`
+sweater| கம்பளிச்சட்டை
+shirt| சட்டை
+jacket| மேலங்கி
+hat| தொப்பி
+trousers| கால்சட்டை`
   ]
+ },
+ {
+  type: 'dragDropImgLabel',
+  label: 'உடல் உறுப்புகள்',
+  id: 'dragDropImgLabel9',
+  commonData: {
+    title: 'படங்களுடன் பெயரைப் பொருத்துக',
+          },
+  data: [
+
+    `ear| காது 
+    eye| கண் 
+    foot| பாதம் 
+    hair| முடி`,
+
+     `hand| கை
+    mouth| வாய் 
+    nose| மூக்கு 
+    tooth| பல்` 
+  ]
+},
+{
+  type: 'dragDropImgLabel',
+  label: 'வீடு',
+  id: 'dragDropImgLabel10',
+  commonData: {
+    title: 'படங்களுடன் பெயரைப் பொருத்துக',
+          },
+  data: [
+
+    `house| வீடு
+      table| மேசை
+      radio| வானொலி
+      chair| நாற்காலி
+      sofa| நீள் சாய்வு இருக்கை
+      shelf| அலமாரி`,
+
+      `grandfather| தாத்தா
+      man| ஆண்
+      woman| பெண்
+      boy| சிறுவன்
+      girl| சிறுமி
+      baby | குழந்தை`,
+
+      `door| கதவு
+        window| ஜன்னல்
+        air-conditioner| குளிரூட்டி
+        washing-machine| துணி துவைக்கும் இயந்திரம்
+        fridge| குளிர்சாதன பெட்டி`,
+
+
+    `toothbrush| பற்குச்சி
+      toothpaste| பற்பசை 
+      bucket| வாளி
+      toilet| கழிப்பறை
+      soap| சவர்க்காரம்`,
+
+    `candle| மெழுகுவர்த்தி
+    ball| பந்து
+    box| பெட்டி
+    coin| நாணயம்
+    medicine| மருந்து
+    balloon| பலூன்`,
+   
+    `glasses| மூக்குகண்ணாடி
+    clock| கடிகாரம் 
+    watch| கைக்கடிகாரம்
+    flower-pot| பூத்தொட்டி
+    umbrella| குடை
+    broom| துடைப்பம்`,
+    
+    `pillow| தலையணை
+    bed-lamp| படுக்கை விளக்கு
+    bed| மெத்தை
+    basket| கூடை
+    dustbin| குப்பை தொட்டி
+    handbag| கைப்பை`,
+
+
+      `stove| அடுப்பு
+      fork| முள் கரண்டி
+      knife|கத்தி
+      pan| உலோகத்தட்டு
+      spoon| கரண்டி
+      pot| பானை`,
+
+      `plate| தட்டு
+      tap| நீர்க்குழாய்
+      lid| மூடி
+      blender| கலப்பான்
+      salt| உப்பு
+      cup| கோப்பை`,
+
+      `bowl| கிண்ணம்
+      waterbottle| தண்ணீர் குடுவை
+      scissors| கத்தரிக்கோல்
+      calculator| கணிப்பான்
+      book| புத்தகம்
+      ink| மை`,
+
+      `eraser| அழிப்பான்
+      pen| பேனா
+      pencil| எழுதுகோல்
+      ruler| அளவுகோல்
+      schoolbag| புத்தகப்பை`
+        ]
+},
+{
+  type: 'dragDropImgLabel',
+  label: 'கருவிகள்',
+  id: 'dragDropImgLabel11',
+  commonData: {
+    title: 'படங்களுடன் பெயரைப் பொருத்துக',
+          },
+  data: [
+
+`axe| கோடரி
+balance| தராசு
+blackboard| கரும்பலகை
+hose| குழாய்
+microscope| நுண்ணோக்கி`,
+
+`needle| ஊசி
+plier| இடுக்கி
+Plough| கலப்பை
+saw| இரம்பம்
+spade| மண்வெட்டி`,
+
+`telescope| தொலைநோக்கி
+test-tube| சோதனை குழாய்
+thermometer| வெப்பமானி
+trowel| கரணை
+walkie-talkie| நடந்துகொண்டே பேசும் கருவி`,
+
+`awl| குத்தூசி
+compass| திசைகாட்டி
+screw| திருகு
+screwdriver| திருப்புளி`
+  ]
+},
+{
+  type: 'dragDropImgLabel',
+  label: 'உணவுப்பொருட்கள்',
+  id: 'dragDropImgLabel12',
+  commonData: {
+    title: 'படங்களுடன் பெயரைப் பொருத்துக',
+          },
+  data: [
+       `chocolate| மிட்டாய்
+    milk| பால்
+    ladoo| லட்டு
+    cheese| பாலாடைக்கட்டி
+    butter| வெண்ணெய்
+    sugar|சர்க்கரை`,
+
+    `meat| இறைச்சி
+    chicken| கோழி
+    ice-cream| பனிக்கூழ்
+    sweets|இனிப்பு
+    bread| ரொட்டி
+    lollipop| குச்சி மிட்டாய்`,
+
+    `juice| பழச்சாறு
+    flour| மாவு
+    lentils| பருப்பு
+    rice|அரிசி|
+    soup| சாறு`
+  ]
+},
+{
+  type: 'dragDropImgLabel',
+  label: 'இசைக்கருவிகள்',
+  id: 'dragDropImgLabel13',
+  commonData: {
+    title: 'படங்களுடன் பெயரைப் பொருத்துக',
+          },
+  data: [
+        `conch| சங்கு
+    drums|முரசு
+    dhol| மத்தளம்
+    flute| புல்லாங்குழல்
+tabla| தபேலா`,
+
+`violin| நரம்பிசைக்கருவி
+guitar| ஆறு நரம்புள்ள இசைக்கருவி
+yazl| யாழ்
+damaru| உடுக்கை`
+  ]
+}
+
+    ]
 };
