ecd757773472175620e9e9e5dbf54929db0aa815
sanha shahal <sanhashahal3@gmail.com>
2021-11-29 09:52:01 +0530

    modified

---
+++ hindi/playlists/more/hi-sound-3.js
@@ -0,0 +1,145 @@
+export default {
+    label: 'ध्वनि',
+    id: 'hi-sound-2',
+    img: 'sound',
+    list: [
+      {
+        type: 'rightOne',
+        label: 'चित्र चुनें',
+        lockAfter: 3,
+        id: 'pick-word',
+        commonData: {
+          title: 'ध्वनि सुनकर चित्र का चयन करें',
+          type: 'words'
+        },
+        data: [
+          {
+            audio: 'hi/hi-animals.mp3',
+            words: 'बिल्ली, कुत्ता, बकरी, भेड्, घोडा, सूअर, हाथी, शेर, बंदर, बाघ'
+          },
+          {
+            audio: 'hi/hi-animals.mp3',
+            audioOffset: 20,
+            words:
+              'जलहस्ती, गैंडा, लोमड़ी, मगरमच्छ, भालू, ऊंट, मछली, मेंडक, साँप, कछुआ'
+          },
+          {
+            audio: 'hi/hi-animals.mp3',
+            audioOffset: 40,
+            words: 'चूहा, गिलहरी, चींटी, मक्खी, चमगादड़, भेंस, तितली'
+          },
+          {
+            audio: 'hi/hi-animals.mp3',
+            audioOffset: 54,
+            words: 'तेंदुआ, गाय, गधा, नेवला, मच्छर, छिपकली, खरगोश'
+          },
+          {
+            audio: 'hi/hi-birds.mp3',
+            words: 'कौवा, बतख़, मुर्गी, तोता, मोर, गौरेया, चूजा, मैना, शुतरमुर्ग'
+          },
+          {
+            audio: 'hi/hi-birds.mp3',
+            audioOffset: 18,
+            words: 'उल्लू, मुर्गा, कठफोड़वा, गिद्ध, सारस, मराल, नीलकंठ'
+          },
+          {
+            audio: 'hi/hi-flowers-bodyParts-kitchen.mp3',
+            audioOffset: 0,
+            words: 'गुलाब, कमल, सूर्यमुखी, गेंदा, चमेली, गुडहल'
+          },
+          {
+            audio: 'hi/hi-flowers-bodyParts-kitchen.mp3',
+            audioOffset: 12,
+            words: 'कान, आँख, टांग, टांग, हाथ, मुँह, नाक, दांत'
+          },
+          {
+            audio: 'hi/hi-flowers-bodyParts-kitchen.mp3',
+            audioOffset: 28,
+            words: 'चूल्हा, कांटा, चम्मच, छुरी, कड़ाही, मटका, मक्खन'
+          },
+          {
+            audio: 'hi/hi-fruits.mp3',
+            audioOffset: 0,
+            words: 'सेब, अंगूर, केला, झरबेर, अमरूद, कटहल, आम, संतरा, पपीता'
+          },
+          {
+            audio: 'hi/hi-fruits.mp3',
+            audioOffset: 18,
+            words: 'अनानास, अनार, तरबूज, अंजीर, शरीफा, आलूबुखारा, खजूर, खरबूज'
+          },
+          {
+            audio: 'hi/hi-vegetables.mp3',
+            audioOffset: 0,
+            words:
+              'गाजर, चुकंदर, प्याज, टमाटर, आलू, पत्तागोभी, फूलगोभी, भुट्टा, खीरा, लहसुन'
+          },
+          {
+            audio: 'hi/hi-vegetables.mp3',
+            audioOffset: 20,
+            words:
+              'अदरक, कद्दू, बादाम, फलि, करेला, बैंगन, शिमला मिर्च, काजू, हरी मिर्च, नारियल '
+          },
+          {
+            audio: 'hi/hi-vegetables.mp3',
+            audioOffset: 40,
+            words:
+              'धनिया पत्ता, करी पत्ता, सेहजन, मटर, भिन्डी, पुदीना, मूंगफली, मूली, पालक'
+          },
+          {
+            audio: 'hi/hi-vehicles.mp3',
+            audioOffset: 0,
+            words: 'हवाईजहाज, नाव, बस, गाड़ी, समुंद्रीजहाज, ट्रैक्टर'
+          },
+          {
+            audio: 'hi/hi-vehicles.mp3',
+            audioOffset: 12,
+            words: 'रेलगाडी, रोगीवाहन, हाथगाडी, खोदक मशीन, दमकल'
+          },
+          {
+            audio: 'hi/hi-house.mp3',
+            audioOffset: 0,
+            words: 'मकान, मेज, बिस्तर, मोमबत्ती, कुर्सी, अलमारी, बच्चा, लड़का'
+          },
+          {
+            audio: 'hi/hi-house.mp3',
+            audioOffset: 16,
+            words: 'लड़की, आदमी, स्त्री, गेंद, ऐनक, दीवार घड़ी, घडी'
+          },
+          {
+            audio: 'hi/hi-others.mp3',
+            audioOffset: 0,
+            words: 'सूर्य, चांद, बारिश, तारा, दूध, अंगूठी, कूंचा, हड्डी'
+          },
+          {
+            audio: 'hi/hi-others.mp3',
+            audioOffset: 16,
+            words: 'घंटी, पक्षी, शंख, पांच, ढोल, खाना, फल, राजा'
+          },
+          {
+            audio: 'hi/hi-others.mp3',
+            audioOffset: 32,
+            words: 'पतंग, सीढ़ी, लड्डू, पत्ता, ऋषि मुनि, चक्का, सीटी, दुनिया'
+          },
+          {
+            audio: 'hi/hi-school.mp3',
+            audioOffset: 0,
+            words:
+              'रबड़, कलम, पेंसिल, पेमाना, बस्ता, कैंची, चोखा करने वाला, पानी की बोतल, गणना यंत्र, किताब, रंग पेंसिल, चित्रांकनी '
+          },
+          {
+            audio: 'hi/hi-tools.mp3',
+            audioOffset: 0,
+            words:
+              'कुल्हाडी, तराज़ू, श्यामपट, रबर की पाइप , प्‍लास,  सूक्ष्मदर्शी, सुई, कुठला, हल, आरा'
+          },
+          {
+            audio: 'hi/hi-tools.mp3',
+            audioOffset: 20,
+            words:
+              'कुदाल, आला, दूरबीन, परखनली, तापमान-यंत्र, करनी, दिशा सूचक यंत्र, चित्रफलक, लोहा-आरी, छुरी '
+          }
+        ]
+      }
+    ]
+  };
+  
