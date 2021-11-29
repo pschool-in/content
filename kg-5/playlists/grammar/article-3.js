9c8ef11b18647fdaec2eff2cb39468ae18e8a4ad
Pradeepa Ravi Kumar <89005688+PRADEEPA03@users.noreply.github.com>
2021-11-30 00:41:22 +0530

    Update article-3.js

--- kg-5/playlists/grammar/article-3.js
+++ kg-5/playlists/grammar/article-3.js
@@ -62,7 +62,7 @@ I will meet *the (a, an)* President today.
 The mangoes cost thirty rupees *a (an, the)* piece.
 I have *an (a, the)* exam today. 
 *A (an, the)* dog is barking.
-She goes to temple in *the (a, an)* morning.
+She goes to temple in *the (a, an)* mornings.
 Abhinav is *the (a, an)* best singer in the class.
 *The (A, An)* camel is the ship of the desert.
 Aarav is sleeping on *the (a, an)* grey sofa.
@@ -73,7 +73,7 @@ He is holding *a (an, the)* bag.`,
 My brother bought *a (an, the)* gift for me. 
 He is eating *an (the, a)* apple. 
 I saw *an (a, the)* elephant.
-My sister poured honey on *the (a, an)* fruits.
+My sister poured honey on *the (a, an)* fruits. 
 I saw *a (an, the)* snake in my garden.
 My uncle is *a (the, an)* policeman.
 Are you going to *the (a, an)* park this evening?
@@ -118,6 +118,56 @@ She is eating *a (an, the)* chocolate pastry.`,
           ]
         }
       },
+      {
+        type: 'selectWord',
+        label: 'Select - Article -1',
+        id: 'select-word',
+        commonData: {
+          title: 'Select the article in the sentence.'
+        },
+        data: [
+          `*An* elephant is a mammal.
+          Eating *an* apple everyday can keep a doctor away.
+          Yesterday, I bought *a* white umbrella.
+          *The* music is playing on the TV.
+          I bought dozen bananas from *the* market.`,
+  
+          `Cheetah is *the* fastest land animal.
+          My dad is *a* professor in Osmania University.
+          My son has *a* big world map in his room.
+          There is *a* dog in the yard.
+          I saw *an* owl on the branch.`,
+  
+         `The teacher read *a* story in the class.
+         I want *a* burger for dinner.
+         Ram is *a* very smart boy.
+         We have *a* black dog.
+         Preethi is *an* honest student.`
+        ]
+      },
+      {
+        type: 'selectWord',
+        label: 'Select - Article-2',
+        id: 'select-word-2',
+        commonData: {
+            title: 'Select the articles in the paragraph.',
+            multiSelect: true
+         },
+        data: [
+`I live in Hyderabad. It is *a* big city. *The* Charminar is *a* famous monument in our city. 
+*The* Birla Planetarium is *the* most frequently visited place by many students every year.
+Nehru Zoological park is *a* famous zoo in Hyderabad which consists of different kinds of animals.`,
+
+`My neighborhood is *a* very nice place. There is *a* big supermarket at *the* end of *the* road. 
+There is also *a* small park near my home. Also we have *a* health center in *the* colony. 
+In addition, there is *a* preschool where my little sister studies.`,
+
+`Once upon *a* time *a* women had *an* only son whose name was Aladdin. 
+One day as he was looking for wild figs out in the desert, he came across *a* dungeon.
+Aladdin went in to *the* dungeon and found *a* magic lamp.
+When Aladdin rubbed *the* magic lamp *a* genie arrived and asked him to wish for something.`
+            ]
+        },
        {
         id: 'fill-up-3',
         label: 'Complete the Story',
@@ -131,10 +181,9 @@ She is eating *a (an, the)* chocolate pastry.`,
 `One afternoon *a (the, an)* fox was walking through *the (a, an)* forest and spotted *a (the, an)* bunch of grapes hanging from over *a (an, the)* lofty branch. “Just *the (a, an)* right thing to quench my thirst,” he thought. Taking a few steps back, *the (a, an)* fox jumped and just missed *the (a, an)* hanging grapes. Again *the (a, an)* fox took a few paces back and tried to reach them but still failed.After *an (a, the)* hour finally *the (a, an)* fox gave up and turned up his nose and said, “They’re probably sour anyway,” and walked away.`,
 
 `One hot day, *a (an, the)* thirsty crow flew all over the fields looking for water. For a long time, he could not find any. He felt very weak, almost lost all hope. Suddenly, he saw *a (an, the)* water jug below *the (a, an)* tree. He flew straight down to see if there was any water inside. Yay! he could see some water inside the jug! *The (a, an)* crow tried to push his head into the jug. Sadly, he found that *the (a, an)* neck of the jug was too narrow. Then he tried to push *the (a, an)* jug to tilt for the water to flow out, but the jug was too heavy.
-
 *The (a, an)* crow thought hard for a while. Then, looking around it, he saw some pebbles. He suddenly had *a (an, the)* good idea. 
 He started picking up *the (a, an)* pebbles one by one, dropping each into the jug. As more and more pebbles filled *the (a, an)* jug, the water level kept rising. Soon it was high enough for *the (a, an)* crow to drink. His plan had worked!`,
-     
+
 `Once upon a time there was *an (a, the)* old farmer who had a horse and a donkey. 
 He was fond of both of them but he took better care of *the (a, an)* horse.
 *The (A, An)* donkey always carried heavy load to the market. 
@@ -143,17 +192,17 @@ One day, the donkey moaned and said: I am so weak! Couldn't you take some of my 
 But soon he had to stop again and said: please help me or I will never reach *the (a, an)* market alive.
 But *the (a, an)* horse replied: Rubbish! Come on, you will manage this time too. 
 After *a (the, an)* few steps the donkey dropped dead on the ground. 
-*The (a, an)* master saw donkey dead and felt vey sad and later transferred all *the (a, an)* load to the horse.`,       
- 
- `One night Manoj and his friend Suresh were travelling by train. 
+*The (a, an)* master saw donkey dead and felt vey sad and later transferred all *the (a, an)* load to the horse.`,
+
+`One night Manoj and his friend Suresh were travelling by train. 
 They spoke for sometime and after *a (the, an)* while they switched off *the (a, an)* light and fell asleep. 
 Soon, *a (an, the)* thief entered the compartment. 
 He saw Manoj's mobile lying beside him. 
 *The (A, An)* thief carefully took it away without disturbing him. *The (A, An)* next morning Suresh wanted to use Manoj's mobile.
-He asked, "Can I use your mobile, Manoj?". Sure, replied Suresh. 
+He asked, "Can I use your mobile, Manoj?". Sure, replied suresh. 
 When he searched, he could not find his mobile anywhere. He said, "I think someone stole it". 
-Oh! I am so sorry, said Suresh. But Manoj smiled and said it is ok, as *the (a, an)* thief will not be able to use it as I have switched it off.`          
+Oh! I am so sorry, said suresh. But Manoj smiled and said it is ok, as *the (a, an)* thief will not be able to use it as I have switched it off.`
         ]
-     }
+      }
     ]
 };
