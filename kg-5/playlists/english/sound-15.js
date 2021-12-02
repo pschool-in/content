c00262b812e1f6aec5701aa0bd23df8e60269332
sanha shahal <sanhashahal3@gmail.com>
2021-12-02 12:52:37 +0530

    updated

---
+++ kg-5/playlists/english/sound-15.js
@@ -0,0 +1,97 @@
+export default {
+    label: 'Vehicles and Jobs 2',
+    id: 'sound-15',
+    list: [
+      {
+        type: 'rightOne',
+        label: 'Identify Vehicles',
+        id: 'sound-vehicles',
+        lockAfter: 2,
+        commonData: {
+          title: 'Listen to the sound and click on the vehicle.',
+          type: 'words',
+          audio: 'kg-5/vehicles-jobs-1.mp3'
+        },
+        data: [
+          {
+            audioOffset: 0,
+            words:
+              'aeroplane, boat, bus, car, cycle, helicopter, motorbike, ship, tractor, lorry'
+          },
+          {
+            audioOffset: 20,
+            words:
+              'train, ambulance, auto, cart, excavator, fire engine, van, bulldozer, rocket, submarine'
+          }
+        ]
+      },
+      {
+        type: 'rightOne',
+        label: 'Identify Jobs',
+        id: 'sound-job',
+        lockAfter: 2,
+        commonData: {
+          title: 'Listen to the sound and click on the picture.',
+          type: 'words',
+          audio: 'kg-5/vehicles-jobs-1.mp3'
+        },
+        data: [
+          {
+            audioOffset: 40,
+            words:
+              'astronaut, clown, fisherman, postman, teacher, blacksmith, cobbler, goldsmith, potter, weaver'
+          },
+          {
+            audioOffset: 60,
+            words:
+              'carpenter, diver, nurse, sailor, chef, doctor, police, soldier, barber, washerman'
+          }
+        ]
+      },
+      {
+        type: 'dictation',
+        label: 'Dictation Vehicles',
+        id: 'dictation-vehicles',
+        lockAfter: 2,
+        commonData: {
+          title: 'Listen to the sound and type the word.',
+          audio: 'kg-5/vehicles-jobs-1.mp3'
+        },
+        data: [
+          {
+            audioOffset: 0,
+            words:
+              'aeroplane, boat, bus, car, cycle, helicopter, motorbike, ship, tractor, lorry'
+          },
+          {
+            audioOffset: 20,
+            words:
+              'train, ambulance, auto, cart, excavator, fire engine, van, bulldozer, rocket, submarine'
+          }
+        ]
+      },
+      {
+        type: 'dictation',
+        label: 'Dictation Jobs',
+        id: 'dictation-job',
+        lockAfter: 2,
+        commonData: {
+          title: 'Listen to the sound and type the word.',
+          audio: 'kg-5/vehicles-jobs-1.mp3'
+        },
+        data: [
+          {
+            audioOffset: 40,
+            words:
+              'astronaut, clown, fisherman, postman, teacher, blacksmith, cobbler, goldsmith, potter, weaver'
+          },
+          {
+            audioOffset: 60,
+            words:
+              'carpenter, diver, nurse, sailor, chef, doctor, police, soldier, barber, washerman'
+          }
+        ]
+      }
+    ]
+  };
+  
