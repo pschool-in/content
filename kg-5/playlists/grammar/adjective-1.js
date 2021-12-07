c1a403c0f44247a63b18d72bd5c85df1aafddfd3
Rethna <rethna2@gmail.com>
2021-12-07 07:31:51 +0530

    updated adjective

--- kg-5/playlists/grammar/adjective-1.js
+++ kg-5/playlists/grammar/adjective-1.js
@@ -62,22 +62,22 @@ eg: `,
       },
       data: [
         `A ball is *round* in shape.
-        A *good* chess player is usually *smart*.
-        My friend has a *brown* muffler.
-        I have a *red* cap.
-        Aman is *confused* about a maths problem.`,
+A *good* chess player is usually *smart*.
+My friend has a *brown* muffler.
+I have a *red* cap.
+Aman is *confused* about a maths problem.`,
 
         `I read a story about an *ugly* witch.
-        A *fast* car always win in races.
-        Coconut trees are *tall*.
-        A lion roars very *loudly*.
-        Goods train is very *long*.`,
+A *fast* car always win in races.
+Coconut trees are *tall*.
+A lion roars very *loudly*.
+Goods train is very *long*.`,
 
         `A *poor* man was sitting on roadside.
-       This is a *pleasant* weather.
-       A *small* rat lives in our house.
-       I have a *purple* dress.
-       A *clean* table.`
+This is a *pleasant* weather.
+A *small* rat lives in our house.
+I have a *purple* dress.
+A *clean* table.`
       ]
     },
     {
@@ -90,16 +90,16 @@ eg: `,
       },
       data: [
         `Ugly, Ugley
-       Brown, Browne
-       Loud, Loude
-       Clean, Chleane
-       Tall, Tale`,
+Brown, Browne
+Loud, Loude
+Clean, Chleane
+Tall, Tale`,
 
         `Long, Longe
-       Fast, Fest
-       Small, Smell
-       Big, Beg
-       Smart, Smert`
+Fast, Fest
+Small, Smell
+Big, Beg
+Smart, Smert`
       ]
     },
     {
@@ -112,22 +112,22 @@ eg: `,
 
       data: [
         `Big, Elephant
-       Dark, Night
-       Small, Ant
-       Pleasant, Weather
-       Loud, Lion`,
+Dark, Night
+Small, Ant
+Pleasant, Weather
+Loud, Lion`,
 
         `Clean, Room
-       Smart, Student
-       Long, Train
-       Ugly, Pig
-       Round, Ball`,
+Smart, Student
+Long, Train
+Ugly, Pig
+Round, Ball`,
 
         `Red, Colour
-       Two, Hands
-       Soft, Toy
-       Fast, Horse
-       Sweet, Dish`
+Two, Hands
+Soft, Toy
+Fast, Horse
+Sweet, Dish`
       ]
     },
     {
