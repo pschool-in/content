0d92036b56c0cc655204d1865d7c898efcdbeaf5
PRADEEPA03 <preethee_12@yahoo.com>
2021-12-08 20:36:53 +0530

    Newly added

--- knowYourEnglish/playlists/kye-qa-7.js
+++ knowYourEnglish/playlists/kye-qa-7.js
@@ -1,1 +1,76 @@
-
+export default {
+    label: 'Know Your English Q & A',
+    id: 'kye-qa-7',
+    list: [
+{
+  type: 'passage',
+  label: 'Know Your English: July 11',
+  id: '100',
+  data: {
+    title: `Know Your English: July 11`,
+    text: `What is the meaning and origin of ‘playing to the gallery’? 
+This idiom which has a negative connotation has been around for several hundred years. When you accuse someone of ‘playing to the gallery’, you are suggesting that the individual, instead of doing the job assigned to him, is intent on gaining the admiration and support of the people around him. Instead of taking tough decisions, he does and says things which will make him popular — much like our politicians.
+Most news channels are playing to the gallery.
+In the world of theatre, the word ‘gallery’ was used to refer to the people sitting in the cheapest seats. Any actor or playwright who tried to please the poor and unsophisticated people sitting in these seats was seen to be doing something demeaning — by trying to win their approval, he was doing something inappropriate.
+What is the meaning of ‘logophile’?
+First let us deal with the pronunciation of the word. The first ‘o’ can be pronounced like the ‘o’ in ‘dog’ and ‘fog’, and the second like the ‘a’ in ‘china’. One way of pronouncing this word is ‘LOG-e-file’ with the stress on the first syllable. The word comes from the Greek ‘logo’ meaning ‘word’ or ‘speech’ and ‘phile’ meaning ‘lover of’. A ‘logophile’ is someone who loves words.
+Our new site will interest the logophiles.
+What is the difference between ‘chubby’ and ‘fat’?
+Both words are frequently used to refer to someone who is overweight. Of the two, however, ‘fat’ has a negative connotation. It suggests that the person is physically unappealing. A fat person may be extremely overweight , but he does not like being called ‘fat’ to his face. Nowadays, there is an attempt to soften the blow by labelling him as being ‘horizontally challenged’. The word ‘fat’ can be used with men, women and children. The use of ‘chubby’, on the other hand, is mostly limited to babies and small children who are slightly overweight. Unlike fat children, the chubby ones are usually attractive or pleasant to look at. Babies with chubby cheeks are considered to be cute. The word comes from ‘chub’, the name of a thick-bodied river fish found in Europe.
+There were several chubby children running around.
+Ram’s daughter is not fat. She’s just chubby.
+Is it okay to say, ‘I didn’t complain because Suchi was there’?
+In terms of grammar, there is nothing wrong with the sentence. Careful users of the language, however, would argue that the sentence is ambiguous; it can be interpreted in two different ways.
+It could mean that you did not complain because Suchi was present. Her presence prevented you from complaining. The sentence could also mean that you actually complained — but your complaint had nothing to do with Suchi being there.
+You would have complained even if she hadn’t been there. Negative sentences with ‘because’ often cause such confusion.
+I am in shape. Round is a shape. — Anonymous`
+    }
+  },
+{
+type: 'passage',
+label: 'Know Your English',
+id: '200',
+data: {
+  title: `Know Your English`,
+  text: `What is the opposite of ‘hibernate’? 
+An animal that sleeps through the winter months is said to ‘hibernate’. In everyday contexts, the word is frequently used to mean ‘inactive’. For example, if after a hectic week at the office, you plan to spend the weekend relaxing or ‘chilling’ at home, you can tell your friends you are going to hibernate during the weekend. The opposite of ‘hibernate’ is ‘estivate’ — also spelt ‘aestivate’. The ‘es’ is pronounced like the ‘es’ in ‘yes’ and ‘mess’, and the following ‘i’ is like the ‘a’ in ‘china’. The final syllable rhymes with ‘gate’, ‘late’ and ‘date’. One way of pronouncing the word is ‘ES-ti-vate’ with the stress on the first syllable. It comes from the Latin ‘aestus’ meaning ‘heat’. Many animals that live in the desert ‘estivate’ during the summer months — they remain asleep or inactive during the hottest months of the year.
+What is the meaning and origin of ‘tip of the ice berg’?
+This is an expression that has been around for several hundred years. We know from the science lessons we did in school that only a very small portion of the iceberg actually remains above water. The bulk of it remains under water; what we actually see floating is only a very small part of the iceberg. Therefore, when you say that a problem or a situation is just the ‘tip of the iceberg’, what you are suggesting is that just like the iceberg, there is more to the problem/situation than meets the eye. What people are able to see or understand is only a very small part of a big/serious problem.
+The gradual downsizing that we see happening in the software industry is only the tip of the iceberg. I think many big companies will fold up within a year.
+If you ask me, the reported cases of missing girls is only the tip of the iceberg.
+How is the word ‘plait’ pronounced?
+There are two ways of pronouncing this word. Some people make it rhyme with the word ‘flat’; others pronounce it like the word ‘plate’. The word comes from the French ‘pleit’ meaning ‘fold’. A ‘plait’ refers to the way a woman/girl wears her hair. In the past, it was common to see young girls wearing two plaits and a married woman with a single plait. Another term for plait is ‘braid’ — rhymes with ‘made’, ‘fade’ and ‘jade’. Americans prefer ‘braid’, while the British seem partial to ‘plait’.
+Make sure her hair is tightly plaited/braided.
+Which is correct? ‘Sorry about/for breaking your beautiful vase’? 
+Both ‘about’ and ‘for’ are possible. When you feel bad about something that you have done, you can apologise by saying ‘sorry for’ or ‘sorry about’. In both cases, you are sorry for/about ‘doing something’.
+I’m truly sorry for/about calling you all sorts of names.
+I’m very sorry about/for breaking your beautiful vase.
+If you want to succeed in politics, you must keep your conscience well under control. — David Lloyd George`
+    }
+  },
+{
+  type: 'passage',
+  label: 'Know Your English: June 13',
+  id: '300',
+  data: {
+    title: `Know Your English: June 13`,
+    text: `What is the meaning of ‘when push comes to shove’?
+This is an informal expression and it has more or less the same meaning as ‘if the worst comes to the worst’. Sometimes, we delay taking action of any kind; we wait till the last minute to actually do so. The situation becomes so bad that we are compelled to do what we should have done a long time ago. This action becomes our last resort to save the situation. It is also possible to say, ‘if push comes to shove’. According to some scholars, the expression comes from the world of rugby.
+If push comes to a shove, we can always postpone the wedding.
+When push came to shove, Mr. Mehta compromised and took a pay cut.
+Which is correct: If I was/were? 
+Actually, both are correct. People who love their grammar would argue that since the sentence begins with an ‘if clause’, the verb that follows should be in the plural: If I were the Mayor, I would ask people not to cut down trees. But there is a tendency, even among native speakers of English, to use the singular verb in informal contexts. If I was the Mayor, I would request people to pay their taxes. The use of the singular verb should be avoided in formal contexts.
+If I were to take the job, my wife would be terribly unhappy.
+If I was to eat all this, I would put on a lot of weight.
+How is the word ‘erudite’ pronounced? 
+There are different ways of pronouncing the word. The simplest way is to pronounce the first ‘e’ like the ‘e’ in ‘set’, ‘bet’ and ‘jet’, and the following ‘u’ like the ‘u’ in ‘pull and ‘full’. The final ‘dite’ rhymes with ‘kite’ and ‘bite’. The word is pronounced ‘E-ru-dite’ with the stress on the first syllable. It comes from the Latin ‘eruditus’ meaning ‘well informed’. An ‘erudite’ is someone who is very learned; he is highly educated and knows a lot about things. He is a scholar. It is interesting to note that it is related to the word ‘rude’, which in turn comes from the Latin ‘rudis’ meaning ‘rough’ or ‘unlearned’.
+The students found it difficult to follow the erudite lecture.
+The erudite members of the faculty decided not to join the strike.
+What is the difference between ‘won’t’ and ‘wont’?
+In terms of pronunciation, there is no difference between the two; the two words rhyme with ‘don’t’. ‘Won’t’ is the contraction of ‘will not’, and is mostly used in speech and in informal styles of writing. ‘Will not’ is preferred in formal styles of writing. ‘Wont’, on the other hand, is considered formal. It is normally used to talk about someone’s habit or way of doing things. For example, if you say, ‘The Minister, as was his wont, shouted at the organisers’, what you are suggesting is that the Minister has the habit of shouting at people. It is customary for him to do so.
+The teacher, as was her wont, cracked a lot of jokes in class.
+Erudition: dust shaken out of a book into an empty skull. — Ambrose Bierce`
+      }
+    }
+  ]
+};
