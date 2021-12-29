905f8de56904376bdceccda3bfffd0450ca0bc95
Rethna <rethna2@gmail.com>
2021-12-29 09:43:15 +0530

    minor correction

--- kg-5/playlists/social/goods-services.js
+++ kg-5/playlists/social/goods-services.js
@@ -8,8 +8,13 @@ export default {
       id: 'reading',
       data: {
         title: 'Goods and Services',
-        text:
-          "We, human beings cannot live alone, on our own. We interact with many people and do transactions and get our requirements done. For example, we cannot produce the food we eat on our own. We go to a store and buy different items, and come home and cook. Here the items what we bought from the store are known as 'goods'. \n If we don't have time to cook on our own, then we go to a restaurant, where our ordered food will be delivered by a waiter. Here, what the restaurant provides is 'service'. \n Why the food is not usually considered as 'goods' and rather considered as 'service'? Because, apart from the food, they also provide good atmosphere, tables and chairs for us to sit, waiter to take orders, and a chef to cook. If we buy service, we usually don't have to worry about internal workings and the service provider will take care of it. \n Doctor, police, lawyer, Carpenter provides service to us. They don't sell anything to us. But their service is very important when we are in need. All kinds of stores, say super market,  provision stores, medicals, stationery stores etc sells 'goods' to us. \n  With little bit of thinking, we can figure out whether something is 'goods' or 'service'."
+        text: [
+          `We, human beings cannot live alone, on our own. We interact with many people and do transactions and get our requirements done. For example, we cannot produce the food we eat on our own. We go to a store and buy different items, and come home and cook. Here the items what we bought from the store are known as 'goods'. 
+ If we don't have time to cook on our own, then we go to a restaurant, where our ordered food will be delivered by a waiter. Here, what the restaurant provides is 'service'. 
+ Why the food is not usually considered as 'goods' and rather considered as 'service'? Because, apart from the food, they also provide good atmosphere, tables and chairs for us to sit, waiter to take orders, and a chef to cook. If we buy service, we usually don't have to worry about internal workings and the service provider will take care of it. 
+ Doctor, police, lawyer, Carpenter provides service to us. They don't sell anything to us. But their service is very important when we are in need. All kinds of stores, say super market,  provision stores, medicals, stationery stores etc sells 'goods' to us. 
+  With little bit of thinking, we can figure out whether something is 'goods' or 'service'.`
+        ]
       }
     },
     {
