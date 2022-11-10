25ad213c38cdacf592f7192dfaf17ae84749fc7c
Rethna <rethna2@gmail.com>
2022-11-10 10:15:34 +0530

    updated tamil

--- kg-5/playlists/tamil/ta-pic-words.js
+++ kg-5/playlists/tamil/ta-pic-words.js
@@ -69,6 +69,145 @@ export default {
       ]
     },
     {
+      type: 'completeWord',
+      label: 'நிரப்புக',
+      id: 'complete-words',
+      commonData: {
+        images: 'custom',
+        lang: 'ta',
+        title: 'விடுபட்ட எழுத்தை தேர்வு செய்க'
+      },
+      data: [
+        `cat| பூனை | __ 
+  dog| நாய் | __
+  goat| ஆடு | __
+  horse| குதிரை | ___
+  cow|  பசு| __
+  pig| பன்றி | ___
+  buffalo| எருமை| ___
+  donkey| கழுதை | ___
+  bull| காளை | __ 
+  camel| ஒட்டகம் | _____`,
+
+        `elephant| யானை | __
+  lion| சிங்கம் | ____
+  monkey| குரங்கு | ____
+  tiger| புலி| __
+  kangaroo| கங்காரு| ____
+  leopard| சிறுத்தை| ____
+  fox| நரி | __
+  rabbit| முயல்| ___
+antelope| மான்| __
+bear| கரடி | ___ `,
+
+        `fish| மீன் | __
+  frog| தவளை | ___
+  snake| பாம்பு | ___
+  squirrel| அணில் | ___
+  tortoise| ஆமை | __
+  rat| எலி| __
+  fly| ஈ| _ 
+  ant| எறும்பு | ____
+  lizard| பல்லி| ___
+  crab| நண்டு | ___`,
+
+        `mosquito| கொசு | __
+  dragonfly| தட்டான்| ____
+  earthworm| மண்புழு| ____
+  bug| வண்டு| ___
+  prawn| இறால்| ___
+  bee| தேனீ| __
+  spider| சிலந்தி| ____
+  winged-termites| ஈசல் | ___
+  bat| வௌவால் |___ 
+  garden-lizard| ஓணான்| ___`,
+
+        `crow| காகம் | ___
+  dove| புறா | __
+  duck| வாத்து | ___
+  hen| கோழி | __
+  parrot| கிளி | __
+  peacock| மயில் | ___`,
+
+        `crane| கொக்கு| ___
+  rooster| சேவல்| ___
+  koel| குயில்| ___
+  eagle| கழுகு| ___
+  myna| மைனா| __
+  owl| ஆந்தை| ___`
+      ]
+    },
+    {
+      type: 'connectLetters',
+      label: 'எழுத்துக்களை இணைக்கவும்',
+      id: 'connect-fruits',
+      commonData: {
+        title: 'எழுத்துக்களை இணைத்து சொல் உருவாக்குக ',
+        printTitle: 'Form the word using the letters.',
+        type: 'image',
+        lang: 'ta',
+        fontSize: '1rem'
+      },
+      data: [
+        `cat, பூனை
+  dog, நாய்
+  goat, ஆடு
+  horse, குதிரை
+  cow,  பசு
+  pig, பன்றி
+  buffalo, எருமை
+  donkey, கழுதை
+  bull, காளை
+  camel, ஒட்டகம்`,
+
+        `elephant, யானை
+  lion, சிங்கம் 
+  monkey, குரங்கு 
+  tiger, புலி
+  kangaroo, கங்காரு
+  leopard, சிறுத்தை
+  fox, நரி
+  rabbit, முயல்
+antelope, மான்
+bear, கரடி`,
+
+        `fish, மீன்
+  frog, தவளை
+  snake, பாம்பு
+  squirrel, அணில்
+  tortoise, ஆமை
+  rat, எலி
+  ant, எறும்பு
+  lizard, பல்லி
+  crab, நண்டு`,
+
+        `mosquito, கொசு
+  dragonfly, தட்டான்
+  earthworm, மண்புழு
+  bug, வண்டு
+  prawn, இறால்
+  bee, தேனீ
+  spider, சிலந்தி
+  winged-termites, ஈசல்
+  bat, வௌவால்
+  garden-lizard, ஓணான்`,
+
+        `crow, காகம்
+  dove, புறா
+  duck, வாத்து
+  hen, கோழி
+  parrot, கிளி
+  peacock, மயில்`,
+
+        `crane, கொக்கு
+  rooster, சேவல்
+  koel, குயில்
+  eagle, கழுகு
+  myna, மைனா
+  owl, ஆந்தை`
+      ]
+    },
+    {
       type: 'dictation',
       label: 'ஓசை கேட்டு எழுதுக',
       id: 'dictation',
