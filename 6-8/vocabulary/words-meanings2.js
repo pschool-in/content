d084b79b28d2636d8c61ea01bda8b9c6af674d97
priya2786 <priyahh123@gmail.com>
2023-02-16 12:17:35 +0530

    words -meanings2 updated

--- 6-8/vocabulary/words-meanings2.js
+++ 6-8/vocabulary/words-meanings2.js
@@ -12,35 +12,37 @@ export default {
               text: [
                 {
                   type: 'html',
-                  text: `<b> Theist</b> : One who believes in the existence of God<br>
-                  <b>anniversary</b> : The yearly return of a date <br>
+                  text: `<b>orator</b> - One who makes a powerful public speech <br>
+                  <b>spokesman</b> - One who speaks for others<br>
+                  <b> theist</b> : One who believes in the existence of God<br>
                   <b>audience</b>: An assembly of listeners <br>
-                  <b>autobiography</b>: The life history of a person written by himself <br>
                   <b>amateur</b>: One who plays for pleasure <hr>`
                  },
                 {
                   type: 'html',
                   text: `<b>botany</b> - The science of plant life <br>
+                  <b>zoology</b> - Science of the life of animals <br>
                   <b>biography</b> - The life history of a person written by another <br>
-                  <b>contemporary</b> - A person who lives at the same time as an other <br>
-                  <b>extempore</b> - Done without any preparation <br>
-                  <b>century</b> -A period of hundred years <hr>`
+                  <b>autobiography</b>: The life history of a person written by himself <br>
+                  <b>manuscript</b> - A piece of writing before it is published<br>
+                  <b>catalogue</b> - A list of books <hr>`
                 },
-                {
+                
+                 {
                   type: 'html',
-                  text: `<b>orator</b> - One who makes a powerful public speech <br>
-                  <b> parasite</b> - That which lives on another <br>
+                  text: ` <b>century</b> -A period of hundred years <br>
+                  <b>anniversary</b> : The yearly return of a date <br>
+                  <b>fortnight</b> - A period of two weeks <br>
                   <b> simultaneous</b> -Happening at the same time <br>
-                  <b>catalogue</b> - A list of books <br>
-                  <b>cemetry</b> - A place for burial of dead bodies <hr>`
+                  <b>contemporary</b> - A person who lives at the same time as an other <hr>`
                 },
                 {
                   type: 'html',
-                  text: `<b>cosmopolitan</b> - Of or from all parts of the world <br>
+                  text: ` <b> monarchy</b> - Government by a king <br>
                   <b>democracy</b> - Government formed by the representatives of the people<br>
+                  <b>dictatorship</b> - Government carried on by an absolute ruler<br>
                   <b>illegal</b> - Contrary to law <br>
-                  <b>tragedy</b> - A play that ends in death <br>
-                  <b>manuscript</b> - A piece of writing before it is published <hr>` 
+                  <b>tragedy</b> - A play that ends in death<hr>` 
               },
             ]
             }
@@ -54,34 +56,36 @@ export default {
               text: [
                 {
                   type: 'html',
-                  text:`<b>spokesman</b> - One who speaks for others<br>
+                  text:`
                   <b>honorary</b> - Performing work without pay <br>
-                  <b>dictatorship</b> - Government carried on by an absolute ruler<br>
                   <b>illiterate</b> - One who does not know how to read and write <br>
+                  <b>orphan</b> - A child whose parents are dead<br>
+                  <b>widower</b> - A man whose wife is dead <br>
                   <b>widow</b> -A woman whose husband is dead <hr>`
                 },
                 {
                   type: 'html',
                 text:`<b>fatalist</b> - One who believes in fate<br>
+                <b> atheist</b> -One who does not believe in the existence of God <br>
                   <b>library</b> - A room containing many books <br>
-                  <b>fortnight</b> - A period of two weeks <br>
-                  <b>fatal</b> - Resulting in death <br>
-                  <b>optimist</b> - One who takes a bright view of things<hr>`
+                  <b>cemetry</b> - A place for burial of dead bodies <br>
+                  <b>extempore</b> - Done without any preparation <hr>`
                 },
                 {
                   type: 'html',
-                text:` <b>pessimist</b> - One who takes a dark view of things <br>
-                  <b>inaudible</b> - That which cannot be heard <br>
-                  <b>orphan</b> - A child whose parents are dead<br>
-                  <b>widower</b> - A man whose wife is dead <br>
-                  <b>vegetarian</b> - One who lives on vegetables or plant food <hr>`
+                text:`
+                <b>pessimist</b> - One who takes a dark view of things <br>
+                 <b>optimist</b> - One who takes a bright view of things<br>
+                 <b>alien</b>-One who resides in a country of which he is not a citizen<br>
+                 <b>cannibal</b> -One who eats human flesh <br>
+                <b>vegetarian</b> - One who lives on vegetables or plant food <hr>`
                 },
                 {
                   type: 'html',
-                  text:` <b>zoology</b> - Science of the life of animals <br>
+                  text:`<b>thermometer</b>-An instrument for measuring temperature <br>
                   <b>mortal</b> - Subject to death <br>
-                  <b> monarchy</b> - Government by a king <br>
-                  <b> atheist</b> -One who does not believe in the existence of God <br>
+                  <b> parasite</b> - That which lives on another <br>
+                  <b>fatal</b> - Resulting in death <br>
                   <b>panacea</b> -A cure of all diseases <hr>`
                 },
                 ]
@@ -96,11 +100,12 @@ export default {
                 text: [
                   {
                     type: 'html',
-                    text:`<b>thermometer</b>-An instrument for measuring temperature <br>
+                    text:`
+                    <b>invincible</b>-That which cannot be conquered<br>
                     <b>audible</b>-Loud enough to be heard<br>
                     <b>inaudible</b>-Not distinct enough to be heard<br>
-                     <b>edible</b>-Fit to be eaten<br>
-                    <b>inedible</b>- Unfit for human consumption<r>`
+                    <b>edible</b>-Fit to be eaten<br>
+                    <b>inedible</b>- Unfit for human consumption<hr>`
                   },
                   {
                     type: 'html',
@@ -108,25 +113,25 @@ export default {
                     <b>ineligible</b>-Not having the qualities of being chosen<br>
                     <b>legible</b>-Writing that is easy to read<br>
                     <b>illegible</b>-Writing that is easy to decipher<br>
-                    <b>literate</b>-Able to read<hr>`
+                    <b>ineffaceable</b>-That which cannot be rubbed out or blotted out<hr>`
                   },
                   {
                     type: 'html',
-                    text:`<b>illiterate</b>-Unable to read<br>
+                    text:`<b>literate</b>-Able to read<br>
+                    <b>illiterate</b>-Unable to read<br>
                     <b>legitimate</b>-Born of married parents<br>
                     <b>illegitimate</b>-Born of unmarried parents<br>
-                    <b>repatriate</b>-To send back a person to his own country<br>
-                    <b>expatriate</b>-To banish from one's country<hr>`
+                    <b>incorrigible</b>-Incapable of being redeemed from evil, i.e., beyond correction<hr>
+                    `
                   },
                   {
                     type: 'html',
-                    text:`
+                    text:`<b>cosmopolitan</b> - of or from all parts of the world <br>
                     <b>migrate</b>-To move from one country to another<br>
+                    <b>expatriate</b>-To banish from one's country<br>
+                    <b>repatriate</b>-To send back a person to his own country<br>
                     <b>Emigrant</b>-One who leaves his country to settle in another<br>
-                    <b>immigrant</b>-One who comes into a foreign country to settle there<br>
-                    <b>incorrigible</b>-Incapable of being redeemed from evil, i.e., beyond correction<br>
-                    <b>ineffaceable</b>-That which cannot be rubbed out or blotted out<br>
-                    <b>invincible</b>-That which cannot be conquered<hr>`
+                    <b>immigrant</b>-One who comes into a foreign country to settle there<hr>`
                   }
                   ]
                 }
