a7ea0503ddd13c2fee85d07160549573646153e0
priya2786 <priyahh123@gmail.com>
2023-02-16 11:31:43 +0530

    words-meanings2 added

---
+++ 6-8/vocabulary/words-meanings2.js
@@ -0,0 +1,136 @@
+export default {
+    label: 'Words and meaning',
+    id: 'words-meaning2',
+    lockAfter: 2,
+    list: [
+        {
+            id: 'reading',
+            type: 'passage',
+            label: 'Words Collection-1',
+            data: {
+              title: 'Words Collection',
+              text: [
+                {
+                  type: 'html',
+                  text: `<b> Theist</b> : One who believes in the existence of God<br>
+                  <b>anniversary</b> : The yearly return of a date <br>
+                  <b>audience</b>: An assembly of listeners <br>
+                  <b>autobiography</b>: The life history of a person written by himself <br>
+                  <b>amateur</b>: One who plays for pleasure <hr>`
+                 },
+                {
+                  type: 'html',
+                  text: `<b>botany</b> - The science of plant life <br>
+                  <b>biography</b> - The life history of a person written by another <br>
+                  <b>contemporary</b> - A person who lives at the same time as an other <br>
+                  <b>extempore</b> - Done without any preparation <br>
+                  <b>century</b> -A period of hundred years <hr>`
+                },
+                {
+                  type: 'html',
+                  text: `<b>orator</b> - One who makes a powerful public speech <br>
+                  <b> parasite</b> - That which lives on another <br>
+                  <b> simultaneous</b> -Happening at the same time <br>
+                  <b>catalogue</b> - A list of books <br>
+                  <b>cemetry</b> - A place for burial of dead bodies <hr>`
+                },
+                {
+                  type: 'html',
+                  text: `<b>cosmopolitan</b> - Of or from all parts of the world <br>
+                  <b>democracy</b> - Government formed by the representatives of the people<br>
+                  <b>illegal</b> - Contrary to law <br>
+                  <b>tragedy</b> - A play that ends in death <br>
+                  <b>manuscript</b> - A piece of writing before it is published <hr>` 
+              },
+            ]
+            }
+          },
+          {
+            id: 'reading',
+            type: 'passage',
+            label: 'Words Collection-2',
+            data: {
+              title: 'Words Collection',
+              text: [
+                {
+                  type: 'html',
+                  text:`<b>spokesman</b> - One who speaks for others<br>
+                  <b>honorary</b> - Performing work without pay <br>
+                  <b>dictatorship</b> - Government carried on by an absolute ruler<br>
+                  <b>illiterate</b> - One who does not know how to read and write <br>
+                  <b>widow</b> -A woman whose husband is dead <hr>`
+                },
+                {
+                  type: 'html',
+                text:`<b>fatalist</b> - One who believes in fate<br>
+                  <b>library</b> - A room containing many books <br>
+                  <b>fortnight</b> - A period of two weeks <br>
+                  <b>fatal</b> - Resulting in death <br>
+                  <b>optimist</b> - One who takes a bright view of things<hr>`
+                },
+                {
+                  type: 'html',
+                text:` <b>pessimist</b> - One who takes a dark view of things <br>
+                  <b>inaudible</b> - That which cannot be heard <br>
+                  <b>orphan</b> - A child whose parents are dead<br>
+                  <b>widower</b> - A man whose wife is dead <br>
+                  <b>vegetarian</b> - One who lives on vegetables or plant food <hr>`
+                },
+                {
+                  type: 'html',
+                  text:` <b>zoology</b> - Science of the life of animals <br>
+                  <b>mortal</b> - Subject to death <br>
+                  <b> monarchy</b> - Government by a king <br>
+                  <b> atheist</b> -One who does not believe in the existence of God <br>
+                  <b>panacea</b> -A cure of all diseases <hr>`
+                },
+                ]
+              }
+            },
+            {
+              id: 'reading',
+              type: 'passage',
+              label: 'Words Collection-3',
+              data: {
+                title: 'Words Collection',
+                text: [
+                  {
+                    type: 'html',
+                    text:`<b>thermometer</b>-An instrument for measuring temperature <br>
+                    <b>audible</b>-Loud enough to be heard<br>
+                    <b>inaudible</b>-Not distinct enough to be heard<br>
+                     <b>edible</b>-Fit to be eaten<br>
+                    <b>inedible</b>- Unfit for human consumption<r>`
+                  },
+                  {
+                    type: 'html',
+                    text:`<b>eligible</b>-Fit to be chosen or selected<br>
+                    <b>ineligible</b>-Not having the qualities of being chosen<br>
+                    <b>legible</b>-Writing that is easy to read<br>
+                    <b>illegible</b>-Writing that is easy to decipher<br>
+                    <b>literate</b>-Able to read<hr>`
+                  },
+                  {
+                    type: 'html',
+                    text:`<b>illiterate</b>-Unable to read<br>
+                    <b>legitimate</b>-Born of married parents<br>
+                    <b>illegitimate</b>-Born of unmarried parents<br>
+                    <b>repatriate</b>-To send back a person to his own country<br>
+                    <b>expatriate</b>-To banish from one's country<hr>`
+                  },
+                  {
+                    type: 'html',
+                    text:`
+                    <b>migrate</b>-To move from one country to another<br>
+                    <b>Emigrant</b>-One who leaves his country to settle in another<br>
+                    <b>immigrant</b>-One who comes into a foreign country to settle there<br>
+                    <b>incorrigible</b>-Incapable of being redeemed from evil, i.e., beyond correction<br>
+                    <b>ineffaceable</b>-That which cannot be rubbed out or blotted out<br>
+                    <b>invincible</b>-That which cannot be conquered<hr>`
+                  }
+                  ]
+                }
+              },  
+        ]
+      };
+  
