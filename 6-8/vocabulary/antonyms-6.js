2c9d8195d2d9bfe8bab91848dc6ce01757b7027d
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-03-09 08:54:08 +0530

    modified antonyms-6.js

--- 6-8/vocabulary/antonyms-6.js
+++ 6-8/vocabulary/antonyms-6.js
@@ -1,6 +1,6 @@
 export default {
-  label: 'Antonyms Collection - 6',
-  id: 'antonyms-6',
+  label: "Antonyms Collection - 6",
+  id: "antonyms-6",
   defs: {
     data: [
       `murky, luminous
@@ -76,249 +76,249 @@ dormant, awake`,
 conflict, peace
 jubilant, dejected
 impartial, partial
-amateur, professional`
-    ]
+amateur, professional`,
+    ],
   },
   list: [
     {
-      id: 'reading',
-      type: 'passage',
-      label: 'Words List',
+      id: "reading",
+      type: "passage",
+      label: "Words List",
       data: {
-        title: 'Antonyms',
+        title: "Antonyms",
         text: [
           //`Two words are said to be antonyms, if they have opposite meaning.`,
           {
-            type: 'hilight',
+            type: "hilight",
             text: `murky × luminous
 nurture × neglect
 astute × ignorant
 lavish × economical
 abrupt × gradual
-abet × discourage`
+abet × discourage`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `abolition × establishment
 feasible × impossible
 wary × rash
 recede × advance
 irate × content
-resolute × indecisive`
+resolute × indecisive`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `ajar × airtight
 dowdy × fashionable
 sullen × upbeat
 chide × compliment
 boisterous × quiet
-deteriorat e× upgrade`
+deteriorate× upgrade`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `drastic × mild
 consent × dissent
 primitive × soothe
 provoke × fickle
 propel × retreat
-desperate × hopeful`
+desperate × hopeful`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `hazardous × secure
 novice × expert
 callous × compassionate
 zealous × indifferent
 vestige × plethora
-industrious × idle`
+industrious × idle`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `perish × revive
 taunt × compliment
 summit × bottom
 athletic × frail
 prosper × oppose
-attic × cellar`
+attic × cellar`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `exclude × cherish
 forbid × permit
 elementary × advanced
 remain × vanish
 continue × interrupt
-wondrous × commonplace`
+wondrous × commonplace`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `pompous × humble
 motley × uniform
 flourish × dwindle
 tyranny × democracy
 bustle × inactivity
-adjunct × principal`
+adjunct × principal`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `dictate × request
 conceal × expose
 sterile × fruitful
 consecutive × interrupted
 courteous × rude
-monotonous × interesting`
+monotonous × interesting`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `momentous × unimportant
 flippant × respectful
 eternal × transient
 ecstasy × misery
-dormant × awake`
+dormant × awake`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `congested × clear
 conflict × peace
 jubilant × dejected
 impartial × partial
-amateur × professional`
-          }
-        ]
-      }
+amateur × professional`,
+          },
+        ],
+      },
     },
     {
-      type: 'match',
-      label: 'Match Antonyms',
-      id: 'match',
+      type: "match",
+      label: "Match Antonyms",
+      id: "match",
       commonData: {
-        title: 'Match Antonyms'
+        title: "Match Antonyms",
       },
       data: [
         {
-          refs: 'data~0'
+          refs: "data~0",
         },
         {
-          refs: 'data~1'
+          refs: "data~1",
         },
         {
-          refs: 'data~2'
+          refs: "data~2",
         },
         {
-          refs: 'data~3'
+          refs: "data~3",
         },
         {
-          refs: 'data~4'
+          refs: "data~4",
         },
         {
-          refs: 'data~5'
+          refs: "data~5",
         },
         {
-          refs: 'data~6'
+          refs: "data~6",
         },
         {
-          refs: 'data~7'
+          refs: "data~7",
         },
         {
-          refs: 'data~8'
+          refs: "data~8",
         },
         {
-          refs: 'data~9'
+          refs: "data~9",
         },
         {
-          refs: 'data~10'
-        }
-      ]
+          refs: "data~10",
+        },
+      ],
     },
     {
-      type: 'completeWord',
-      label: 'Write the Antonym',
-      id: 'complete-word',
+      type: "completeWord",
+      label: "Write the Antonym",
+      id: "complete-word",
       commonData: {
-        lang: 'en',
-        title: 'Type the antonym of the given word.'
+        lang: "en",
+        title: "Type the antonym of the given word.",
       },
       data: [
         {
-          refs: 'data~0'
+          refs: "data~0",
         },
         {
-          refs: 'data~1'
+          refs: "data~1",
         },
         {
-          refs: 'data~2'
+          refs: "data~2",
         },
         {
-          refs: 'data~3'
+          refs: "data~3",
         },
         {
-          refs: 'data~4'
+          refs: "data~4",
         },
         {
-          refs: 'data~5'
+          refs: "data~5",
         },
         {
-          refs: 'data~6'
+          refs: "data~6",
         },
         {
-          refs: 'data~7'
+          refs: "data~7",
         },
         {
-          refs: 'data~8'
+          refs: "data~8",
         },
         {
-          refs: 'data~9'
+          refs: "data~9",
         },
         {
-          refs: 'data~10'
-        }
-      ]
+          refs: "data~10",
+        },
+      ],
     },
     {
-      type: 'connectLetters',
-      label: 'Pick the word',
-      id: 'connect-letters',
+      type: "connectLetters",
+      label: "Pick the word",
+      id: "connect-letters",
       commonData: {
         title:
-          'Connect the letters from left to right to form the antonym for the below word.',
-        clueFont: 'big'
+          "Connect the letters from left to right to form the antonym for the below word.",
+        clueFont: "big",
       },
       data: [
         {
-          refs: 'data~0'
+          refs: "data~0",
         },
         {
-          refs: 'data~1'
+          refs: "data~1",
         },
         {
-          refs: 'data~2'
+          refs: "data~2",
         },
         {
-          refs: 'data~3'
+          refs: "data~3",
         },
         {
-          refs: 'data~4'
+          refs: "data~4",
         },
         {
-          refs: 'data~5'
+          refs: "data~5",
         },
         {
-          refs: 'data~6'
+          refs: "data~6",
         },
         {
-          refs: 'data~7'
+          refs: "data~7",
         },
         {
-          refs: 'data~8'
+          refs: "data~8",
         },
         {
-          refs: 'data~9'
+          refs: "data~9",
         },
         {
-          refs: 'data~10'
-        }
-      ]
-    }
-  ]
+          refs: "data~10",
+        },
+      ],
+    },
+  ],
 };
