7044c1295c03468972eca666037369f8c9d25ba1
priya2786 <priyahh123@gmail.com>
2023-03-13 16:19:34 +0530

    anotonyms updated

--- 6-8/vocabulary/antonyms-4.js
+++ 6-8/vocabulary/antonyms-4.js
@@ -1,6 +1,6 @@
 export default {
-  label: 'Antonyms Collection - 4',
-  id: 'antonyms-4',
+  label: "Antonyms Collection - 4",
+  id: "antonyms-4",
   defs: {
     data: [
       `fix, break
@@ -24,7 +24,7 @@ victory, failure
 trust, distrust
 poised, anxious`,
 
-      `corrupt, principled
+      `corrupt, honest
 deposit, withdraw
 massive, miniature
 constant, unstable
@@ -39,7 +39,7 @@ havoc, peace
 excavate, bury`,
 
       `zig-zag, straight
-accomplish, blunder
+accomplish, giveup 
 maximum, merest
 available, unavailable
 myth, truth
@@ -71,234 +71,274 @@ common, uncommon
 attack, defend
 ability, inability
 imperfect, perfect
-flaw, perfection`
-    ]
+flaw, perfection`,
+    ],
   },
   list: [
     {
-      id: 'reading',
-      type: 'passage',
-      label: 'Words List',
+      id: "reading",
+      type: "passage",
+      label: "Words List",
       data: {
-        title: 'Antonyms',
+        title: "Antonyms",
         text: [
           //`Two words are said to be antonyms, if they have opposite meaning.`,
           {
-            type: 'hilight',
+            type: "hilight",
             text: `fix × break
 resist × yield
 lead × follow
 arrest × release
 concentrate × daydream
-appropriate × improper`
+appropriate × improper`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `assist × prevent
 grim × pleasant
 support  ×  contradict
 steer × follow
 magnificient × ugly
 observe × glance
-scheduled × unplanned`
+scheduled × unplanned`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `contain × exclude
 cling × detach
 predator × prey
 victory × failure
 trust × distrust
-poised × anxious`
+poised × anxious`,
           },
           {
-            type: 'hilight',
-            text: `corrupt ×principled
+            type: "hilight",
+            text: `corrupt ×honest
 deposit × withdraw
 massive × miniature
 constant × unstable
 climax × letdown
-detach × link`
+detach × link`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `pedestrian × motorist
 eligible × disqualified
 debate × agree
 criticize × praise
 havoc × peace
-excavate × bury`
+excavate × bury`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `zig-zag × straight
-accomplish × blunder
+accomplish × giveup
 maximum × merest
 available × unavailable
 myth × truth
-soar × plummet`
+soar × plummet`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `multiply × lessen
 misunderstand × grasp
 manual × automated
 impressive × insignificant
 gigantic × tiny
-minute × immense`
+minute × immense`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `bravery × cowardice
 crooked × straight
 stain × purify
 ancestor × descendant
 exposure × cover,
-cruel × kind`
+cruel × kind`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `written × oral
 master × servant
 haste × delay
 vague × clear
 punish × reward
-arrival × departure`
+arrival × departure`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `attract × repel
 common × uncommon
 attack × defend
 ability × inability
 imperfect × perfect
-flaw × perfection`
-          }
-        ]
-      }
+flaw × perfection`,
+          },
+        ],
+      },
     },
     {
-      type: 'match',
-      label: 'Match Antonyms',
-      id: 'match',
+      type: "match",
+      label: "Match Antonyms",
+      id: "match",
       commonData: {
-        title: 'Match Antonyms'
+        title: "Match Antonyms",
       },
       data: [
         {
-          refs: 'data~0'
+          refs: "data~0",
         },
         {
-          refs: 'data~1'
+          refs: "data~1",
         },
         {
-          refs: 'data~2'
+          refs: "data~2",
         },
         {
-          refs: 'data~3'
+          refs: "data~3",
         },
         {
-          refs: 'data~4'
+          refs: "data~4",
         },
         {
-          refs: 'data~5'
+          refs: "data~5",
         },
         {
-          refs: 'data~6'
+          refs: "data~6",
         },
         {
-          refs: 'data~7'
+          refs: "data~7",
         },
         {
-          refs: 'data~8'
+          refs: "data~8",
         },
         {
-          refs: 'data~9'
-        }
-      ]
+          refs: "data~9",
+        },
+      ],
     },
     {
-      type: 'completeWord',
-      label: 'Write the Antonym',
-      id: 'complete-word',
+      type: "completeWord",
+      label: "Write the Antonym",
+      id: "complete-word",
       commonData: {
-        lang: 'en',
-        title: 'Type the antonym of the given word.'
+        lang: "en",
+        title: "Type the antonym of the given word.",
       },
       data: [
-        {
-          refs: 'data~0'
-        },
-        {
-          refs: 'data~1'
-        },
-        {
-          refs: 'data~2'
-        },
-        {
-          refs: 'data~3'
-        },
-        {
-          refs: 'data~4'
-        },
-        {
-          refs: 'data~5'
-        },
-        {
-          refs: 'data~6'
-        },
-        {
-          refs: 'data~7'
-        },
-        {
-          refs: 'data~8'
-        },
-        {
-          refs: 'data~9'
-        }
-      ]
+        `fix| break| _____
+        resist| yield| _____
+        lead| follow| ______
+        arrest| release| __le__e
+        concentrate| daydream| d__dr__m
+        appropriate| improper| i_p___er`,
+
+        `assist| prevent| p_____t
+        grim| pleasant| p__as__t
+        support| contradict| co__r__ict
+        magnificient| ugly| ____
+        observe| glance| g__nce
+        scheduled| unplanned| un__a__ed`,
+
+        `contain| exclude| e_c_u_e
+        cling| detach| d____h
+        predator| prey| ____
+        victory| failure| _______
+        trust| distrust| _i__r__t
+        poised| anxious| a__i__s`,
+
+        `corrupt| honest| h____t
+        deposit| withdraw| _i_h_r__
+        massive| miniature| m__i__u_e
+        constant| unstable| u_s__b_e
+        climax| letdown| l__d_wn
+        detach| link| ____`,
+
+        `pedestrian| motorist| m__o__s_
+        eligible| disqualified| d__q__l__i_d
+        debate| agree| ___ee
+        criticize| praise| p_a_s_
+        havoc| peace| p___e
+        excavate| bury| ____`,
+
+        `zig-zag| straight| s_r__g_t
+        accomplish| giveup | g__e__
+        maximum| merest| m___st
+        available| unavailable| __a__ila__e
+        myth| truth| _____
+        soar| plummet| p__m__t`,
+
+        `multiply| lessen| l_____
+        misunderstand| grasp| _____
+        manual| automated| au___a___
+        impressive| insignificant| _n_i__i_ic__t
+        gigantic| tiny| ____
+        minute| immense| i__e__e`,
+
+        `bravery| cowardice| c__a__i_e
+        crooked| straight| st__i__t
+        stain| purify| p_r__y
+        ancestor| descendant| d__cen__nt
+        exposure|  cover|  _____
+        cruel| kind| ____`,
+
+        `written| oral| ____
+        master| servant| s__v__t
+        haste| delay| _____
+        vague| clear| _____
+        punish| reward| r_____
+        arrival| departure| d___r__re`,
+
+        `attract| repel| _____
+        common| uncommon| _n___m_n
+        attack| defend| _____d
+        ability| inability| _n_b_l_t_
+        imperfect| perfect| p_rf__t
+        flaw| perfection| pe___c___n`,
+      ],
     },
+
     {
-      type: 'connectLetters',
-      label: 'Pick the word',
-      id: 'connect-letters',
+      type: "connectLetters",
+      label: "Pick the word",
+      id: "connect-letters",
       commonData: {
         title:
-          'Connect the letters from left to right to form the antonym for the below word.',
-        clueFont: 'big'
+          "Connect the letters from left to right to form the antonym for the below word.",
+        clueFont: "big",
       },
       data: [
         {
-          refs: 'data~0'
+          refs: "data~0",
         },
         {
-          refs: 'data~1'
+          refs: "data~1",
         },
         {
-          refs: 'data~2'
+          refs: "data~2",
         },
         {
-          refs: 'data~3'
+          refs: "data~3",
         },
         {
-          refs: 'data~4'
+          refs: "data~4",
         },
         {
-          refs: 'data~5'
+          refs: "data~5",
         },
         {
-          refs: 'data~6'
+          refs: "data~6",
         },
         {
-          refs: 'data~7'
+          refs: "data~7",
         },
         {
-          refs: 'data~8'
+          refs: "data~8",
         },
         {
-          refs: 'data~9'
-        }
-      ]
-    }
-  ]
+          refs: "data~9",
+        },
+      ],
+    },
+  ],
 };
--- 6-8/vocabulary/antonyms-5.js
+++ 6-8/vocabulary/antonyms-5.js
@@ -1,6 +1,6 @@
 export default {
-  label: 'Antonyms Collection - 5',
-  id: 'antonyms-5',
+  label: "Antonyms Collection - 5",
+  id: "antonyms-5",
   defs: {
     data: [
       `bicker, concur
@@ -46,16 +46,16 @@ yoke, release
 wilt, revive`,
 
       `vicious, cordial
-keen, insipid
+keen, reluctant
 subsequent, former
-justify, invalidate
-punishment, bounty
-amnesty, blame`,
+justify, deny
+punishment, reward
+amnesty, punishment`,
 
       `anxiety, relief
 radical, conservative
-charity, theft
-abnormal, customary
+charity, unkindness
+abnormal, regular
 famous, anonymous
 diligent, lazy`,
 
@@ -64,240 +64,279 @@ preference, dislike
 furious, calm
 moderate, extreme
 mischievous, well-behaved
-militant, peaceful`,
+militant, pacifist`,
 
       `migrant, immovable
 coarse, fine
 foe, friend
 immaculate, dirty
 gratitude, ungratefulness
-gaunt, plump`
-    ]
+gaunt, plump`,
+    ],
   },
   list: [
     {
-      id: 'reading',
-      type: 'passage',
-      label: 'Words List',
+      id: "reading",
+      type: "passage",
+      label: "Words List",
       data: {
-        title: 'Antonyms',
+        title: "Antonyms",
         text: [
           //`Two words are said to be antonyms, if they have opposite meaning.`,
           {
-            type: 'hilight',
+            type: "hilight",
             text: `bicker× concur
 distress× pleasure
 captivity× freedom
 limp× stiff
 substantial× insignificant
-quench× parch`
+quench× parch`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `fiasco× triumph
 utmost× minimal
 infamous× glorious
 inevitable× avoidable
 onset× conclusion
-concise× wordy`
+concise× wordy`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `arrogant× modest
 offbeat× regular
 terminate× initiate
 generate× demolish
 conserve× squander
-crude× refined`
+crude× refined`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `dominate× submit
 efficient× unproductive
 solitary× together
 fatigue× liveliness
 exhibit× conceal
-squash× expand`
+squash× expand`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `agony× comfort
 myriad× scarcity
 gratify× disappoint
 cultivate× neglect
 admire× detest
-persist× quit`
+persist× quit`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `absurd× sensible
 dainty× clumsy
 economise× waste
 frosty× sweltering
 yoke× release
-wilt× revive`
+wilt× revive`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `vicious× cordial
-keen× insipid
+keen× reluctant
 subsequent× former
-justify× invalidate
-punishment× bounty
-amnesty× blame`
+justify× deny
+punishment× reward
+amnesty× punishment`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `anxiety× relief
 radical× conservative
-charity× theft
-abnormal× customary
+charity× unkindness
+abnormal× regular
 famous× anonymous
-diligent× lazy`
+diligent× lazy`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `fantastic× sober
 preference× dislike
 furious× calm
 moderate× extreme
 mischievous× well-behaved
-militant× peaceful`
+militant× pacifist`,
           },
           {
-            type: 'hilight',
+            type: "hilight",
             text: `migrant× immovable
 coarse× fine
 foe× friend
 immaculate× dirty
 gratitude× ungratefulness
-gaunt× plump`
-          }
-        ]
-      }
+gaunt× plump`,
+          },
+        ],
+      },
     },
     {
-      type: 'match',
-      label: 'Match Antonyms',
-      id: 'match',
+      type: "match",
+      label: "Match Antonyms",
+      id: "match",
       commonData: {
-        title: 'Match Antonyms'
+        title: "Match Antonyms",
       },
       data: [
         {
-          refs: 'data~0'
+          refs: "data~0",
         },
         {
-          refs: 'data~1'
+          refs: "data~1",
         },
         {
-          refs: 'data~2'
+          refs: "data~2",
         },
         {
-          refs: 'data~3'
+          refs: "data~3",
         },
         {
-          refs: 'data~4'
+          refs: "data~4",
         },
         {
-          refs: 'data~5'
+          refs: "data~5",
         },
         {
-          refs: 'data~6'
+          refs: "data~6",
         },
         {
-          refs: 'data~7'
+          refs: "data~7",
         },
         {
-          refs: 'data~8'
+          refs: "data~8",
         },
         {
-          refs: 'data~9'
-        }
-      ]
+          refs: "data~9",
+        },
+      ],
     },
     {
-      type: 'completeWord',
-      label: 'Write the Antonym',
-      id: 'complete-word',
+      type: "completeWord",
+      label: "Write the Antonym",
+      id: "complete-word",
       commonData: {
-        lang: 'en',
-        title: 'Type the antonym of the given word.'
+        lang: "en",
+        title: "Type the antonym of the given word.",
       },
       data: [
-        {
-          refs: 'data~0'
-        },
-        {
-          refs: 'data~1'
-        },
-        {
-          refs: 'data~2'
-        },
-        {
-          refs: 'data~3'
-        },
-        {
-          refs: 'data~4'
-        },
-        {
-          refs: 'data~5'
-        },
-        {
-          refs: 'data~6'
-        },
-        {
-          refs: 'data~7'
-        },
-        {
-          refs: 'data~8'
-        },
-        {
-          refs: 'data~9'
-        }
-      ]
+        `bicker| concur| co___r
+      distress| pleasure| pl__s___
+      captivity| freedom| f__e__m
+      limp| stiff| st___
+      substantial| insignificant| i___g____cant
+      quench| parch| _____`,
+
+        `fiasco| triumph| t__u__h
+      utmost| minimal| m_n_m_l
+      infamous| glorious| gl__i__s
+      inevitable| avoidable| a___d___e
+      onset| conclusion| co___u___n
+      concise| wordy| w___y`,
+
+        `arrogant| modest| m__e__
+      offbeat| regular| r__u___
+      terminate| initiate| i___i__e
+      generate| demolish| de__l__h
+      conserve| squander| s__a___r
+      crude| refined| r___n__`,
+
+        `dominate| submit| _____t
+      efficient| unproductive| un___d___i__
+      solitary| together| t__e___r
+      fatigue| liveliness| l__el__e__
+      exhibit| conceal| c__c__l
+      squash| expand| __p___`,
+
+        `agony| comfort| c__f___
+      myriad| scarcity| _c__c__y
+      gratify| disappoint| d_s___o__t
+      cultivate| neglect| n_g_e_t
+      admire| detest| d__e__
+      persist| quit| ____`,
+
+        `absurd| sensible| sensible
+      dainty| clumsy| __u__y
+      economise| waste| wa___
+      frosty| sweltering| _w___e__ng
+      yoke| release| r__e__e
+      wilt| revive| _e_iv_`,
+
+        `vicious| cordial| ___d_a_
+      keen| reluctant| _el__t__t
+      subsequent| former| _o_m__
+      justify| deny| ____
+      punishment| reward| r_w__d
+      amnesty| punishment| p___s___nt`,
+
+        `anxiety| relief| r_l__f
+      radical| conservative| c__s___a___e
+      charity| unkindness| u___n___s_
+      abnormal| regular| r__u___
+      famous| anonymous| a___y__us
+      diligent| lazy| ____`,
+
+        `fantastic| sober| _____
+      preference| dislike| d__l__e
+      furious| calm| ____
+      moderate| extreme| ex___m_
+      mischievous| well-behaved| w__l-b__a___
+      militant| pacifist| pa__f__t`,
+
+        `migrant| immovable| i__o___l_
+      coarse| fine| ____
+      foe| friend| ______
+      immaculate| dirty| _____
+      gratitude| ungratefulness| u_g__te___n__s
+      gaunt| plump| p___p`,
+      ],
     },
     {
-      type: 'connectLetters',
-      label: 'Pick the word',
-      id: 'connect-letters',
+      type: "connectLetters",
+      label: "Pick the word",
+      id: "connect-letters",
       commonData: {
         title:
-          'Connect the letters from left to right to form the antonym for the below word.',
-        clueFont: 'big'
+          "Connect the letters from left to right to form the antonym for the below word.",
+        clueFont: "big",
       },
       data: [
         {
-          refs: 'data~0'
+          refs: "data~0",
         },
         {
-          refs: 'data~1'
+          refs: "data~1",
         },
         {
-          refs: 'data~2'
+          refs: "data~2",
         },
         {
-          refs: 'data~3'
+          refs: "data~3",
         },
         {
-          refs: 'data~4'
+          refs: "data~4",
         },
         {
-          refs: 'data~5'
+          refs: "data~5",
         },
         {
-          refs: 'data~6'
+          refs: "data~6",
         },
         {
-          refs: 'data~7'
+          refs: "data~7",
         },
         {
-          refs: 'data~8'
+          refs: "data~8",
         },
         {
-          refs: 'data~9'
-        }
-      ]
-    }
-  ]
+          refs: "data~9",
+        },
+      ],
+    },
+  ],
 };
