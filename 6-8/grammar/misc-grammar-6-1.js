54356d476cfa9881b07f39af6c6270defe0af327
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-03-14 08:52:22 +0530

    Modified misc-grammar-6-1.js

--- 6-8/grammar/misc-grammar-6-1.js
+++ 6-8/grammar/misc-grammar-6-1.js
@@ -363,6 +363,346 @@ cat, subject`,
               name: "Adverb",
               text: `He ran *fast* and caught the train.`,
             },
+            {
+              name: "Noun",
+              text: ` I paid my *fine*.`,
+            },
+            {
+              name: "Verb",
+              text: `He is *fined* for his absence.`,
+            },
+            {
+              name: "Adjective",
+              text: `What a *fine* weather it is!`,
+            },
+            {
+              name: "Adverb",
+              text: `Please tune *fine*.`,
+            },
+            {
+              name: "Noun",
+              text: `There is no *fire* in the hearth.`,
+            },
+            {
+              name: "Verb",
+              text: `The gun was *fired*.`,
+            },
+            {
+              name: "Adjective",
+              text: `Did you enjoy the *fire*-works last night.`,
+            },
+            {
+              name: "Verb",
+              text: `At last he *freed* himself from the cruel moneylender.`,
+            },
+            {
+              name: "Adjective",
+              text: `I have a *free* pass for the show.`,
+            },
+            {
+              name: "Adverb",
+              text: `The bird was set *free*.`,
+            },
+            {
+              name: "Noun",
+              text: `Whatever God does, He does for our *good*.`,
+            },
+            {
+              name: "Adjective",
+              text: `He is a *good* boy.`,
+            },
+            {
+              name: "Adverb",
+              text: `The boy is doing *good*.`,
+            },
+            {
+              name: "Noun",
+              text: `Never do things by *halves*.`,
+            },
+            {
+              name: "Adjective",
+              text: `*Half* a loaf is better than no bread.`,
+            },
+            {
+              name: "Adverb",
+              text: `Well begun is *half* done.`,
+            },
+            {
+              name: "Noun",
+              text: `I have no *house* to live in.`,
+            },
+            {
+              name: "Verb",
+              text: `The library was *housed* in an old building.`,
+            },
+            {
+              name: "Adjective",
+              text: `The library was *housed* in an old building.`,
+            },
+            {
+              name: "Adjective",
+              text: `Mr. Ravi is a *house*-surgeon these days.`,
+            },
+            {
+              name: "Noun",
+              text: `*Iron* is a useful metal.`,
+            },
+            {
+              name: "Verb",
+              text: `*Iron* your clothes.`,
+            },
+            {
+              name: "Adjective",
+              text: `Sardar Patel was an *iron* man.`,
+            },
+            {
+              name: "Noun",
+              text: `The patient is sure to breathe his *last*.`,
+            },
+            {
+              name: "Verb",
+              text: `The meeting will *last* for two hours.`,
+            },
+            {
+              name: "Adjective",
+              text: `This bank is on its *last* leg.`,
+            },
+            {
+              name: "Adverb",
+              text: `He came *last* of all.`,
+            },
+            {
+              name: "Noun",
+              text: `There is no *light* in the hall.`,
+            },
+            {
+              name: "Verb",
+              text: `Please *light* the lamp.`,
+            },
+            {
+              name: "Adjective",
+              text: `Give only *light* food to the patient.`,
+            },
+            {
+              name: "Adverb",
+              text: `*Light* come, *light* go.`,
+            },
+            {
+              name: "Noun",
+              text: `I do not know your *likes* and dislikes.`,
+            },
+            {
+              name: "Verb",
+              text: `I *like* this apple.`,
+            },
+            {
+              name: "Adjective",
+              text: `*Like* father, *like* son.`,
+            },
+            {
+              name: "Preposition",
+              text: `He works *like* a bee.`,
+            },
+            {
+              name: "Noun",
+              text: `What is the *long* and short of this matter?`,
+            },
+            {
+              name: "Verb",
+              text: `I *long* to have a watch.`,
+            },
+            {
+              name: "Adjective",
+              text: `It is a *long* story.`,
+            },
+            {
+              name: "Adverb",
+              text: `May you live *long*.`,
+            },
+            {
+              name: "Noun",
+              text: `Our *master* is absent today.`,
+            },
+            {
+              name: "Verb",
+              text: `I have *mastered* the situation of the city.`,
+            },
+            {
+              name: "Adjective",
+              text: `Here is a *master*-key.`,
+            },
+            {
+              name: "Noun",
+              text: `*Much* has already been done in this respect.`,
+            },
+            {
+              name: "Pronoun",
+              text: `*Much* of the work has already been done.`,
+            },
+            {
+              name: "Adjective",
+              text: `He has done *much* work today.`,
+            },
+            {
+              name: "Adverb",
+              text: `He has worked *much* today.`,
+            },
+            {
+              name: "Noun",
+              text: `What is the *mean* of ten and sixteen.`,
+            },
+            {
+              name: "Verb",
+              text: `What do you *mean* to say?`,
+            },
+            {
+              name: "Adjective",
+              text: `Is he not a *mean* fellow?`,
+            },
+            {
+              name: "Noun",
+              text: `There is a gold *mine* in Mysore.`,
+            },
+            {
+              name: "Pronoun",
+              text: `*Mine* is not a bad picture.`,
+            },
+            {
+              name: "Verb",
+              text: `Diamonds are *mined* in South Africa.`,
+            },
+            {
+              name: "Verb",
+              text: `The tribesmen are *nearing* the city.`,
+            },
+            {
+              name: "Adjective",
+              text: `He is my *near* relative.`,
+            },
+            {
+              name: "Adverb",
+              text: `Come *near*, please.`,
+            },
+            {
+              name: "Preposition",
+              text: `He lives *near* our house.`,
+            },
+            {
+              name: "Noun",
+              text: `My *need* is greater than yours.`,
+            },
+            {
+              name: "Verb",
+              text: `He *needs* a pen.`,
+            },
+            {
+              name: "Adjective",
+              text: `This is the *only* washerman here.`,
+            },
+            {
+              name: "Adverb",
+              text: `He *only* sleeps and does nothing.`,
+            },
+            {
+              name: "Conjunction",
+              text: `You may do whatever you like, *only* do not come near me.`,
+            },
+            {
+              name: "Noun",
+              text: `I do not know the ins and *outs* of this matter.`,
+            },
+            {
+              name: "Adjective",
+              text: `I have booked my luggage at the Railway *out* agency.`,
+            },
+            {
+              name: "Adverb",
+              text: `Come *out*, please.`,
+            },
+            {
+              name: "Preposition",
+              text: `*Out* of the frying pan, into the fire.`,
+            },
+            {
+              name: "Noun",
+              text: `The *past* of our country was very very glorious.`,
+            },
+            {
+              name: "Adjective",
+              text: `Do you know his *past* history?`,
+            },
+            {
+              name: "Adverb",
+              text: `The soldiers marched *past*.`,
+            },
+            {
+              name: "Preposition",
+              text: `The old man is *past* eighty.`,
+            },
+            {
+              name: "Noun",
+              text: `The *pen* is mightier than the sword.`,
+            },
+            {
+              name: "Verb",
+              text: ` I have *penned* a few line in reply.`,
+            },
+            {
+              name: "Adjectives",
+              text: `I have some *pen* friends in China.`,
+            },
+            {
+              name: "Noun",
+              text: `He has every *right* to do so.`,
+            },
+            {
+              name: "Verb",
+              text: `Every wrong should be *righted*.`,
+            },
+            {
+              name: "Adjective",
+              text: `This is my *right* arm.`,
+            },
+            {
+              name: "Adverb",
+              text: `He was *right* glad to see me.`,
+            },
+            {
+              name: "Noun",
+              text: `The man-made moon has already made more than one hundred *rounds* of the earth.`,
+            },
+            {
+              name: "Verb",
+              text: `All the thieves have been *rounded* up by the police.`,
+            },
+            {
+              name: "Adjective",
+              text: `We sat at a *round* table.`,
+            },
+            {
+              name: "Adverb",
+              text: `With great difficulty I brought him *round* to my views.`,
+            },
+            {
+              name: "Preposition",
+              text: `The man-made moon went *round* the earth.`,
+            },
+            {
+              name: "Noun",
+              text: `Put the *saddle* on the back of the horse.`,
+            },
+            {
+              name: "Verb",
+              text: `I am *saddled* with great responsibility.`,
+            },
+            {
+              name: "Verb",
+              text: `He has *saved* a lot of money.`,
+            },
+            {
+              name: "Preposition",
+              text: `All *save* Ravi were present.`,
+            },
           ],
         },
       ],
