dfafaf6ae659ded4f351d24c7d12ceb8864b5037
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-04-20 09:56:22 +0530

    added activities article-7.js

--- 6-8/grammar/article-7.js
+++ 6-8/grammar/article-7.js
@@ -148,9 +148,9 @@ export default {
           {
             qText: `When is 'the' used?`,
             options: `Before the name of rivers, seas, canals, oceans, valleys, deserts, forests
-            Before the name of a single mountain or hill
-            Before the name of a musical instrument
-            Before a proper noun, common noun, abstract noun or material noun to make them common nouns`
+Before the name of a single mountain or hill
+Before the name of a musical instrument
+Before a proper noun, common noun, abstract noun or material noun to make them common nouns`
           },
           {
             qText: `When do we use 'an' instead of 'a'?`,
@@ -162,6 +162,104 @@ export default {
           }
         ]
       }
+    },
+    {
+      label: 'True or False',
+      type: 'classifySentence',
+      id: 'true-false',
+      commonData: {
+        title: 'Classify the following sentences as True or False',
+        types: ['True', 'False']
+      },
+      data: [
+        [
+          `"An" is used before words that start with a vowel sound.
+"A" is used before words that start with a consonant sound.
+"An" can be used before words that start with a silent "h". `,
+
+          `"The" is an indefinite article.
+"A" can be used before words that start with a silent "h". `
+        ],
+        [
+          `"The" can be used with both countable and uncountable nouns. `,
+
+          `"The" can only be used with singular nouns. 
+"An" can only be used with plural nouns.
+"A" can only be used with countable nouns.
+"A" can be used with both countable and uncountable nouns.`
+        ],
+        [
+          `"The" is always used before a specific noun.
+"The" can be used with abstract nouns.`,
+
+          `"An" is always used before a vowel.
+"A" and "an" are interchangeable. 
+"A" and "an" are both definite articles.`
+        ],
+        [
+          `"The" can be used with superlative adjectives.
+"A" and "an" are both indefinite articles.
+"The" can be used before a proper noun.
+"The" can be used before a possessive pronoun.`,
+
+          `"A" can be used before an adjective.`
+        ],
+        [
+          `"The" can be used before a singular noun that represents a whole class of things.
+"An" can be used before an acronym.`,
+
+          `"An" can be used before an uncountable noun.
+"A" can be used before an uncountable noun. 
+"A" can be used before a singular noun that represents a whole class of things.`
+        ],
+        [
+          `"A" can be used before a proper noun.`,
+
+          `"The" is always used before a plural noun.
+"The" is always used before a common noun.
+"The" is always used before an adjective.
+"An" can be used before a noun that starts with a consonant sound.`
+        ]
+      ]
+    },
+    {
+      id: 'fill-up-art',
+      label: 'Articles',
+      type: 'fillup',
+      commonData: {
+        title: `Fill in the blanks with suitable articles.`
+      },
+      data: [
+        `*The (an)* dog barked at *the (x)* mailman.
+*A (x)* cat jumped onto *the (an)* windowsill.
+*An (A)* apple *a (the)* day keeps *the (a)* doctor away.
+*The (x)* car honked loudly in traffic.
+*A (x)* bird flew into *the (an)* tree.`,
+
+        `*The (A)* ocean is vast and mysterious.
+*A (An)* spider crawled across the ceiling.
+The book on *the (x)* shelf caught my eye.
+*An (a)* umbrella is useful on *a (the)* rainy day.
+*The (A)* sun rose over the mountains.`,
+
+        `A guitar player strummed *a (x)* melody.
+The airplane flew *x (the)* overhead.
+*An (A)* elephant trumpeted in the distance.
+The waiter brought us *x (the)* menus to peruse.
+A basketball bounced on *the (a)* court.`,
+
+        `The lion roared in *the (x)* zoo enclosure.
+An artist painted *a (the)* beautiful landscape.
+*The (a)* coffee shop was bustling with activity.
+*A (an)* frog hopped into the pond.
+*The (a)* newspaper reported on the latest news.`,
+
+        `*An (a)* actor was memorizing their lines for the play.
+The chef cooked *a (x)* delicious meal.
+A scientist conducted *an (the)* experiment.
+The flower bloomed in *the (a)* garden.
+*An (A)* athlete was training hard for the competition.`
+      ]
     },
     {
       id: 'a-an-the',
@@ -221,6 +319,45 @@ Can you pass me *the* salt?`
         `*x* Examinations are always fearful. They are *a* great cause of fear, anxiety, tension and worry. *The* night before *the* examination is really painful. *The* students have no sleep. They stop eating *x* food. They want to read and revise everything. They are disturbed if someone tells them *an* important question. They at once study for its answer. They are irritated if someone disturbs them. They pass *the* whole night in tension. They do not take anything in *the* morning. They go to *the* examination hall on *x* empty stomachs.`,
 
         `*The* scene at *a* Railway Platform is always enjoyable. It presents *a* picture of *x* mini India. Last Sunday I went to *the* city Railway station to see my uncle off. *The* platform was overcrowded. There were many stalls on *the* platform. Small children were running here and there. Coolies in *x* red dresses were going to and fro. Passengers were taking *x* tea. Some people were reading *x* newspapers. They were waiting for *the* train. Soon *the* train came. My uncle got *a* seat and *the* train started. I returned home.`
+      ]
+    },
+    {
+      id: 'complete-conv',
+      label: 'Complete the Conversation ',
+      type: 'fillup',
+      commonData: {
+        noOptions: true,
+        title:
+          'Complete the conversations using articles. Mark `x` wherever articles are not used.'
+      },
+      lockAfter: 1,
+      data: [
+        `Jay: Hey, did you see *the* news article about *the* new restaurant that opened downtown?
+Jake: No, I haven't seen it yet. Is it *a* fancy restaurant?`,
+
+        `Jay: Yeah, it looks pretty upscale. They have *a* menu with *a* lot of unique dishes, like *an* avocado and crab salad.
+Jake: Hmm, that sounds interesting. Do they have *a* website with *the* menu?`,
+
+        `Jay: Yeah, I think they do. Let me check on my *x* phone.
+Jake: Thanks. I want to see if they have *a* vegetarian option. I don't eat meat.`,
+
+        `Jay: Oh, I see. Yeah, they have *a* few vegetarian options. They have *a* roasted vegetable plate and *a* spinach salad.
+Jake: Perfect! Let's plan to go there for *x* lunch tomorrow.`,
+
+        `Jay: Sounds good to me. Should we make *a* reservation?
+Jake: Yeah, we should definitely make *a* reservation to make sure we get *a* table.`,
+
+        `Amit: Hey, did you see *the* article in *the* newspaper today?
+Rahul: No, I haven't had *a* chance to read it yet. What was it about?`,
+
+        `Amit: It was *an* opinion piece about *the* upcoming election.
+Rahul: Oh, interesting. I'll have to take *a* look. Did they mention any specific candidates?`,
+
+        `Amit: Yeah, they talked about *the* frontrunner and *the* issues they're focusing on.
+Rahul: Hmm, I wonder if they mentioned *the* controversy surrounding *the* candidate's past statements.`,
+
+        `Amit: They did touch on it briefly, but mostly focused on *the* candidate's current platform.
+Rahul: Okay, got it. Thanks for *the* summary. I'll definitely check it out later.`
       ]
     },
     {
