81dd0ea2df3f05a54e1b16a7d6d7cd632eb725c8
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-04-24 11:55:23 +0530

    updated  5-letter-words.js, 6-letter-words.js

--- 3-5/vocabulary/6-letter-words.js
+++ 3-5/vocabulary/6-letter-words.js
@@ -9,7 +9,7 @@ export default {
       data: {
         title: 'Words List',
         text: [
-          `Find below some basic six letter words. Get familiar with them.`,
+          `Find below some basic six-letter words. Get familiar with them.`,
           {
             type: 'sitewords',
             text: `active, afraid, amount, animal, answer, anyone, appear, attack, attend, author, battle, become, bottle, bottom, branch, bright, burden, camera, charge, circle, coffee, course, damage, danger, degree, demand, device, direct, driver, eleven, energy, enough, export, family, famous, female, follow, forget, future, health, lesson, listen, manner, market, matter, member, memory, middle, minute, mirror, mother, museum, myself, normal, number, office, online, option, output, please, prison, recent, remain, repair, report, result, reward, salary, school, screen, season, second, select, silver, simple, single, speech, street, strong, summer, system, talent, twenty, update, useful, volume, weight, window, winner, wonder`,
@@ -23,18 +23,18 @@ export default {
       type: 'sequence',
       label: 'Form Words',
       commonData: {
-        title: 'Connect the blocks to form meaningful word.'
+        title: 'Connect the blocks to form a meaningful word.'
       },
       data: [
-        'active, forget, silver, degree, remain, bottle, minute, update, middle, twenty',
-        'afraid, future, single, device, repair, branch, mother, volume, recent, author',
-        'animal, health, speech, direct, report, bright, myself, window, select, damage',
-        'anyone, lesson, street, driver, result, burden, normal, member, talent, follow',
-        'appear, listen, system, eleven, reward, camera, number, course, prison, attack',
+        'active, forget, silver | livers, degree, remain | marine | airmen, bottle, minute, update, middle, twenty',
+        'afraid, future, single, device, repair, branch, mother, volume, recent | centre | center, author',
+        'animal | lamina | manila, health, speech, direct | credit, report | porter, bright, myself, window, select, damage',
+        'anyone, lesson, street | tester, driver, result | rustle | luster, burden | burned, normal, member, talent | latent, follow',
+        'appear | rapper, listen | enlist | silent | tinsel, system, eleven, reward | drawer, camera, number, course, prison, attack',
         'attend, manner, useful, energy, salary, charge, office, summer, family, season',
-        'battle, memory, weight, enough, screen, coffee, option, online, answer, matter',
-        'become, mirror, winner, famous, second, danger, output, export, school, circle',
-        'bottom, museum, wonder, female, simple, demand, please, amount, market, strong'
+        'battle | tablet, memory, weight, enough, screen, coffee, option, online, answer, matter',
+        'become, mirror, winner, famous, second, danger, output, export, school, circle | cleric',
+        'bottom, museum, wonder, female, simple, demand, please | asleep, amount, market, strong'
       ]
     },
     {
@@ -44,7 +44,7 @@ export default {
       data: {
         title: 'Words List',
         text: [
-          `Find below more six letter words. Get familiar with them.`,
+          `Find below more six-letter words. Get familiar with them.`,
           {
             type: 'sitewords',
             text: `accept, action, advice, always, august, beauty, before, better, border, breath, bridge, button, cannot, center, change, choice, common, corner, create, decide, define, depend, design, detail, dinner, doctor, eighth, engine, escape, expand, expert, finger, finish, forest, friend, garden, global, ground, height, honest, income, inside, leader, length, letter, liquid, little, manage, margin, master, mobile, narrow, nation, nature, object, orange, parent, people, person, planet, plenty, police, profit, proper, public, reader, reason, record, remote, remove, repeat, replay, sample, search, secret, silent, sister, social, source, square, status, thirty, travel, trying, unique, wealth, winter, worker, writer, yellow`,
@@ -58,18 +58,18 @@ export default {
       type: 'sequence',
       label: 'Form Words - 2',
       commonData: {
-        title: 'Connect the blocks to form meaningful word.'
+        title: 'Connect the blocks to form a meaningful word.'
       },
       data: [
-        'breath, narrow, writer, finger, sister, detail, police, replay, change, nation',
-        'choice, nature, accept, garden, social, doctor, profit, margin, square, expand',
-        'common, object, always, ground, source, eighth, public, center, mobile, advice',
-        'corner, orange, august, height, status, expert, reason, silent, escape, remove',
-        'create, parent, beauty, honest, thirty, finish, record, master, action, manage',
-        'decide, people, before, income, travel, forest, remote, engine, reader, cannot',
-        'define, person, better, inside, wealth, friend, repeat, yellow, liquid, secret',
-        'depend, planet, border, leader, winter, global, sample, proper, button, little',
-        'design, plenty, bridge, letter, worker, length, search, dinner, unique, trying'
+        'breath, narrow, writer, finger | fringe, sister | resist, detail | dilate | tailed, police, replay, change, nation',
+        'choice, nature, accept, garden | danger | gander | ranged, social, doctor, profit, margin | arming, square, expand',
+        'common, object, always, ground, source | course, eighth | height, public, center | centre, mobile, advice',
+        'corner, orange, august, height | eighth, status, expert, reason, silent | enlist | listen | tinsel, escape, remove',
+        'create, parent | entrap, beauty, honest, thirty, finish, record, master | stream, action, manage',
+        'decide, people, before, income, travel, forest | softer | foster, remote, engine, reader | dearer, cannot',
+        'define, person, better, inside, wealth, friend | finder | refind, repeat, yellow, liquid, secret',
+        'depend, planet, border, leader | dealer, winter, global, sample, proper, button, little',
+        'design | signed, plenty, bridge, letter, worker | rework, length, search | chaser | arches, dinner, unique, trying'
       ]
     }
   ]
