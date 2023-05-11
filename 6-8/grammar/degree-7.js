cf5eccb47bc8967e4db0287303454a686877991c
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-05-11 12:11:03 +0530

    updated degree-7.js, word-formation-7.js, adjective-7.js

---
+++ 6-8/grammar/degree-7.js
@@ -0,0 +1,331 @@
+export default {
+  id: 'degree-7',
+  label: 'Degrees of Comparrison',
+  lockAfter: 3,
+  list: [
+    {
+      label: 'Notes',
+      type: 'passage',
+      id: 'degree',
+      data: {
+        title: 'Affixation and Degrees of Comparrison',
+        text: [
+          `An affix is a word element of English grammar used to alter the meaning or form of a word and comes in the form of either a prefix or a suffix. 
+# Affixation in Comparative and Superlative Degree
+1. Most comparative and superlative degrees are made by adding -er and -est. If the adjective ends in -e, only then -r and -st are added.`,
+          {
+            type: 'html',
+            text: `Eg: sweet -> sweet<b>er</b> -> sweet<b>est</b><br> 
+safe -> saf<b>er</b> -> saf<b>est</b><br>
+dark -> dark<b>er</b> -> dark<b>est</b>`
+          },
+          `2. Adjectives ending in y, drop the -y and take -ier and -iest.`,
+          {
+            type: 'html',
+            text: `Eg: crazy -> craz<b>ier</b> -> craz<b>iest</b><br>
+early  -> earl<b>ier</b> -> earl<b>iest</b><br>
+happy -> happ<b>ier</b> -> happ<b>iest</b>`
+          },
+          `3. In some adjectives that have just one syllable, the last consonant is doubled.`,
+          {
+            type: 'html',
+            text: `Eg: thin —> thi<b>nner</b> —> thi<b>nnest</b><br>
+sad —> sa<b>dder</b> -> sa<b>ddest</b><br>
+dim -> di<b>mmer</b> -> di<b>mmest</b>`
+          },
+          `4. Adjectives that have more than two or three syllables take more and most to make comparative and superlative degrees. These include adjectives that end in -ed, -ful, -less, -ing, etc.`,
+          {
+            type: 'html',
+            text: `Eg: excited -> <b>more</b> excited —> <b>most</b> excited<br>
+useful —> <b>more</b> useful -> <b>most</b> useful`
+          }
+        ]
+      }
+    },
+    {
+      label: 'Multiple Choice Questions',
+      type: 'mcq',
+      id: 'mcq-affix',
+      data: {
+        title: 'Multiple Choice Questions',
+        questions: [
+          {
+            qText: `What is an affix in English grammar?`,
+            options: `A word element used to alter the meaning or form of a word, A grammatical construction that changes the tense of a verb, A type of adverb used to modify a verb`
+          },
+          {
+            qText: `What are the two forms of affixes in English?`,
+            options: `Prefixes and suffixes, Adjectives and adverbs, Nouns and verbs, Conjunctions and prepositions`
+          },
+          {
+            qText: `How are most comparative and superlative degrees formed?`,
+            options: `By adding -er and -est, By adding -ed and -ing, By adding -ly, By adding -able and -ible`
+          },
+          {
+            qText: `What happens to adjectives ending in -y when forming comparative and superlative degrees?`,
+            options: `The -y is dropped and -er/-est is added, The -y is doubled, The -y is changed to -ly`
+          },
+          {
+            qText: `What happens to some adjectives with just one syllable when forming comparative and superlative degrees?`,
+            options: `The last consonant is doubled, The last vowel is doubled, The adjective is shortened, The adjective is lengthened`
+          }
+        ]
+      }
+    },
+    {
+      id: 'comparisson-2',
+      label: 'Fillup',
+      type: 'fillup',
+      commonData: {
+        noOptions: true,
+        title:
+          'Fill in the missing blanks with the most appropriate degree of comparison.'
+      },
+      lockAfter: 2,
+      data: [
+        `Big -> *Bigger* -> Biggest
+Thin -> Thinner -> *Thinnest*
+Fat -> *Fatter* -> Fattest
+Dim -> Dimmer -> *Dimmest*
+Hot -> *Hotter* -> Hottest`,
+
+        `Sad -> Sadder -> *Saddest*
+Slim -> *Slimmer* -> Slimmest
+Red -> Redder -> *Reddest*
+Bright -> *Brighter* -> Brightest
+Short -> Shorter -> *Shortest*
+Weak -> *Weaker* -> Weakest`,
+
+        `Long -> Longer -> *Longest*
+Smart -> *Smarter* -> Smartest
+Cool -> Cooler -> *Coolest*
+Dark -> *Darker* -> Darkest
+Small -> Smaller -> *Smallest*`,
+
+        `Bold -> *Bolder* -> Boldest
+Humble -> Humbler -> *Humblest*
+Simple -> *Simpler* -> Simplest
+Fine -> Finer -> *Finest*
+Noble -> *Nobler* -> Noblest`,
+
+        `Brave -> Braver -> *Bravest*
+Wealthy -> *Wealthier* -> Wealthiest
+*Healthy* -> Healthier -> Healthiest
+Costly -> *Costlier* -> Costliest`,
+
+        `Busy -> Busier -> *Busiest*
+*Cosy* -> Cosier -> Cosiest
+Little -> *Less* -> *Least*
+Many -> *More* -> Most`
+      ]
+    },
+    {
+      id: 'fill-up-comparisson',
+      label: 'Fill in the blanks',
+      type: 'fillup',
+      commonData: {
+        title: `Fill in the blanks with the most appropriate degree of comparison.`
+      },
+      data: [
+        `Athiya is *smarter (smartest)* than most of her friends.
+Nobody is as *good (best)* as you.
+Priya was the *most elegant (more elegant)* of all.
+Harminder is *taller (tallest)* than her brother.
+The most recent movie was *excellent (excellenter)*.`,
+
+        `Mount Everest is the *highest (higher)* peak.
+Woody is the *most watched (watched more)* series.
+Sreya is *happier (happy)* than Tina.
+Arhan is a *patient (patienter)* kid.
+These maths problems are *easy (easiest)*.`,
+
+        `This flower is *prettier (pretty)* than that one.
+My sister is *more intelligent (intelligent)* than me.
+Who is the *tallest (most tall)* in your class?
+She is the *best (most good)* of all the singers in the competition.
+This is the *most expensive (expensive)* restaurant I have ever been to.`,
+
+        `My uncle's house is *bigger (big)* than mine.
+That was the *most surprising (most surprised)* news I had ever heard.
+This dress is *more beautiful (beautiful)* than the one I saw yesterday.
+The *best (better)* players on the team are my brother and John.
+The *best (good)* time to plant a tree was twenty years ago.`,
+
+        `This problem is *more complicated (complicated)* than I thought.
+This laptop is *faster (more fast)* than my old one.
+She is *shorter (short)* than him.
+She is *better (more good)* at singing than her sister.
+He is *more skilled (more skill)* at math than me.`
+      ]
+    },
+    {
+      id: 'fillup-comp-2',
+      label: 'Fill in the blanks - 2',
+      type: 'fillup',
+      commonData: {
+        noOptions: true,
+        title:
+          'Fill in the blanks with the most appropriate degree of comparison.'
+      },
+      lockAfter: 2,
+      data: [
+        `Mira works hard. Her sister works *harder*. Her brother works the *hardest*.
+You should do well on your test. It should be *better* than last time. In fact, you should do the *best* this time.
+Ben is crazy. Zen is *crazier* than Ben. But Ron is the *craziest* of them all.`,
+
+        `Rabab is thin. Her sister Rehab is *thinner*. But Rhea is the *thinnest* of them.
+Kartik did badly in his entrance test. Hemant did *worse* than him. Sachin did the *worst*.
+Sangita draws sketches as good as Sanchi does. Sanjay does *better* than his sister. But their mother can draw pictures the *best*.`,
+
+        `Shilpa is tall.	Sheena is *taller* than Shilpa.	Samira is the *tallest* of all the girls.
+She is pretty. Reem is *prettier* than her sister. Riya is the *prettiest* of them.
+Sam is intelligent.	Sam is *more intelligent* than Tina. Sam is the *most intelligent* kid in class.`,
+
+        `He is smart. He is *smarter* than Geetha. He is the *smartest* among them.
+Rinita has a big house.	Rinita has a *bigger* house than Seena.	Roshini has the *biggest* house among them.
+My friend, Bindhu looks happy. Bindhu looks *happier* than my other friends. Bindhu, looks the *happiest* while playing.`,
+
+        `This cake is delicious. That cake is *more delicious* than this one. The chocolate cake is the *most delicious* of all.
+Mary is smart. Jane is *smarter* than Mary. Sara is the *smartest* person I know.
+This book is interesting. That book is *more interesting* than this one. The other book is the *most interesting* of all.`
+      ]
+    },
+    {
+      id: 'fillup-adj',
+      label: 'Fill in the blanks',
+      type: 'fillup',
+      commonData: {
+        noOptions: true,
+        title: 'Fill in the blank with the correct degree of the adjective'
+      },
+      lockAfter: 2,
+      data: [
+        `Rohan is *wiser* than Ayush. (wise)
+This bag is too big, do you have a *smaller* bag? (small)
+The apples are *harder* than the plums. (hard)
+French is *more difficult* than English. (difficult)
+Compared to roti, naan is *better*. (good)`,
+
+        `The tulips are *more beautiful* than roses. (beautiful)
+MS Dhoni is one of the *best* cricket players in the world. (good)
+This is the *most valuable* necklace in our shop. (valuable)
+He is my *elder* brother. (old)
+Burj Khalifa is the *tallest* building in the world. (tall)`,
+
+        `Are you feeling *better* now? (good)
+May is the *hottest* month of the year. (hot)
+Sheela is *taller* than her sister. (tall)
+Yogesh is the *wiser* of the two. (wise)
+You are the *most regular* boy in the class. (regular)`,
+
+        `My brother is *taller*  than me. (tall)
+This is the *most interesting*  book I have ever read. (interesting)
+The red dress is *more expensive*  than the blue one. (expensive)
+John is the *happiest*  person I know. (happy)
+The cat is the *laziest*  animal in the house. (lazy)`,
+
+        `The test was *more difficult* than I expected. (difficult)
+This pizza is *more delicious*  than the one we had last week. (delicious)
+That movie was *scarier*  than I thought it would be. (scary)
+This flower is *more beautiful*  than the one in my garden. (beautiful)
+The elephant is the *biggest*  animal in the zoo. (big)`
+      ]
+    },
+    {
+      label: 'Positive, Comparative or Superlative',
+      type: 'classifySentence',
+      id: 'degree-classify',
+      commonData: {
+        title:
+          'Classify based on the degree of comparison used in the following sentences.',
+        types: ['Positive', 'Comparative', 'Superlative']
+      },
+      data: [
+        [
+          `The sun is shining.
+She has a beautiful voice.
+The book is interesting.
+The flowers are colorful.`,
+
+          `This building is taller than the one next to it.
+She runs faster than her brother.
+The elephant is bigger than the rhino.`,
+
+          `The lion is the loudest animal in the zoo.
+She is the strongest person in her class.
+The giraffe is the tallest animal on earth.`
+        ],
+        [
+          `He is a talented musician.
+The sea is calm today.
+The puppy is playful.`,
+
+          `The vanilla cake is cheaper than the chocolate cake.
+Her new phone is better than her old one.
+He was braver than I thought he would be.
+I am happier when I am with my friends.`,
+
+          `The Pacific Ocean is the largest ocean in the world.
+Losing his job was the saddest moment in his life.
+The blue whale is the biggest animal on earth.`
+        ],
+        [
+          `The biriyani smells delicious.
+The mountain is majestic.
+The rainbow is stunning.`,
+
+          `Her family is wealthier than mine.
+The weightlifter is stronger than the runner.
+The suitcase is heavier than I expected.
+The sun is brighter than the moon.`,
+
+          `Her puppy is the smallest dog in the litter.
+Einstein is one of the smartest people in history.
+The firefighter is the bravest person I know.`
+        ],
+        [
+          `The coffee is hot and fresh.
+He is a generous person.
+The ice cream is sweet and creamy.`,
+
+          `She is kinder to animals than anyone I know.
+The new car is more modern than the old one.
+The math test was more difficult than the science test.`,
+
+          `Being with her makes me the happiest.
+Bill Gates is one of the richest people in the world.
+The elephant is the heaviest animal on land.
+She is the kindest person I have ever met.`
+        ],
+        [
+          `She is a helpful teacher.
+The sky is blue.
+The children are adorable.
+The movie is entertaining.`,
+
+          `The streets are busier in the daytime.
+The instructions for this game are simpler than the ones for the other game.
+Eating vegetables is healthier than eating junk food.`,
+
+          `The newest technology is the most modern.
+Calculus is the most difficult subject in school.
+The Mona Lisa is one of the most beautiful paintings in the world.`
+        ],
+        [
+          `She is a wonderful friend.
+The beach is relaxing.
+Drawing is a creative work.`,
+
+          `Her new dress is more beautiful than the old one.
+The Great Wall of China is longer than the Berlin Wall.
+His jokes are funnier than hers.`,
+
+          `Usain Bolt is the fastest runner in the world.
+The sun is not the brightest star in the Milky Way.
+Times Square is one of the busiest places in New York City.
+The alphabet is the simplest thing to learn.`
+        ]
+      ]
+    }
+  ]
+};
---
+++ 6-8/vocabulary/word-formation-7.js
@@ -0,0 +1,311 @@
+export default {
+  id: 'word-formation-7',
+  label: 'Compound Words',
+  lockAfter: 3,
+  list: [
+    {
+      label: 'Notes',
+      type: 'passage',
+      id: 'word-formn',
+      data: {
+        title: 'Word Formation',
+        text: [
+          `Compound words are formed by combining two or more words to create a new word that has a different meaning. Compound words are mainly of three types.
+1. Closed Compounds: These are compound words that are written as one word without any space or hyphens.`,
+          {
+            type: 'sitewords',
+            text: 'anybody, everybody, nobody, somebody, anyone, everyone, someone, anything, everything, nothing, something, anywhere, everywhere, nowhere, somewhere, babysit, background, barefoot, baseball, basketball, football, bathroom, bedroom, blackberry, blueberry, breakfast, cannot, checkout, cowboy, daylight, desktop, fingerprint, firefly, forever, gentleman, grandmother, grandfather, granddaughter, grapefruit, grasshopper, headquarters, handshake, inside, keyboard, lipstick, mailbox, nevertheless, nonetheless, notebook, outside, payday, railroad, rainbow, raincoat, skateboard, smartphone, snowball, sometimes, sunflower, toothbrush, turntable, undercover, upstream, waterfall, watermelon, weekend, within, without',
+            width: 120
+          },
+          `2. Open Compounds: These are compound words that are written as two separate words.`,
+          {
+            type: 'sitewords',
+            text: 'book store, black eye, cell phone, close call, common sense, cotton candy, dining room, first aid, full moon, French fry, heart attack, high school, hot dog, ice cream, life jacket, living room, no one, peanut butter, post office, prime minister, real estate, remote control, report card, rib cage, role model, roller coaster, salad dressing, search engine, slam dunk, sleeping bag, time capsule, vacuum cleaner, vending machine, video game, waiting room, washing machine, web page',
+            width: 120
+          },
+          `3. Hyphenated Compounds: These are compound words that are written with a hyphen in between the words.`,
+          {
+            type: 'sitewords',
+            text: 'check-in, clean-cut, editor-in-chief, empty-handed, far-fetched, father-in-law, mother-in-law, sister-in-law, free-for-all, know-how, know-it-all, life-size, merry-go-round, long-distance, long-term, mind-blowing, nitty-gritty, one-sided, one-dimensional, two-dimensional, over-the-counter, run-in, runner-up, strong-arm, topsy-turvy, toss-up, up-to-date, well-being, word-of-mouth',
+            width: 120
+          },
+          `=> Compounds words can also be classified as the following:
+1. Compound nouns: These are nouns formed by combining two or more words.
+Eg: toothbrush, sunglasses, teapot, birthday.
+2. Compound adjectives: These are adjectives formed by combining two or more words.
+Eg: red-hot, well-known, high-speed, open-minded.
+3. Compound verbs: These are verbs formed by combining two or more words.
+Eg: drive-in, break up, turn off, pick up.
+4. Compound prepositions: These are prepositions formed by combining two or more words.
+Eg: according to, because of, in spite of, out of.`
+        ]
+      }
+    },
+    {
+      type: 'completeWord',
+      id: 'complete-word-cmp',
+      label: 'Complete Word - Compound Words',
+      commonData: {
+        title: `Answer the riddles with compound words.`
+      },
+      data: [
+        `I can be high or low, but I'm always full of energy. What am I? |Power surge| P____ surge
+I'm something you can hold in your hand, but I'm not solid. What am I?| Air pocket| A__ pocket
+I'm a tool that can be used to cut, but I'm not a knife. What am I?| Wire cutter| W___ cutter
+I'm something you can wear on your head, but I'm not a hat. What am I?| Headband| H___band
+I'm something you can use to light up a room, but I'm not a lamp. What am I? |Light bulb| L____ bulb
+I'm something you can use to clean your teeth, but I'm not toothpaste. What am I? |Dental floss| D_____ floss
+I'm a machine that can be used to lift heavy objects, but I'm not a crane. What am I? |Hydraulic jack| H________ jack
+I'm something you use to hold two things together, but I'm not glue. What am I? |Paperclip| P____clip
+I'm a container that you can carry around, but I'm not a bag. What am I? |Lunchbox| L____box
+I'm something you use to make a call, but I'm not a mobile. What am I? |Telephone| T___phone`,
+
+        `I'm something you can use to write, but I'm not a pen. What am I? |Chalkboard| C____board
+I'm a machine that can be used to cook food, but I'm not an oven. What am I? |Microwave| M____wave
+I'm a tool that can be used to cut down trees, but I'm not an ax. What am I? |Chainsaw| C____saw
+I'm something you can use to make coffee, but I'm not a coffee maker. What am I? |Coffee grounds| C_____ grounds
+I can be opened and closed, and I keep your food fresh. What am I? |Ziplock| Z__lock
+I'm something you use to write on a chalkboard. What am I? |Chalk stick| C____ stick
+I'm something you tie before you go outside. What am I? |Shoe lace| S___ lace
+I'm the type of ball that you bounce. What am I? |Rubber ball| R_____ ball
+I'm something you use to clean the dishes. What am I? |Dish soap| D___ soap
+I'm something you wear on your head to keep warm. What am I? |Winter hat| W_____ hat`,
+
+        `I'm a type of cheese that is yellow and has holes in it. What am I? |Swiss cheese| S____ cheese
+I'm something you use to brush your hair. What am I? |Hair brush| H___ brush
+I'm something you wear on your feet to go swimming. What am I? |Swim fin| S___ fin
+I'm something you use to clean your teeth, but I'm not toothpaste. What am I? |Toothbrush| T____brush
+I'm something you use to carry your things, but I'm not a backpack. What am I? |Handbag| H___bag
+I'm something you use to keep food fresh, but I'm not a fridge. What am I? |Tupperware| T_____ware
+I'm something you can use to clean the floor, but I'm not a broom. What am I? |Dustpan| D___pan`
+      ]
+    },
+    {
+      type: 'rightOne',
+      label: 'Correct Form',
+      id: 'biggest',
+      commonData: {
+        title: `Pick the correct form of the given words.`,
+        printTitle: 'Circle the correct form of thegiven words.',
+        fontSize: '1.3rem'
+      },
+      data: [
+        `anybody, any body, any-body
+everybody, every body, every-body
+nobody, no body, no-body
+check-in, checkin, check in
+clean-cut, cleancut, clean cut  
+somebody, some body, some-body
+anyone, any one, any-one
+book store, book-store, bookstore
+black eye, blackeye, black-eye
+cell phone, cellphone, cell-phone`,
+
+        `everyone, every one, every-one
+someone, some one, some-one
+anything, any thing, any-thing
+editor-in-chief, editorinchief, editor in chief
+empty-handed, empty handed, emptyhanded
+everything, every thing, every-thing
+nothing, no thing, no-thing
+close call, closecall, close-call
+common sense, commonsense, common-sense
+cotton candy, cottoncandy, cotton-candy`,
+
+        `something, some thing, some-thing
+anywhere, any where, any-where
+everywhere, every where, every-where
+nowhere, no where, no-where,
+somewhere, some where, some-where
+strong-arm, strongarm, strong arm
+topsy-turvy, topsyturvy, topsy turvy
+dining room, dining-room, diningroom
+first aid, first-aid, firstaid
+full moon, full-moon, fullmoon`,
+
+        `babysit, baby sit, baby-sit
+background, back ground, back-ground
+barefoot, bare foot, bare-foot
+far-fetched, farfetched, far fetched
+father-in-law, fatherinlaw, father in law 
+baseball, base ball, base-ball
+basketball, basket ball, basket-ball
+French fry, Frenchfry, French-fry,
+heart attack, heartattack, heart-attack,
+high school, highschool, high-school`,
+
+        `football, foot ball, foot-ball
+bathroom, bath room, bath-room
+bedroom, bed room, bed-room
+mother-in-law, motherinlaw, mother in law
+sister-in-law, sisterinlaw, sister in law
+blackberry, black berry, black-berry
+blueberry, blue berry, blue-berry
+hot dog, hotdog, hot-dog
+ice cream, icecream, ice-cream
+life jacket, lifejacket, life-jacket`,
+
+        `breakfast, break fast, break-fast
+cannot, can not, can-not
+checkout, check out, check-out
+free-for-all, freeforall, free for all
+know-how, know how, knowhow
+cowboy, cow boy, cow-boy
+daylight, day light, day-light
+living room, living-room, livingroom
+no one, no-one, noone
+peanut butter, peanutbutter, peanut-butter`,
+
+        `desktop, desk top, desk-top
+fingerprint, finger print, finger-print
+firefly, fire-fly, fire fly
+know-it-all, know it all, knowitall
+life-size, lifesize, life size
+forever, for ever, for-ever
+gentleman, gentle man gentle-man
+post office, postoffice, post-office
+prime minister, prime-minister, primeminister
+real estate, realestate, real-estate`,
+
+        `grandmother, grand mother, grand-mother
+grandfather, grand father, grand-father
+granddaughter, grand daughter, grand-daughter
+merry-go-round, merry go round, merrygoround
+long-distance, longdistance, long distance
+grapefruit, grape fruit, grape-fruit
+grasshopper, grass hopper, grass-hopper
+remote control, remote-control, remotecontrol
+report card, reportcard, report-card
+rib cage, rib-cage, ribcage`,
+
+        `headquarters, head quarters, head-quarters
+handshake, hand shake, hand-shake
+inside, in-side, in side
+keyboard, key-board, key board
+long-term, longterm, long term
+mind-blowing, mindblowing, mind blowing
+lipstick, lip stick, lip-stick
+role model, role-model, rolemodel
+roller coaster, roller-coaster, rollercoaster
+salad dressing, saladdressing, salad-dressing`,
+
+        `mailbox, mail-box, mail box
+nevertheless, never the less, never-the-less
+nonetheless, none the less, none-the-less
+notebook, note book, note-book
+nitty-gritty, nitty gritty, nittygritty
+one-sided, onesided, one sided
+outside, out side, out-side
+search engine, search-engine, searchengine
+slam dunk, slamdunk, slam-dunk
+sleeping bag, sleeping-bag, sleepingbag `,
+
+        `payday, pay day, pay-day
+railroad, rail road, rail-road
+rainbow, rain bow, rain-bow
+one-dimensional, onedimensional, one dimensional
+two-dimensional, twodimensional, two dimensional
+raincoat, rain coat, rain-coat
+skateboard, skate board, skate-board
+time capsule, time-capsule, timecapsule
+vacuum cleaner, vacuumcleaner, vacuum-cleaner
+vending machine, vending-machine, vendingmachine`,
+
+        `smartphone, smart phone, smart-phone
+snowball, snow-ball, snow ball
+sometimes, some times, some-times
+over-the-counter, overthecounter, over the counter
+run-in, runin, run in  
+sunflower, sun flower, sun-flower
+toothbrush, tooth brush, tooth-brush
+video game, videogame,  video-game
+waiting room, waitingroom, waiting-room
+washing machine, washingmachine, washing-machine
+web page, web-page, webpage`,
+
+        `runner-up, runnerup, runner up
+toss-up, tossup, toss up
+up-to-date, uptodate, up to date
+well-being, wellbeing, well being 
+word-of-mouth, wordofmouth, word of mouth `
+      ]
+    },
+    {
+      label: 'Noun or Adjective',
+      type: 'group',
+      id: 'noun-adj',
+      data: {
+        title: 'Classify the following as compound noun or compound adjective.',
+        types: [
+          {
+            name: 'Noun',
+            text: 'anybody, everybody, nobody, somebody, anyone'
+          },
+          {
+            name: 'Adjective',
+            text: 'red-hot, well-known, high-speed, open-minded'
+          }
+        ]
+      }
+    },
+    {
+      label: 'Verb or Preposition',
+      type: 'group',
+      id: 'verb-pre',
+      data: {
+        title:
+          'Classify the following as compound verb or compound preposition.',
+        types: [
+          {
+            name: 'Verb',
+            text: 'drive-in, break up, turn off, pick up'
+          },
+          {
+            name: 'Preposition',
+            text: 'according to, because of, in spite of, out of'
+          }
+        ]
+      }
+    },
+    {
+      label: 'Fill in the blanks',
+      type: 'matchByDragDrop',
+      id: 'drag-drop',
+      lockAfter: 1,
+      commonData: {
+        title: 'Fill in the blanks using suitable compound words.'
+      },
+      data: [
+        `The ladybug crawled up the *sunflower* stem.
+The soccer team practiced on the 8grassy field*. 
+The *bookshelf* in the corner was filled with novels.
+The *ice cream* truck drove slowly down the neighborhood street.
+The *chocolate chip* cookie was delicious.`,
+
+        `The *skyscraper* was a marvel of modern engineering.
+She wore a brightly colored *sunflower-print* dress to the party.
+The football team practiced on the *well-manicured* turf field.
+The movie theater was showing a *science-fiction* thriller.
+He drove his car through the busy downtown *intersection*.`,
+
+        `The campers gathered around the crackling *campfire*.
+The chef prepared a mouth-watering *three-course* meal.
+The bookstore had a vast selection of *best-selling* novels.
+The babysitter read the bedtime story to the *wide-eyed* children.
+The fireman used a *high-pressure* water hose to extinguish the flames.`,
+
+        `The *lifeguard* watched the swimmers from the poolside chair.
+The *birthday* cake was decorated with colorful frosting and candles.
+The doctor prescribed a *high-dose* pain medication for the patient.
+The soccer players kicked the ball back and forth on the *grassy field*.
+The wedding reception was held in a beautifully decorated *banquet hall*.`,
+
+        `The *snowstorm* caused widespread power outages across the city.
+The *pet store* sold a variety of furry and scaly creatures.
+The construction workers used *heavy-duty* equipment to excavate the site.
+The *fashion designer* created a stunning haute couture collection.
+The *police officer* apprehended the suspect and put him in handcuffs.`
+      ]
+    }
+  ]
+};
