84b000f74ebd08184225bc9195f1d62a2fb89102
Rethna <rethna2@gmail.com>
2023-05-15 10:22:38 +0530

    updated math

--- 6-8/math/rational-numbers.js
+++ 6-8/math/rational-numbers.js
@@ -3,111 +3,121 @@ export default {
   id: 'rational-numbers',
   list: [
     {
-      label: 'Natural and Whole Numbers',
+      label: 'Notes',
       type: 'passage',
-      id: 'conditional-notes',
+      id: 'notes',
       data: {
-        title: 'Numbers',
+        title: 'Rational Numbers',
         text: [
-          `Natural numbers are the set of positive integers, from 1 to ∞ , but it doesn't include fractional and decimal numbers. They are also known as counting numbers.
-Whole numbers are similar to Natural numbers but they start from 0 instead of 1.
-Integers are a set of positive numbers, zero and negative numbers. But it doesn't include decimals and fractions.
-Natural Numbers = {1, 2, 3, 4, ....}
-Whole Numbers = {0, 1, 2, 3, 4, ....}
-Integers = {..., -2, -1, 0, 1, 2, 3, ...}
-# Closure Property
-If we add two natural numbers, the result is also a natural number. 
-Eg: 3 + 5 = 8 (3, 5, 8 - natural numbers).
-Similarly if we multiply two natural numbers, the result is also a natural number. 
-Eg: 4 x 6 = 24 (4, 6, 24 are natural numbers)
-Natural numbers are always closed under addition and multiplication. It is called the Closure property.
-Closure property does not hold good for subtraction and division.
-Eg: 3-5= -2 (not a natural number)
-10/4 = 2.5 (not a natural number)
-# Associative Property
-If we have to add three numbers in the mind, there are two ways. We can add the first two numbers and add the result with the third number. Alternatively, we can add the second and third number and add the result with the first number.
-Lets say, we have to add 20, 30 & 50
-Method 1: (20 + 30) + 50 = 50+50 = 100
-Method 2: 20 + (30 + 50) = 20 + 80 = 100
-Whichever method we follow, the answer is the same. This property is known as associative property.
-The associative property holds true for addition and multiplication of natural numbers.
-The associative property does not hold true for addition and multiplication of natural numbers.
-# Commutative Property
-30 + 50 = 50 + 30
-10 x 20 = 20 x 10
-30 - 50 ≠  50 - 30
-5 ÷  10 ≠  10 ÷  5
-Commutative property holds true for addition and multiplication and doesn't hold true for subtraction and division.
-# Additive identity
-The number 0 is called the additive identity.  Adding any number with the additive identity, gives the number itself.
-# Multiplicative identity
-The number 1 is called the multiplicative identity. Multiplying any number with 1, gives the number itself.`
-        ]
-      }
-    },
-    {
-      label: 'Multiple Choice Questions',
-      type: 'mcq',
-      id: 'mcq-adj',
-      data: {
-        title: 'Multiple Choice Questions',
-        questions: [
+          `A rational number is a type of real number, which is in the form of p/q where q is not equal to zero.
+Rational numbers can be positive or negative. Positive numbers are greater than zero. Negative numbers are less than zero.
+For positive rational numbers, both numerator and denominator have same sign. For negative rational numbers, numerator and denominator are of opposite sign.
+# Negative of a number`,
           {
-            qText: `Which of the following number is called additive identity?`,
-            options: `one, *zero, natural numbers, none of the above`
+            type: 'html',
+            text: `In general, for an integer a, we have,<br><br>
+a + (– a) = (– a) + a = 0<br><br>
+so, a is the negative of –a and –a is the negative of a.
+-a is called the negative or additive inverse of a.`
+          },
+          `# Reciprocal
+Reciprocal of a rational number is a number which we multiply with the rational number to get the product of 1.
+What would you multiply 8/21 , to get the product 1? Obviously by 21/8`,
+          {
+            type: 'display',
+            text: 'since, 8/21 × 21/8 = 1'
           },
           {
-            qText: `Which of the following is not a natural number?`,
-            options: `0, 1, 7, 10`
-          },
-          {
-            qText: `Which of the following is not a whole number?`,
-            options: `-1, 0, 1, 5`
-          },
-          {
-            qText: `Commutative property holds true for ______.`,
-            options: `multiplication, subtraction, division`
-          },
-          {
-            qText: `Associative Property holds true for ________.`,
-            options: `addition, subtraction, division`
-          },
-          {
-            qText: `Which of the following number is called multiplicative identity?`,
-            options: `zero, natural numbers, *one, none of the above`
-          },
-          {
-            qText: `What is the last number in natural numbers?`,
-            options: `Infinity, Crores, Billions`
+            type: 'display',
+            text: `We say that a rational number c/d is called the reciprocal or multiplicative inverse of another non-zero rational number a/b, if a/b × c/d = 1`
           }
         ]
       }
     },
     {
-      label: 'True or False',
-      type: 'classifySentence',
-      id: 'true-false',
+      type: 'numberInput',
+      id: 'commutative',
+      label: 'Additive Inverse',
       data: {
-        title: 'Classify the following sentences as True or False',
-        types: [
-          {
-            name: 'True',
-            text: `Every natural number is part of whole numbers.
-            Zero is part of whole number.
-            Natural numbers obey closure property during addition.
-            Whole numbers obey closure property during multiplication.
-            Commutative property holds good for addition of natural numbers.`
-          },
-          {
-            name: 'False',
-            text: `Every whole number is part of natural numbers.
-            Zero is part of natural number.
-            Whole numbers obey closure property during subtraction.
-            Natural numbers obey closure property during division.
-            Commutative property holds good for subtraction of whole numbers. numbers.`
-          }
+        type: 'word-problem',
+        title: 'Write the additive inverse of the below number.',
+        isSentence: false,
+        firstLineFontSize: '2rem',
+        text: `-50  | 50
+100 | -100
+0 | 0
+45.5 | -45.5
+-34.9 | 34.9`
+      }
+    },
+    {
+      type: 'numberInput',
+      id: 'commutative',
+      label: 'Multiplicative Inverse',
+      data: {
+        type: 'word-problem',
+        title: 'Write the multiplicative inverse of the below number.',
+        isSentence: false,
+        firstLineFontSize: '2rem',
+        list: [
+          `2/3 
+Ans = ?0/?1
+3, 2`,
+
+          `7/11
+Ans = ?0/?1
+11, 7`,
+
+          `7
+Ans = ?0/?1
+1, 7`,
+
+          `1/11
+Ans = ?0
+11`,
+
+          `-9/13
+Ans = ?0/?1
+13, -9`
         ]
       }
+    },
+    {
+      type: 'numberInput',
+      id: 'findvalue',
+      label: 'Find the Value',
+      commonData: {
+        type: 'word-problem',
+        title: 'Fill in the blanks.',
+        isSentence: false,
+        answerBox: false,
+        firstLineFontSize: '2rem'
+      },
+      data: [
+        `1 - 4/5 = ?0/?1  | 1, 5
+1 - 7/11 = ?0/?1  | 4, 11
+1 - 9/20 = ?0/?1  | 11, 20
+1 - 7/15 = ?0/?1  | 8, 15
+1 - 5/13 = ?0/?1  | 8, 13`,
+
+        `-2/5 + -1/5 = ?0/?1  | -3, 5
+-7/25 + -7/25 = ?0/?1  | -14, 25
+-3/10 + -2/5 = ?0/?1  | -7, 10
+-2/7 + -1/7 = ?0/?1  | -3, 7
+-13/50 + -11/50 = ?0/?1  | -24, 50`,
+
+        `2/7 - ( -3/7 ) = ?0/?1 | 5, 7
+4/9 - ( -1/9 ) = ?0/?1 | 5, 9
+11/25 - ( -2/25 ) = ?0/?1 | 13, 25
+7/20 - ( -4/20 ) = ?0/?1 | 11, 20
+3/13 - ( -7/13 ) = ?0/?1 | 10, 13`,
+
+        `-1/9 - ( -2/9 ) = ?0/?1 | 1, 9
+-4/7 - ( -1/7 ) = ?0/?1 | -3, 7
+-11/25 - ( -2/25 ) = ?0/?1 | -9, 25
+-5/20 - ( -4/20 ) = ?0/?1 | -1, 20
+-3/13 - ( -7/13 ) = ?0/?1 | 4, 13`
+      ]
     }
   ]
 };
