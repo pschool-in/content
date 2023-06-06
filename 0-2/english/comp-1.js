c63a3c01b06f619ad55928709d24fb96a204c364
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-06-06 10:42:47 +0530

    updated comp-1.js

--- 0-2/english/comp-1.js
+++ 0-2/english/comp-1.js
@@ -46,7 +46,7 @@ export default {
         questions: [
           {
             qText: 'What kind of insect is a butterfly?',
-            options: 'colorful, winged, six-legged, *all of the above'
+            options: 'colorful, colorless, seven-legged'
           },
           {
             qText: 'How many legs does a butterfly have?',
@@ -141,15 +141,175 @@ export default {
           },
           {
             qText: 'Which of the following body parts do jellyfish lack?',
-            options: 'brain, eyes, heart, *all of the above'
+            options: 'brain, stomach, tentacles'
           },
           {
             qText: 'How do jellyfish use their tentacles?',
             options: 'for stinging prey, for breathing, for flying'
           },
           {
-            qText: 'What can jellyfish look like?',
-            options: 'clear, blue, pink, all of the above'
+            qText: 'Which part helps them to sting their prey?',
+            options: 'tentacles, fin, gills'
+          }
+        ]
+      }
+    },
+    {
+      type: 'informationProcessing',
+      id: 'flamingo',
+      label: 'Flamingo',
+      data: {
+        title: 'Read the passage and answer the questions',
+        type: 'passage',
+        text: `Flamingos are tall and pink in color. Then have thin legs and long necks. They have large wings and a tiny tail. They love to eat snails and shrimp. They only eat 60 grams of food a day.`,
+        image: 'flamingo',
+        fontSize: '1.3rem',
+        questions: [
+          {
+            qText: 'What is the main color of flamingos?',
+            options: 'pink, blue, green, yellow'
+          },
+          {
+            qText: 'What type of food do flamingos prefer to eat?',
+            options:
+              'Snails and Shrimp, Grass and Leaves, Apples and Bananas, Fish and Meat'
+          },
+          {
+            qText: 'How much food does a flamingo consume daily?',
+            options: '60 grams, 100 grams, 500 grams, 1000 grams'
+          },
+          {
+            qText:
+              'Which of the following features are characteristic of flamingos?',
+            options: 'Thin legs, Short necks, Blue colour'
+          }
+        ]
+      }
+    },
+    {
+      type: 'informationProcessing',
+      id: 'peacock',
+      label: 'Peacock',
+      data: {
+        title: 'Read the passage and answer the questions',
+        type: 'passage',
+        text: `Peacocks are beautiful birds with majestic wings. They are known worldwide for their beauty. They are our national bird. They are mostly seen in rainforests. The moon-like spots on Peacock's feathers make the shape of an eye when fully opened.`,
+        image: 'peacock',
+        fontSize: '1.3rem',
+        questions: [
+          {
+            qText: 'What is the national bird of India?',
+            options: 'Peacock, Sparrow, Eagle, Pigeon'
+          },
+          {
+            qText: 'Where are peacocks mostly found?',
+            options: 'rainforests, deserts, grasslands, arctic regions'
+          },
+          {
+            qText: "What makes the peacock's feathers look like eyes?",
+            options: 'moon-shaped spots, stripes, dots, zigzag patterns'
+          },
+          {
+            qText: 'What makes peacocks known worldwide?',
+            options:
+              'Their majestic wings, Their long-distance flights, Their ability to mimic human speech'
+          }
+        ]
+      }
+    },
+    {
+      type: 'informationProcessing',
+      id: 'owl',
+      label: 'Owl',
+      data: {
+        title: 'Read the passage and answer the questions',
+        type: 'passage',
+        text: `An owl is a bird that hates the light. It can see well at night. They hunt at night. They eat insects and small birds. They are wise creatures.`,
+        image: 'owl',
+        fontSize: '1.3rem',
+        questions: [
+          {
+            qText: 'What is the main characteristic of an owl?',
+            options:
+              "It hates the light, It loves the light, It can't see well in the dark, It has excellent hearing"
+          },
+          {
+            qText: 'When do owls typically hunt?',
+            options:
+              'During the night, During the day, At dusk and dawn, Whenever they feel like it'
+          },
+          {
+            qText: 'What is a common prey for owls?',
+            options:
+              'Insects and small birds, Plants and fruits,  Fish and amphibians, Small mammals'
+          },
+          {
+            qText: 'How are owls often described?',
+            options:
+              'As intelligent or wise creatures, As clumsy and slow flyers, As solitary and aggressive animals, As brightly colored birds'
+          }
+        ]
+      }
+    },
+    {
+      type: 'informationProcessing',
+      id: 'vulture',
+      label: 'Vulture',
+      data: {
+        title: 'Read the passage and answer the questions',
+        type: 'passage',
+        text: `Vulture is a large and heavy bird. They help in cleaning the environment. They feed on rotten and dead creatures. They have very good eye sight. They build their nests on very high places.`,
+        image: 'vulture',
+        fontSize: '1.3rem',
+        questions: [
+          {
+            qText: 'What type of creatures do vultures typically feed on?',
+            options: 'Rotten and dead creatures, insects, fish, small animals'
+          },
+          {
+            qText:
+              'What is one of the benefits of vultures in the environment?',
+            options:
+              'They clean the environment, They help in pollination, They spread diseases'
+          },
+          {
+            qText:
+              'Which sense do vultures possess that helps them locate food?',
+            options: 'Very good eyesight, Good tastebuds, Good sense of smell'
+          },
+          {
+            qText: 'Where do vultures typically build their nests?',
+            options: 'On very high places, On tree branches, In caves'
+          }
+        ]
+      }
+    },
+    {
+      type: 'informationProcessing',
+      id: 'hornbill',
+      label: 'Hornbill',
+      data: {
+        title: 'Read the passage and answer the questions',
+        type: 'passage',
+        text: `Hornbills are bright colored birds. Hornbills are close relatives of Toucans. Hornbills have a long, yellow and curved beak. They have thin necks, broad wings and long tails. They have very short tongues. Their best friends are mongoose.`,
+        image: 'hornbill',
+        fontSize: '1.3rem',
+        questions: [
+          {
+            qText: 'What is the physical characteristic of hornbills?',
+            options: 'broad wings, short beak, thick neck, short tail'
+          },
+          {
+            qText: 'Which bird is a close relative of hornbills?',
+            options: 'Toucans, Parrots, Sparrows, Penguins'
+          },
+          {
+            qText: 'What color is the beak of hornbills?',
+            options: 'yellow, red green, blue'
+          },
+          {
+            qText: 'Who are the best friends of hornbills?',
+            options: 'Mongoose, Monkey, Elephant, Lion'
           }
         ]
       }
