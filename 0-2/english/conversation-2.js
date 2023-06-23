5a7640e7fddc68177c7ac24cee4cb6b37e7a1358
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-06-23 11:15:31 +0530

    added conversation-2.js, thoughts-8.js

--- 0-2/english/conversation-2.js
+++ 0-2/english/conversation-2.js
@@ -19,6 +19,53 @@ Emily: You're welcome! We're glad we could help.`
       }
     },
     {
+      id: 'kitten-mcq',
+      label: 'Multiple Choice Questions',
+      type: 'mcq',
+      data: {
+        title: 'Choose the correct option',
+        questions: [
+          {
+            qText: 'What did Emily find outside their gate?',
+            options:
+              'A little kitten, A lost puppy, A lost bird, A stray squirrel'
+          },
+          {
+            qText: `Why did Sarah suggest finding the kitten's owner?`,
+            options: `Because the kitten looked lost
+Because she wanted to sell the kitten
+Because she wanted to keep the kitten
+Because she was scared of the kitten`
+          },
+          {
+            qText: `What did Emily and Sarah do to find the kitten's owner?`,
+            options: `They knocked on their neighbors' doors
+They put up posters around the neighborhood
+They took the kitten to an animal shelter
+They called their friends to ask if they owned the kitten`
+          },
+          {
+            qText:
+              'How did the neighbor react when they found out the kitten belonged to them?',
+            options: `They thanked Emily and Sarah for finding the kitten
+They asked Emily and Sarah to keep the kitten
+They ignored Emily and Sarah`
+          }
+        ]
+      }
+    },
+    {
+      label: 'Complete Word',
+      type: 'completeWord',
+      id: 'kitten-word',
+      data: {
+        text: `Very cute or lovable.|ADORABLE
+Unable to find one's way.|LOST
+Slide unintentionally.|SLIP
+People who live close to each other.|NEIGHBOR`
+      }
+    },
+    {
       id: 'pencil',
       type: 'passage',
       label: 'The Magic Pencil',
@@ -35,6 +82,51 @@ Alex: Sure, here you go. Be careful, it might run away!`
       }
     },
     {
+      id: 'pencil-mcq',
+      label: 'Multiple Choice Questions',
+      type: 'mcq',
+      data: {
+        title: 'Choose the correct option',
+        questions: [
+          {
+            qText: `What makes Alex's pencil special?`,
+            options: `It brings drawings to life.
+It never needs sharpening.
+It has a unique design.
+It can draw realistic flowers.`
+          },
+          {
+            qText: `How does Lisa react when she sees Alex's flower drawing?`,
+            options: `She finds it unbelievable.
+She dislikes the drawing style.
+She wants to draw her own flower.`
+          },
+          {
+            qText: `What does Alex warn Lisa about when he gives her the pencil?`,
+            options: `The drawings might come to life and run away.
+The pencil might break easily.
+The pencil might be too sharp.
+The drawings might disappear.`
+          },
+          {
+            qText: `Alex's pencil has a __________ feature that brings his drawings to life.`,
+            options: `special, spacial, spetial`
+          }
+        ]
+      }
+    },
+    {
+      label: 'Complete Word',
+      type: 'completeWord',
+      id: 'pencil-word',
+      data: {
+        text: `To go or escape suddenly.|RUN AWAY|_U_ A_A_
+Extraordinary or incredible.|UNBELIEVABLE|U__E_I__A__E
+To cease to be visible.|DISAPPEAR|_I__P_E_R
+One of a kind or without equal.|UNIQUE`
+      }
+    },
+    {
       id: 'ant',
       type: 'passage',
       label: 'The Clever Ant',
@@ -46,6 +138,50 @@ Priya: Yes, it's huge! But why is everyone gathering around it?
 Mary: There's an obstacle in their way. They're trying to figure out how to cross it.
 Priya: Oh, look! That ant just climbed on top of the obstacle and made a bridge with its body.
 Mary: Wow, that ant is so clever! The others can now walk across the bridge.`
+      }
+    },
+    {
+      id: 'ant-mcq',
+      label: 'Multiple Choice Questions',
+      type: 'mcq',
+      data: {
+        title: 'Choose the correct option',
+        questions: [
+          {
+            qText: `What are the ants trying to do near the ant hill?`,
+            options: `Cross an obstacle
+Create a gathering spot
+Build a bridge
+Find food`
+          },
+          {
+            qText: `The ants are trying to figure out how to ________ the obstacle.`,
+            options: `cross, create, find, skip`
+          },
+          {
+            qText: `What did the ant do to help the others cross the obstacle?`,
+            options: `Made a bridge with its body
+Called for help
+Climbed over it
+Removed the obstacle`
+          },
+          {
+            qText: `The ants ___________ leaves and twigs to construct the bridge.`,
+            options: `gather, get together, move`
+          }
+        ]
+      }
+    },
+    {
+      label: 'Complete Word',
+      type: 'completeWord',
+      id: 'ant-word',
+      data: {
+        text: `A structure that connects two points over an obstacle.|BRIDGE
+The physical structure of a living organism.|BODY
+To come together in one place.|GATHER
+Something that blocks or hinders progress.|OBSTACLE
+Quick-witted or intelligent.|CLEVER`
       }
     },
     {
@@ -67,6 +203,46 @@ Lily and Peter: It worked!`
       }
     },
     {
+      id: 'forest-mcq',
+      label: 'Multiple Choice Questions',
+      type: 'mcq',
+      data: {
+        title: 'Choose the correct option',
+        questions: [
+          {
+            qText: `Which of the following best describes the magical forest?`,
+            options: `A place where wishes come true.
+A forest behind the school.
+A mysterious place with hidden treasures.
+A forest filled with enchanted creatures.`
+          },
+          {
+            qText: `The __________ forest was filled with sparkling lights and mystical creatures.`,
+            options: `enchanted, cursed, mysterious, haunted`
+          },
+          {
+            qText: `According to the __________, anyone who finds the hidden treasure will be granted eternal happiness.`,
+            options: `legend, book, map, fable`
+          },
+          {
+            qText: `Lily closed her eyes and made a silent __________ for her dream to come true.`,
+            options: `wish, whisper, spell, cry`
+          }
+        ]
+      }
+    },
+    {
+      label: 'Complete Word',
+      type: 'completeWord',
+      id: 'forest-word',
+      data: {
+        text: `Having special powers or properties beyond the realm of nature.|MAGICAL
+A traditional story or narrative handed down through generations.|LEGEND
+A desire or longing for something to happen or be true.|WISH
+Under the influence of magic or possessing magical qualities.|ENCHANTED`
+      }
+    },
+    {
       id: 'key',
       type: 'passage',
       label: 'The Mystery of the Missing Key',
@@ -80,6 +256,45 @@ Meera: Yes, I searched everywhere. Maybe it fell out of my bag at school.
 Meera: Hi, Lily. Did you find a key yesterday? It's for my diary.
 Lily: Yes, I did! I found it near the library. I thought it looked important.
 Meera: Thank you so much, Lily. I was worried. Now my diary is safe again.`
+      }
+    },
+    {
+      id: 'key-mcq',
+      label: 'Multiple Choice Questions',
+      type: 'mcq',
+      data: {
+        title: 'Choose the correct option',
+        questions: [
+          {
+            qText: `Where did Meera find her missing key?`,
+            options: `At school near the library
+In Lily's bag
+Her mom had it all along`
+          },
+          {
+            qText: `Meera was ___________ about her missing key.`,
+            options: `worried, scared, happy, sad`
+          },
+          {
+            qText: `Where did Meera think she might have lost the key to her diary?`,
+            options: `At school, In her bag, In her room, In the library`
+          },
+          {
+            qText: `Whom did Meera ask about the key?`,
+            options: `Her mom, Her teacher, The librarian, School staff`
+          }
+        ]
+      }
+    },
+    {
+      label: 'Complete Word',
+      type: 'completeWord',
+      id: 'key-word',
+      data: {
+        text: `A book for recording personal experiences and thoughts.|DIARY
+A place where books are kept for borrowing or reference.|LIBRARY
+Having great value or significance.|IMPORTANT
+Feeling anxious or concerned about something.|WORRIED`
       }
     }
   ]
---
+++ 6-8/reading/thoughts-8.js
@@ -0,0 +1,124 @@
+export default {
+  label: 'Great Thoughts',
+  id: 'thoughts-8',
+  lockAfter: 2,
+  list: [
+    {
+      id: 'mockingbird',
+      type: 'passage',
+      label: `To Kill a Mockingbird`,
+      data: {
+        title: `To Kill a Mockingbird`,
+        author: 'Harper Lee',
+        text: `"Atticus said to Jem one day, "I'd rather you shot at tin cans in the backyard, but I know you'll go after birds. Shoot all the blue jays you want, if you can hit 'em, but remember it's a sin to kill a mockingbird." That was the only time I ever heard Atticus say it was a sin to do something, and I asked Miss Maudie about it. "Your father's right," she said. "Mockingbirds don't do one thing except make music for us to enjoy. They don't eat up people's gardens, don't nest in corn cribs, they don't do one thing but sing their hearts out for us. That's why it's a sin to kill a mockingbird."`
+      }
+    },
+    {
+      id: 'women',
+      type: 'passage',
+      label: 'Little Women',
+      data: {
+        title: 'Little Women',
+        author: 'Louisa May Alcott',
+        text: `There are many Beths in the world, shy and quiet, sitting in corners till needed, and living for others so cheerfully that no one sees the sacrifices till the little cricket on the hearth stops chirping, and the sweet, sunshiny presence vanishes, leaving silence and shadow behind.`
+      }
+    },
+    {
+      id: 'lion',
+      type: 'passage',
+      label: 'The Lion, the Witch, and the Wardrobe',
+      data: {
+        title: 'The Lion, the Witch, and the Wardrobe',
+        type: 'poem',
+        lineGap: '5px',
+        author: ' C. S. Lewis',
+        text: `Wrong will be right,
+when Aslan comes in sight,
+At the sound of his roar,
+sorrows will be no more,
+When he bares his teeth,
+winter meets its death,
+And when he shakes his mane,
+we shall have spring again.`
+      }
+    },
+    {
+      id: 'dune',
+      type: 'passage',
+      label: 'Dune',
+      data: {
+        title: 'Dune',
+        author: 'Frank Herbert',
+        text: `"I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain." `
+      }
+    },
+    {
+      id: 'fahrenheit',
+      type: 'passage',
+      label: 'Fahrenheit 451',
+      data: {
+        title: 'Fahrenheit 451',
+        author: 'Ray Bradbury',
+        text: `"Everyone must leave something behind when he dies, my grandfather said. A child or a book or a painting or a house or a wall built or a pair of shoes made. Or a garden planted. Something your hand touched some way so your soul has somewhere to go when you die, and when people look at that tree or that flower you planted, you're there.
+It doesn't matter what you do, he said, so long as you change something from the way it was before you touched it into something that's like you after you take your hands away. The difference between the man who just cuts lawns and a real gardener is in the touching, he said. The lawn-cutter might just as well not have been there at all; the gardener will be there a lifetime."`
+      }
+    },
+    {
+      id: 'dream',
+      type: 'passage',
+      label: '1963 "I Have a Dream" speech',
+      data: {
+        title: '1963 "I Have a Dream" speech',
+        author: 'Martin Luther King, Jr.',
+        text: `"I say to you today, my friends, so even though we face the difficulties of today and tomorrow, I still have a dream. It is a dream deeply rooted in the American dream. I have a dream that one day this nation will rise up and live out the true meaning of its creed, 'We hold these truths to be self-evident, that all men are created equal' …
+"I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character."`
+      }
+    },
+    {
+      id: '1863',
+      type: 'passage',
+      label: '1863 Gettysburg Address',
+      data: {
+        title: '1863 Gettysburg Address',
+        author: 'Abraham Lincoln',
+        text: `"It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us; that from these honored dead we take increased devotion to that cause for which they here gave the last full measure of devotion; that we here highly resolve that these dead shall not have died in vain; that this nation, under God, shall have a new birth of freedom, and that government of the people, by the people, for the people, shall not perish from the Earth."`
+      }
+    },
+    {
+      id: '2004',
+      type: 'passage',
+      label: '2004 Democratic National Convention keynote address',
+      data: {
+        title: '2004 Democratic National Convention keynote address',
+        author: 'Barack Obama',
+        text: `"There's not a liberal America and a conservative America; there's the United States of America. There's not a Black America and white America and Latino America and Asian America; there's the United States of America …
+"We are one people, all of us pledging allegiance to the stars and stripes, all of us defending the United States of America. In the end, that's what this election is about. Do we participate in a politics of cynicism, or do we participate in a politics of hope?"`
+      }
+    },
+    {
+      id: '1942',
+      type: 'passage',
+      label: 'Quit India, 1942',
+      data: {
+        title: 'Quit India, 1942',
+        author: 'Mahatma Gandhi',
+        text: `"Everyone must leave something behind when he dies, my grandfather said. A child or a book or a painting or a house or a wall built or a pair of shoes made. Or a garden planted. Something your hand touched some way so your soul has somewhere to go when you die, and when people look at that tree or that flower you planted, you're there.
+It doesn't matter what you do, he said, so long as you change something from the way it was before you touched it into something that's like you after you take your hands away. The difference between the man who just cuts lawns and a real gardener is in the touching, he said. The lawn-cutter might just as well not have been there at all; the gardener will be there a lifetime."`
+      }
+    },
+    {
+      id: '1964',
+      type: 'passage',
+      label: ' I Am The First Accused, 1964',
+      data: {
+        title: ' I Am The First Accused, 1964',
+        author: 'Nelson Mandela',
+        text: `"Above all, we want equal political rights, because without them our disabilities will be permanent. I know this sounds revolutionary to the whites in this country, because the majority of voters will be Africans. This makes the white man fear democracy.
+But this fear cannot be allowed to stand in the way of the only solution which will guarantee racial harmony and freedom for all. It is not true that the enfranchisement of all will result in racial domination.
+Political division, based on colour, is entirely artificial and, when it disappears, so will the domination of one colour group by another.
+The ANC has spent half a century fighting against racialism. When it triumphs it will not change that policy.
+This then is what the ANC is fighting. Their struggle is a truly national one. It is a struggle of the African people, inspired by their own suffering and their own experience. It is a struggle for the right to live."`
+      }
+    }
+  ]
+};
