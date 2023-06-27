9b4ee525eb1439978640c85eaad6dbb23db938b6
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-06-27 11:41:11 +0530

    updated grammar-6-sc.js, facts-opinion.js, conversation-3.js, conversation-5.js

---
+++ 3-5/reading/conversation-3.js
@@ -0,0 +1,229 @@
+export default {
+  label: "Let's Speak",
+  id: 'conversation-3',
+  lockAfter: 2,
+  list: [
+    {
+      id: 'ranger',
+      type: 'passage',
+      label: 'Ranger Rick',
+      data: {
+        title: 'Ranger Rick',
+        text: [
+          {
+            type: 'html',
+            text: `[A dense forest.]<br><br>
+<b>Max</b>: Lily, where are we? It's so dark!<br>
+<b>Lily</b>: I think we're lost, Max. What should we do?<br>
+<b>Max</b>: Let's ask for help. Hello, is anyone there?<br>
+<b>Voice</b>: I'm here, children. I am Ranger Rick. How can I assist you?<br>
+<b>Lily</b>: We're lost in the woods, Ranger Rick. Can you help us find our way back?<br>
+<b>Ranger Rick</b>: Don't worry, kids. Follow me, and we'll find your way home.<br><br>
+[Max and Lily follow Ranger Rick through the forest.]<br><br>
+<b>Max</b>: Look, Lily! Our house is just ahead!<br>
+<b>Lily</b>: Thank you, Ranger Rick. We're safe now!<br>
+<b>Ranger Rick</b>: You're welcome, children. Remember, always stay on the trail.<br>
+[Max and Lily wave goodbye to Ranger Rick and run towards their house.]`
+          }
+        ]
+      }
+    },
+    {
+      id: 'mommy',
+      type: 'passage',
+      label: 'Day out with Mommy',
+      data: {
+        title: 'Day out with Mommy',
+        text: [
+          {
+            type: 'html',
+            text: `<b>Sara</b>: Good morning!<br>
+<b>Mom</b>: Good morning, sweetie! Did you sleep well?<br>
+<b>Sara</b>: Yes, I had a great sleep. What are we doing today?<br>
+<b>Mom</b>: Well, after breakfast, we can go to the park. How does that sound?<br>
+<b>Sara</b>: Yay! I love playing at the park. Can we bring my ball?<br>
+<b>Mom</b>: Of course! We'll bring your ball and maybe some snacks too. What games do you want to play?<br>
+<b>Sara</b>: I want to swing on the swings, slide down the slide, and maybe even play catch with you!<br>
+<b>Mom</b>: That sounds like so much fun! We'll have a great time together. Let's finish breakfast and get ready to go.<br>
+<b>Sara</b>: Okay! I can't wait to have an amazing day with you, Mommy!`
+          }
+        ]
+      }
+    },
+    {
+      id: 'superhero',
+      type: 'passage',
+      label: 'Superheroes',
+      data: {
+        title: 'Superheroes',
+        text: [
+          {
+            type: 'html',
+            text: `<b>Peter</b>: Hey, do you want to play superheroes today?<br>
+<b>Mary</b>: Yeah, that sounds awesome! Who do you want to be?<br>
+<b>Peter</b>: I'll be Spider-Man! I love his web-slinging powers.<br>
+<b>Mary</b>: Cool! I'll be Wonder Woman. She's so strong and brave.<br>
+<b>Peter</b>: Let's create our own superhero mission. We can save the world from an evil villain!<br>
+<b>Mary</b>: Yes! We'll rescue people and protect the city. Nothing can stop us!<br>
+<b>Peter</b>: We'll need super gadgets and secret hideouts too.<br>
+<b>Mary</b>: Definitely! Let's use our imaginations and have the best adventure ever!<br>
+<b>Peter</b>: Let's go, superheroes! The world needs us!`
+          }
+        ]
+      }
+    },
+    {
+      id: 'game',
+      type: 'passage',
+      label: 'Game Time',
+      data: {
+        title: 'Game Time',
+        text: [
+          {
+            type: 'html',
+            text: `<b>Reem</b>: Hey, did you hear about the new game that came out? It's so cool!<br>
+<b>Rishi</b>: Yeah, I played it yesterday. It's awesome! You can build your own virtual world and explore with friends.<br>
+<b>Reem</b>: That sounds amazing! I can't wait to try it. What's your favorite part of the game?<br>
+<b>Rishi</b>: Definitely the mini-games. They're so fun and challenging. I also love customizing my character and finding hidden treasures.<br>
+<b>Reem</b>: Oh, I love collecting treasures too! We should play together sometime and go on adventures.<br>
+<b>Rishi</b>: That would be awesome! Let's plan a gaming session after school. We'll be the best team ever!`
+          }
+        ]
+      }
+    },
+    {
+      id: 'monkey',
+      type: 'passage',
+      label: 'The Monkey that went to School',
+      data: {
+        title: 'The Monkey that went to School',
+        text: [
+          {
+            type: 'html',
+            text: `<b>Charlie</b>: Hey, did you hear about the monkey who went to school?<br>
+<b>John</b>: No, what happened?<br>
+<b>Charlie</b>: Well, he climbed up the flagpole and started swinging from it, shouting, "I'm the king of the school!"<br>
+<b>John</b>: Haha, that's silly! Did the teachers catch him?<br>
+<b>Charlie</b>: Oh, they tried, but he was too fast. He swung from the flagpole to the basketball hoop, then bounced off and landed in the principal's office!<br>
+<b>John</b>: That monkey sure knows how to have fun! I wish I could swing around like that.<br>
+<b>Charlie</b>: Yeah, but imagine if all the students turned into monkeys. The school would be a jungle!<br>
+<b>John</b>: Hahaha, that would be chaos! Let's hope that doesn't happen.`
+          }
+        ]
+      }
+    },
+    {
+      id: 'circus',
+      type: 'passage',
+      label: 'Monkey Circus',
+      data: {
+        title: 'Monkey Circus',
+        text: [
+          {
+            type: 'html',
+            text: `<b>Hari</b>: Hey, have you ever seen a monkey ride a bicycle?<br>
+<b>Henry</b>: No way! Monkeys can't ride bikes. That's just silly!<br>
+<b>Abdul</b>: Actually, I heard they can. My cousin's friend's neighbor saw it at the zoo!<br>
+<b>Hari</b>: Really? I gotta see that for myself! Let's go to the zoo and find the bicycle-riding monkey!<br>
+<b>Henry</b>: Wait, what if the monkey wants to join the circus instead? They could be the greatest monkey acrobat ever!<br>
+<b>Abdul</b>: Yeah, with their balancing skills, they'd be a superstar! But they better not try to juggle bananas while riding the bike!<br>
+<b>Hari</b>: Haha, can you imagine a monkey on a unicycle juggling bananas? That would be bananas!<br>
+<b>Henry</b>: And if they fall, they could land in a pile of banana peels. Slippery situation for a monkey!<br>
+<b>Abdul</b>: We should definitely start a monkey circus. The crowd would go bananas for it!<br>
+<b>Hari</b>: Let's do it! But first, we need to find that legendary bike-riding monkey. The circus awaits!`
+          }
+        ]
+      }
+    },
+    {
+      id: 'superpower',
+      type: 'passage',
+      label: 'Superpowers',
+      data: {
+        title: 'Superpowers',
+        text: [
+          {
+            type: 'html',
+            text: `<b>Rahul</b>: Hey, did you know that I can speak in animal language?<br>
+<b>Sara</b>: Really? That's amazing! Show me!<br>
+<b>Rahul</b>: [making funny animal noises] Woof, meow, quack!<br>
+<b>Riya</b>: Haha! That's not how animals talk. Let me try. [starts imitating a monkey] Ooh-ooh, ah-ah!<br>
+<b>Sara</b>: Look, I can do better! [pretends to be a dinosaur] Roar! I'm a ferocious T-Rex!<br>
+<b>Rahul</b>: Oh no, a T-Rex! Run for your lives!<br>
+<b>Riya</b>: Don't worry, I'll save us! [puts on a superhero cape] I am Super Kid, defender of playgrounds!<br>
+<b>Sara</b>: Quick, Super Kid, there's an ice cream emergency! The popsicles are melting!<br>
+<b>Rahul</b>: We need a superpower to freeze time! Who can do that?<br>
+<b>Riya</b>: I know! It's Brain Freeze Boy! [pretends to have a frozen brain] Brrr, I can freeze anything with my ice-cold thoughts!<br>
+<b>Rahul</b>: Haha, let's save the ice cream before it's too late!<br>
+<b>Sara</b>: Together, we are the Super Funny Squad, here to rescue treats and spread laughter!<br>
+<b>Riya</b>: Laughter is our superpower, and we're unstoppable!<br><br>
+<b>All children</b>: [giggling and playing, enjoying their imaginative adventure]`
+          }
+        ]
+      }
+    },
+    {
+      id: 'bobby',
+      type: 'passage',
+      label: 'Bobby',
+      data: {
+        title: 'Bobby',
+        text: [
+          {
+            type: 'html',
+            text: `<b>George</b>: Hey, did you hear about the time Bobby tried to eat a whole watermelon in one bite?<br>
+<b>Rahul</b>: No way! That sounds impossible. What happened?<br>
+<b>George</b>: Well, he opened his mouth really wide and took a huge bite. But then the watermelon slipped out of his hands and SPLAT! It landed right on his face!<br>
+<b>Rahul</b>: Haha, that must have been hilarious! Did he turn into a watermelon monster?<br>
+<b>George</b>: Almost! His face was all sticky and red. He looked like a silly clown. We couldn't stop laughing!<br>
+<b>Rahul</b>: I wish I could have seen that. Bobby's always up to something funny. What other crazy things has he done?<br>
+<b>George</b>: Oh, you won't believe it! He tried to ride his tricycle backward down the hill once but ended up getting scolded by his mom instead.<br><br>
+[Their Crazy Bobby Stories continued for a long time.]`
+          }
+        ]
+      }
+    },
+    {
+      id: 'dream',
+      type: 'passage',
+      label: 'Dream',
+      data: {
+        title: 'Dream',
+        text: [
+          {
+            type: 'html',
+            text: `<b>Peter</b>: Good morning, Mommy!<br>
+<b>Mom</b>: Good morning, sweetheart! How did you sleep?<br>
+<b>Peter</b>: I had the best dream ever! I was flying like a superhero!<br>
+<b>Mom</b>: That sounds amazing! Today is going to be a great day then. Let's have a healthy breakfast to start our day.<br>
+<b>Peter</b>: Yay! I love pancakes. Can I help you make them?<br>
+<b>Mom</b>: Of course, you can! We'll make them together. What's your plan for today?<br>
+<b>Peter</b>: I want to play in the park with my friends. Can we go, please?<br>
+<b>Mom</b>: Absolutely! We'll go to the park after breakfast. Remember to have fun and be kind to everyone.<br>
+<b>Peter</b>: I will! Thank you, Mommy! You're the best!<br>
+<b>Mom</b>: You're welcome, sweetheart. Have a wonderful day!`
+          }
+        ]
+      }
+    },
+    {
+      id: 'memories',
+      type: 'passage',
+      label: 'Memories',
+      data: {
+        title: 'Memories',
+        text: [
+          {
+            type: 'html',
+            text: `<b>Susie</b>: Hey, did you hear about the time Billy tried to fly like a superhero?<br>
+<b>Timmy</b>: Yeah, he put on a cape and jumped off the swing! But instead of flying, he just landed on his bottom. It was hilarious!<br>
+<b>Billy</b>: And remember when Susie tried to teach her dog to dance? The poor pup ended up doing a crazy wiggle instead. We couldn't stop laughing!<br>
+<b>Susie</b>: Oh, and let's not forget when Timmy dressed up as a dinosaur and scared his little sister. She ran all the way to the neighbor's yard screaming!<br>
+<b>Timmy</b>: Haha, that was epic! But you know what's even funnier? When we all tried to build a treehouse and ended up with a pile of sticks!<br>
+<b>Billy</b>: Yeah, it collapsed as soon as we stepped on it. We were the worst builders ever! But those memories always make us giggle, don't they?`
+          }
+        ]
+      }
+    }
+  ]
+};
