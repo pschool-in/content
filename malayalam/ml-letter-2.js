76829f8c4378394a94eb732f293445ebb03bfa0e
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-08-08 10:54:46 +0530

    updated

---
+++ malayalam/ml-letter-2.js
@@ -0,0 +1,203 @@
+export default {
+  label: 'Letters',
+  lang: 'ml',
+  id: 'ml-letter-2',
+  list: [
+    {
+      type: 'rightOne',
+      label: 'Identify Letter',
+      id: 'sound-letter',
+      lockAfter: 3,
+      commonData: {
+        title: 'Identify Letter based on Sound',
+        type: 'letters'
+      },
+      data: [
+        {
+          audioOffset: 0,
+          audio: 'ml/ml-letter-1.mp3',
+          words: 'ക, കാ, കി, കീ, കു, കൂ, കെ, കേ, കൊ, കോ, കൌ, കം, കഃ'
+        },
+        {
+          audioOffset: 26,
+          audio: 'ml/ml-letter-1.mp3',
+          words: 'ഖ, ഖാ, ഖി, ഖീ, ഖു, ഖൂ, ഖെ, ഖേ, ഖൊ, ഖോ, ഖൌ, ഖം, ഖഃ'
+        },
+        {
+          audioOffset: 52,
+          audio: 'ml/ml-letter-1.mp3',
+          words: 'ഗ, ഗാ, ഗി, ഗീ, ഗു, ഗൂ, ഗെ, ഗേ, ഗൊ, ഗോ, ഗൌ, ഗം, ഗഃ'
+        },
+        {
+          audioOffset: 78,
+          audio: 'ml/ml-letter-1.mp3',
+          words: 'ഘ, ഘാ, ഘി, ഘീ, ഘു, ഘൂ, ഘെ, ഘേ, ഘൊ, ഘോ, ഘൌ, ഘം, ഘഃ'
+        },
+        {
+          audioOffset: 104,
+          audio: 'ml/ml-letter-1.mp3',
+          words: 'ച, ചാ, ചി, ചീ, ചു, ചൂ, ചെ, ചേ, ചൊ, ചോ, ചൌ, ചം, ചഃ'
+        },
+        //2
+        {
+          audioOffset: 0,
+          audio: 'ml/ml-letter-2.mp3',
+          words: 'ഛ, ഛാ, ഛി, ഛീ, ഛു, ഛൂ, ഛെ, ഛേ, ഛൊ, ഛോ, ഛൌ, ഛം, ഛഃ'
+        },
+        {
+          audioOffset: 26,
+          audio: 'ml/ml-letter-2.mp3',
+          words: 'ജ, ജാ, ജി, ജീ, ജു, ജൂ, ജെ, ജേ, ജൊ, ജോ, ജൌ, ജം, ജഃ'
+        },
+        {
+          audioOffset: 52,
+          audio: 'ml/ml-letter-2.mp3',
+          words: 'ട, ടാ, ടി, ടീ, ടു, ടൂ, ടെ, ടേ, ടൊ, ടോ, ടൌ, ടം, ടഃ'
+        },
+        {
+          audioOffset: 78,
+          audio: 'ml/ml-letter-2.mp3',
+          words: 'ഠ, ഠാ, ഠി, ഠീ, ഠു, ഠൂ, ഠെ, ഠേ, ഠൊ, ഠോ, ഠൌ, ഠം, ഠഃ'
+        },
+        {
+          audioOffset: 104,
+          audio: 'ml/ml-letter-2.mp3',
+          words: 'ഡ, ഡാ, ഡി, ഡീ, ഡു, ഡൂ, ഡെ, ഡേ, ഡൊ, ഡോ, ഡൌ, ഡം, ഡഃ'
+        },
+        //3
+        {
+          audioOffset: 0,
+          audio: 'ml/ml-letter-3.mp3',
+          words: 'ഢ, ഢാ, ഢി, ഢീ, ഢു, ഢൂ, ഢെ, ഢേ, ഢൊ, ഢോ, ഢൌ, ഢം, ഢഃ'
+        },
+        {
+          audioOffset: 26,
+          audio: 'ml/ml-letter-3.mp3',
+          words: 'ണ, ണാ, ണി, ണീ, ണു, ണൂ, ണെ, ണേ, ണൊ, ണോ, ണൌ, ണം, ണഃ'
+        },
+        {
+          audioOffset: 52,
+          audio: 'ml/ml-letter-3.mp3',
+          words: 'ത, താ, തി, തീ, തു, തൂ, തെ, തേ, തൊ, തോ, തൌ, തം, തഃ'
+        },
+        {
+          audioOffset: 78,
+          audio: 'ml/ml-letter-3.mp3',
+          words: 'ഥ, ഥാ, ഥി, ഥീ, ഥു, ഥൂ, ഥെ, ഥേ, ഥൊ, ഥോ, ഥൌ, ഥം, ഥഃ'
+        },
+        {
+          audioOffset: 104,
+          audio: 'ml/ml-letter-3.mp3',
+          words: 'ദ, ദാ, ദി, ദീ, ദു, ദൂ, ദെ, ദേ, ദൊ, ദോ, ദൌ, ദം, ദഃ'
+        },
+        // 4
+        {
+          audioOffset: 0,
+          audio: 'ml/ml-letter-4.mp3',
+          words: 'ധ, ധാ, ധി, ധീ, ധു, ധൂ, ധെ, ധേ, ധൊ, ധോ, ധൌ, ധം, ധഃ'
+        },
+        {
+          audioOffset: 26,
+          audio: 'ml/ml-letter-4.mp3',
+          words: 'ന, നാ, നി, നീ, നു, നൂ, നെ, നേ, നൊ, നോ, നൌ, നം, നഃ'
+        },
+        //5
+        {
+          audioOffset: 0,
+          audio: 'ml/ml-letter-5.mp3',
+          words: 'പ, പാ, പി, പീ, പു, പൂ, പെ, പേ, പൊ, പോ, പൌ, പം, പഃ'
+        },
+        {
+          audioOffset: 26,
+          audio: 'ml/ml-letter-5.mp3',
+          words: 'ഫ, ഫാ, ഫി, ഫീ, ഫു, ഫൂ, ഫെ, ഫേ, ഫൊ, ഫോ, ഫൌ, ഫം, ഫഃ'
+        },
+        {
+          audioOffset: 52,
+          audio: 'ml/ml-letter-5.mp3',
+          words: 'ബ, ബാ, ബി, ബീ, ബു, ബൂ, ബെ, ബേ, ബൊ, ബോ, ബൌ, ബം, ബഃ'
+        },
+        {
+          audioOffset: 78,
+          audio: 'ml/ml-letter-5.mp3',
+          words: 'ഭ, ഭാ, ഭി, ഭീ, ഭു, ഭൂ, ഭെ, ഭേ, ഭൊ, ഭോ, ഭൌ, ഭം, ഭഃ'
+        },
+        {
+          audioOffset: 104,
+          audio: 'ml/ml-letter-5.mp3',
+          words: 'മ, മാ, മി, മീ, മു, മൂ, മെ, മേ, മൊ, മോ, മൌ, മം, മഃ'
+        },
+        //6
+        {
+          audioOffset: 0,
+          audio: 'ml/ml-letter-6.mp3',
+          words: 'യ, യാ, യി, യീ, യു, യൂ, യെ, യേ, യൊ, യോ, യൌ, യം, യഃ'
+        },
+        {
+          audioOffset: 26,
+          audio: 'ml/ml-letter-6.mp3',
+          words: 'ര, രാ, രി, രീ, രു, രൂ, രെ, രേ, രൊ, രോ, രൌ, രം, രഃ'
+        },
+        {
+          audioOffset: 52,
+          audio: 'ml/ml-letter-6.mp3',
+          words: 'ല, ലാ, ലി, ലീ, ലു, ലൂ, ലെ, ലേ, ലൊ, ലോ, ലൌ, ലം, ലഃ'
+        },
+        {
+          audioOffset: 78,
+          audio: 'ml/ml-letter-6.mp3',
+          words: 'വ, വാ, വി, വീ, വു, വൂ, വെ, വേ, വൊ, വോ, വൌ, വം, വഃ'
+        },
+        {
+          audioOffset: 104,
+          audio: 'ml/ml-letter-6.mp3',
+          words: 'ശ, ശാ, ശി, ശീ, ശു, ശൂ, ശെ, ശേ, ശൊ, ശോ, ശൌ, ശം, ശഃ'
+        },
+        //7
+        {
+          audioOffset: 0,
+          audio: 'ml/ml-letter-7.mp3',
+          words: 'സ, സാ, സി, സീ, സു, സൂ, സെ, സേ, സൊ, സോ, സൌ, സം, സഃ'
+        },
+        {
+          audioOffset: 26,
+          audio: 'ml/ml-letter-7.mp3',
+          words: 'ഹ, ഹാ, ഹി, ഹീ, ഹു, ഹൂ, ഹെ, ഹേ, ഹൊ, ഹോ, ഹൌ, ഹം, ഹഃ'
+        },
+        {
+          audioOffset: 52,
+          audio: 'ml/ml-letter-7.mp3',
+          words: 'ള, ളാ, ളി, ളീ, ളു, ളൂ, ളെ, ളേ, ളൊ, ളോ, ളൌ, ളം, ളഃ'
+        },
+        {
+          audioOffset: 78,
+          audio: 'ml/ml-letter-7.mp3',
+          words: 'ഴ, ഴാ, ഴി, ഴീ, ഴു, ഴൂ, ഴെ, ഴേ, ഴൊ, ഴോ, ഴൌ, ഴം, ഴഃ'
+        },
+        {
+          audioOffset: 104,
+          audio: 'ml/ml-letter-7.mp3',
+          words: 'റ, റാ, റി, റീ, റു, റൂ, റെ, റേ, റൊ, റോ, റൌ, റം, റഃ '
+        }
+      ]
+    },
+    {
+      type: 'completeWord',
+      label: 'Complete the word',
+      id: 'complete-word',
+      data: {
+        images: 'custom',
+        text: `car| കാ, ർ | _ , ർ | കാ, ക, ഫ, മ
+    peacock| മ,യി,ൽ | _,യി,ൽ | മ, കാ, ക, ഫ
+    onion|ഉ,ള്ളി |_,ള്ളി | ഉ, മ, ഒ, ച
+    camel|ഒ,ട്ട,കം |_,ട്ട,കം | ഒ, ഉ, മ, ച
+    jackfruit|ച,ക്ക |_,ക്ക | ച, ഉ, മ, ഒ
+    crab|ഞ,ണ്ട് |_,ണ്ട് | ഞ, ഉ, മ, ഒ
+    bell|മ,ണി |_,ണി | മ, ഉ, ഒ, ച
+    pen|പേ,ന |_,ന | പേ, ഉ, ഫ, ക
+    fruits|ഫ,ലം |_,ലം | ഫ, പേ, ഉ, ക
+    tiger|ക,ടു,വ |_,ടു,വ | ക, പേ, ഉ, ഫ`
+      }
+    }
+  ]
+};
