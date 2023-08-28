aae4efee514087f30035a84d23780975e4b3431f
Rethna <rethna2@gmail.com>
2023-08-28 09:23:57 +0530

    minor changes in hindi config files

--- hindi/hindi-more.js
+++ hindi/hindi-more.js
@@ -56,7 +56,7 @@ export default {
         },
         {
           label: 'संभाषण',
-          id: 'conversation'
+          id: 'hi-conversation'
         },
         {
           label: 'पहेलियाँ',
