15e21919019c8938db3668d52f91443fc9faf789
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-10-17 08:07:39 +0530

    updating

--- 3-5/vocabulary/new-words.js
+++ 3-5/vocabulary/new-words.js
@@ -82,7 +82,6 @@ export default {
             }
           ]
         },
-
         {
           questions: [
             {
@@ -101,6 +100,169 @@ export default {
               qText:
                 'As soon as he switched on the light, the cockroaches *scurried* away!',
               options: 'ran, jumped, hid'
+            },
+            {
+              qText: 'The magician performed a *bewitching* trick.',
+              options: 'enchanting, evil, bad intentions'
+            }
+          ]
+        },
+        {
+          questions: [
+            {
+              qText: 'She is always *meticulous* about her work.',
+              options: 'carefull and precise, unattentive, focused'
+            },
+            {
+              qText:
+                'The students showed great *enthusiasm* during the science experiment.',
+              options: 'eagerness, ambitious, alarmed'
+            },
+            {
+              qText:
+                'The athlete displayed remarkable *agility* during the competition.',
+              options: 'quick, slow, lazy'
+            },
+            {
+              qText: 'The garden was filled with *fragrant* flowers.',
+              options: 'pleasant smell, foul smell, stinking'
+            },
+            {
+              qText: 'The music at the party was *deafening*.',
+              options: 'extremely loud, extremely quite, defective'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'mcq-2',
+      label: 'We are Opposites!',
+      type: 'mcq',
+      commonData: {
+        title: 'Guess the antonym of the underlined word.'
+      },
+      data: [
+        {
+          questions: [
+            {
+              qText: 'These children are very *mischievous*',
+              options: 'kind, naughty, funny, '
+            },
+            {
+              qText: 'The new building is *enormous*!',
+              options: 'small, huge, colorful'
+            },
+            {
+              qText: `The white clothes are *dazzling* in the sun!`,
+              options: 'dull, shining, huge'
+            },
+            {
+              qText: 'The boat was about to *capsize*.',
+              options: 'stabilize, sink, rotate'
+            },
+            {
+              qText: 'The bank was *burgled* in broad daylight!',
+              options: 'secure, robbed, locked'
+            }
+          ]
+        },
+        {
+          questions: [
+            {
+              qText: 'City roads are usually quite *broad*.',
+              options: 'wide, narrow, dirty'
+            },
+            {
+              qText: 'The view of the valley was *stunning*!',
+              options: 'amazing, dangerous, dark'
+            },
+            {
+              qText: `Prem did not *invite* Arun to his party.`,
+              options: 'call, ask, come'
+            },
+            {
+              qText: 'We *sighted* three tiger cubs in the distance.',
+              options: 'saw, heard, walked'
+            },
+            {
+              qText: 'The telephone is an instrument to *communicate*.',
+              options: 'talk, see, travel'
+            }
+          ]
+        },
+        {
+          questions: [
+            {
+              qText: 'My mother was quite *annoyed* with me!',
+              options: 'irritated, kind, polite'
+            },
+            {
+              qText: 'Which is the shortest *route* to school?',
+              options: 'way, transport, bus'
+            },
+            {
+              qText: 'This is where I *live*.',
+              options: 'stay, sleep, play'
+            },
+            {
+              qText: 'Potato chips are Tanya’s *favourite* snack.',
+              options: 'best, hated'
+            },
+            {
+              qText: 'Metals like copper will *appear* dull if not polished.',
+              options: 'look, think'
+            }
+          ]
+        },
+        {
+          questions: [
+            {
+              qText: 'The food in this restaurant is very *delicious*!',
+              options: 'tasty, hot, colorful'
+            },
+            {
+              qText: 'The tailor has *ruined* my dress!',
+              options: 'spoilt, lost, forgot'
+            },
+            {
+              qText: 'Her father likes his food *extremely* hot.',
+              options: 'very, little, tasty'
+            },
+            {
+              qText:
+                'As soon as he switched on the light, the cockroaches *scurried* away!',
+              options: 'ran, jumped, hid'
+            },
+            {
+              qText: 'The magician performed a *bewitching* trick.',
+              options: 'enchanting, evil, bad intentions'
+            }
+          ]
+        },
+        {
+          questions: [
+            {
+              qText: 'She is always *meticulous* about her work.',
+              options: 'carefull and precise, unattentive, focused'
+            },
+            {
+              qText:
+                'The students showed great *enthusiasm* during the science experiment.',
+              options: 'eagerness, ambitious, alarmed'
+            },
+            {
+              qText:
+                'The athlete displayed remarkable *agility* during the competition.',
+              options: 'quick, slow, lazy'
+            },
+            {
+              qText: 'The garden was filled with *fragrant* flowers.',
+              options: 'pleasant smell, foul smell, stinking'
+            },
+            {
+              qText: 'The music at the party was *deafening*.',
+              options: 'extremely loud, extremely quite, defective'
             }
           ]
         }
